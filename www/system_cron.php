7a48175eeeab58731050b3a24949ef6bf6cc9aff
Michael Schneider <ms49434@users.noreply.github.com>
2016-04-03 10:02:55 +0000

    text changes
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@2470 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/system_cron.php
+++ www/system_cron.php
@@ -6,16 +6,12 @@
 	Copyright (c) 2012-2016 The NAS4Free Project <info@nas4free.org>.
 	All rights reserved.
 
-	Portions of freenas (http://www.freenas.org).
-	Copyright (c) 2005-2011 by Olivier Cochard <olivier@freenas.org>.
-	All rights reserved.
-
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
 
 	1. Redistributions of source code must retain the above copyright notice, this
 	   list of conditions and the following disclaimer.
-	   
+
 	2. Redistributions in binary form must reproduce the above copyright notice,
 	   this list of conditions and the following disclaimer in the documentation
 	   and/or other materials provided with the distribution.
--- www/system_loaderconf_edit.php
+++ www/system_loaderconf_edit.php
@@ -11,6 +11,7 @@
 
 	1. Redistributions of source code must retain the above copyright notice, this
 	   list of conditions and the following disclaimer.
+
 	2. Redistributions in binary form must reproduce the above copyright notice,
 	   this list of conditions and the following disclaimer in the documentation
 	   and/or other materials provided with the distribution.
@@ -33,26 +34,34 @@
 require("auth.inc");
 require("guiconfig.inc");
 
+$sphere_scriptname = basename(__FILE__);
+$sphere_header = 'Location: '.$sphere_scriptname;
+$sphere_header_parent = 'Location: system_loaderconf.php';
+$sphere_notifier = 'loaderconf';
+$sphere_array = [];
+$sphere_record = [];
+$prerequisites_ok = true;
+
 $mode_page = ($_POST) ? PAGE_MODE_POST : (($_GET) ? PAGE_MODE_EDIT : PAGE_MODE_ADD); // detect page mode
 
 if (PAGE_MODE_POST == $mode_page) { // POST is Cancel or not Submit => cleanup
 	if ((isset($_POST['Cancel']) && $_POST['Cancel']) || !(isset($_POST['Submit']) && $_POST['Submit'])) {
-		header("Location: system_loaderconf.php");
+		header($sphere_header_parent);
 		exit;
 	}
 }
 
-$pconfig = [];
+$sphere_record = [];
 $prerequisites_ok = true;
 
 if ((PAGE_MODE_POST == $mode_page) && isset($_POST['uuid']) && is_uuid_v4($_POST['uuid'])) {
-	$pconfig['uuid'] = $_POST['uuid'];
+	$sphere_record['uuid'] = $_POST['uuid'];
 } else {
 	if ((PAGE_MODE_EDIT == $mode_page) && isset($_GET['uuid']) && is_uuid_v4($_GET['uuid'])) {
-		$pconfig['uuid'] = $_GET['uuid'];
+		$sphere_record['uuid'] = $_GET['uuid'];
 	} else {
 		$mode_page = PAGE_MODE_ADD; // Force ADD
-		$pconfig['uuid'] = uuid();
+		$sphere_record['uuid'] = uuid();
 	}
 }
 
@@ -60,10 +69,10 @@ if (!(isset($config['system']['loaderconf']['param']) && is_array($config['syste
 	$config['system']['loaderconf']['param'] = [];
 }
 array_sort_key($config['system']['loaderconf']['param'], "name");
-$a_loaderconf = &$config['system']['loaderconf']['param'];
+$sphere_array = &$config['system']['loaderconf']['param'];
 
-$cnid = array_search_ex($pconfig['uuid'], $a_loaderconf, "uuid"); // find index of uuid
-$mode_updatenotify = updatenotify_get_mode("loaderconf", $pconfig['uuid']); // get updatenotify mode for uuid
+$cnid = array_search_ex($sphere_record['uuid'], $a_loaderconf, "uuid"); // find index of uuid
+$mode_updatenotify = updatenotify_get_mode($sphere_notifier, $sphere_record['uuid']); // get updatenotify mode for uuid
 $mode_record = RECORD_ERROR;
 if (false !== $cnid) { // uuid found
 	if ((PAGE_MODE_POST == $mode_page || (PAGE_MODE_EDIT == $mode_page))) { // POST or EDIT
@@ -87,53 +96,53 @@ if (false !== $cnid) { // uuid found
 	}
 }
 if (RECORD_ERROR == $mode_record) { // oops, someone tries to cheat, over and out
-	header("Location: system_loaderconf.php");
+	header($sphere_header_parent);
 	exit;
 }
 
 if (PAGE_MODE_POST == $mode_page) { // We know POST is "Submit", already checked
 	unset($input_errors);
-	$pconfig['enable'] = isset($_POST['enable']) ? true : false;
-	$pconfig['name'] = trim($_POST['name']);
-	$pconfig['value'] = $_POST['value'];
-	$pconfig['comment'] = $_POST['comment'];
+	$sphere_record['enable'] = isset($_POST['enable']) ? true : false;
+	$sphere_record['name'] = trim($_POST['name']);
+	$sphere_record['value'] = $_POST['value'];
+	$sphere_record['comment'] = $_POST['comment'];
 
 	// Input validation.
 	$reqdfields = explode(" ", "name value");
 	$reqdfieldsn = array(gettext("Name"), gettext("Value"));
 	$reqdfieldst = explode(" ", "string string");
 
-	do_input_validation($pconfig, $reqdfields, $reqdfieldsn, $input_errors);
-	do_input_validation_type($pconfig, $reqdfields, $reqdfieldsn, $reqdfieldst, $input_errors);
+	do_input_validation($sphere_record, $reqdfields, $reqdfieldsn, $input_errors);
+	do_input_validation_type($sphere_record, $reqdfields, $reqdfieldsn, $reqdfieldst, $input_errors);
 
 	if ($prerequisites_ok && empty($input_errors)) {
 		if (RECORD_NEW == $mode_record) {
-			$a_loaderconf[] = $pconfig;
-			updatenotify_set("loaderconf", UPDATENOTIFY_MODE_NEW, $pconfig['uuid']);
+			$sphere_array[] = $sphere_record;
+			updatenotify_set($sphere_notifier, UPDATENOTIFY_MODE_NEW, $sphere_record['uuid']);
 		} else {
-			$a_loaderconf[$cnid] = $pconfig;
+			$sphere_array[$cnid] = $sphere_record;
 			if (UPDATENOTIFY_MODE_UNKNOWN == $mode_updatenotify) {
-				updatenotify_set("loaderconf", UPDATENOTIFY_MODE_MODIFIED, $pconfig['uuid']);
+				updatenotify_set($sphere_notifier, UPDATENOTIFY_MODE_MODIFIED, $sphere_record['uuid']);
 			}
 		}
 		write_config();
-		header("Location: system_loaderconf.php");
+		header($sphere_header_parent);
 		exit;
 	}
 } else { // EDIT / ADD
 	switch ($mode_record) {
 		case RECORD_NEW:
-			$pconfig['enable'] = true;
-			$pconfig['name'] = "";
-			$pconfig['value'] = "";
-			$pconfig['comment'] = "";
+			$sphere_record['enable'] = true;
+			$sphere_record['name'] = "";
+			$sphere_record['value'] = "";
+			$sphere_record['comment'] = "";
 			break;
 		case RECORD_NEW_MODIFY:
 		case RECORD_MODIFY:
-			$pconfig['enable'] = isset($a_loaderconf[$cnid]['enable']);
-			$pconfig['name'] = trim($a_loaderconf[$cnid]['name']);
-			$pconfig['value'] = $a_loaderconf[$cnid]['value'];
-			$pconfig['comment'] = $a_loaderconf[$cnid]['comment'];
+			$sphere_record['enable'] = isset($sphere_array[$cnid]['enable']);
+			$sphere_record['name'] = trim($sphere_array[$cnid]['name']);
+			$sphere_record['value'] = $sphere_array[$cnid]['value'];
+			$sphere_record['comment'] = $sphere_array[$cnid]['comment'];
 			break;
 	}
 }
@@ -163,15 +172,15 @@ $pgtitle = array(gettext("System"), gettext("Advanced"), gettext("loader.conf"),
 				<?php if (!empty($input_errors)) print_input_errors($input_errors);?>
 				<?php if (file_exists($d_sysrebootreqd_path)) print_info_box(get_std_save_message(0));?>
 				<table width="100%" border="0" cellpadding="6" cellspacing="0">
-					<?php html_titleline_checkbox("enable", "", $pconfig['enable'] ? true : false, gettext("Enable"));?>
-					<?php html_inputbox("name", gettext("Name"), $pconfig['name'], gettext("Name of the variable."), true, 40);?>
-					<?php html_inputbox("value", gettext("Value"), $pconfig['value'], gettext("The value of the variable."), true);?>
-					<?php html_inputbox("comment", gettext("Comment"), $pconfig['comment'], gettext("You may enter a description here for your reference."), false, 40);?>
+					<?php html_titleline_checkbox("enable", "", $sphere_record['enable'] ? true : false, gettext("Enable"));?>
+					<?php html_inputbox("name", gettext("Name"), $sphere_record['name'], gettext("Name of the variable."), true, 40);?>
+					<?php html_inputbox("value", gettext("Value"), $sphere_record['value'], gettext("The value of the variable."), true);?>
+					<?php html_inputbox("comment", gettext("Comment"), $sphere_record['comment'], gettext("You may enter a description here for your reference."), false, 40);?>
 				</table>
 				<div id="submit">
 					<input name="Submit" type="submit" class="formbtn" value="<?=(RECORD_NEW != $mode_record) ? gettext("Save") : gettext("Add")?>" />
 					<input name="Cancel" type="submit" class="formbtn" value="<?=gettext("Cancel");?>" />
-					<input name="uuid" type="hidden" value="<?=$pconfig['uuid'];?>" />
+					<input name="uuid" type="hidden" value="<?=$sphere_record['uuid'];?>" />
 				</div>
 				<?php include("formend.inc");?>
 			</form>
