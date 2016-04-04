23a9dbebc6a5b8abd36253fbca06ed572ab7a2c3
Michael Zoon <zoon01@users.noreply.github.com>
2016-04-04 03:25:37 +0000

    Upgrade phpvirtualbox to 5.0-5.
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@2483 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/ports/phpvirtualbox/files/patch-js_phpvirtualbox.js
+++ build/ports/phpvirtualbox/files/patch-js_phpvirtualbox.js
@@ -1,6 +1,6 @@
---- js/phpvirtualbox.js.orig	2015-10-06 17:01:44.551054000 +0900
-+++ js/phpvirtualbox.js	2015-10-06 19:16:37.225658000 +0900
-@@ -931,7 +931,8 @@
+--- js/phpvirtualbox.js.orig	2016-03-16 11:10:17.306411000 +0100
++++ js/phpvirtualbox.js	2016-03-16 11:10:17.333915000 +0100
+@@ -930,7 +930,8 @@
  						   rowStr += ' <img src="images/vbox/blank.gif" style="vspace:0px;hspace:0px;height2px;width:10px;" /> (' + chost + ':' + d['VRDEServerInfo']['port'] + ')';
  					   // VNC   
  					   } else {
