f776d9b23aa961f3afd1af3483f6bd63a3c12366
Michael Zoon <zoon01@users.noreply.github.com>
2016-04-13 01:10:44 +0000

    set svn link
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/10.2.0.2@2533 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/make.sh
+++ build/make.sh
@@ -96,7 +96,7 @@ echo "NAS4FREE_TMPDIR=${NAS4FREE_TMPDIR}" >> ${NAS4FREE_MK}
 
 # Local variables
 NAS4FREE_URL=$(cat $NAS4FREE_SVNDIR/etc/prd.url)
-NAS4FREE_SVNURL="https://svn.code.sf.net/p/nas4free/code/trunk"
+NAS4FREE_SVNURL="https://svn.code.sf.net/p/nas4free/code/branches/10.2.0.2"
 NAS4FREE_SVN_SRCTREE="svn://svn.FreeBSD.org/base/releng/10.2"
 
 # Size in MB of the MFS Root filesystem that will include all FreeBSD binary
