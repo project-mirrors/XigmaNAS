7e3724effe2ccea1b9b84f14f386bac3f164a87c
Michael Zoon <zoon01@users.noreply.github.com>
2016-04-26 23:46:41 +0000

    Add warning message
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@2581 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/system_backup.php
+++ www/system_backup.php
@@ -6,15 +6,12 @@
 	Copyright (c) 2012-2016 The NAS4Free Project <info@nas4free.org>.
 	All rights reserved.
 
-	Portions of freenas (http://www.freenas.org).
-	Copyright (c) 2005-2011 by Olivier Cochard <olivier@freenas.org>.
-	All rights reserved.
-
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
 
 	1. Redistributions of source code must retain the above copyright notice, this
 	   list of conditions and the following disclaimer.
+
 	2. Redistributions in binary form must reproduce the above copyright notice,
 	   this list of conditions and the following disclaimer in the documentation
 	   and/or other materials provided with the distribution.
@@ -243,10 +240,13 @@ $(document).ready(function(){
 						</div>
 					</td>
 			    </tr>
-			  </table>
+		</table>
+			<div id="remarks">
+				<?php html_remark("warning", gettext("Warning"), sprintf(gettext("It is recommended to use encryption before you store the backup in a safe location.")));?>
+				</div>
 			</td>
 		</tr>
-	</table>
-	<?php include("formend.inc");?>
+</table>
+<?php include("formend.inc");?>
 </form>
 <?php include("fend.inc");?>
