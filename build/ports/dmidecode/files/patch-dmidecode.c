655ccb9bdf5d0e1b038beb0b57d1f17806500de2
Michael Zoon <zoon01@users.noreply.github.com>
2016-06-24 21:54:38 +0000

    update patch dmidecode
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@2773 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/ports/dmidecode/files/patch-dmidecode.c
+++ build/ports/dmidecode/files/patch-dmidecode.c
@@ -1,6 +1,35 @@
 --- dmidecode.c.orig	2015-09-03 08:03:19.000000000 +0200
-+++ dmidecode.c	2015-12-11 12:16:27.000000000 +0200
-@@ -4521,16 +4521,30 @@
++++ dmidecode.c	2016-06-24 23:43:31.000000000 +0200
+@@ -2946,7 +2946,7 @@
+  * first 5 characters of the device name to be trimmed. It's easy to
+  * check and fix, so do it, but warn.
+  */
+-static void dmi_fixup_type_34(struct dmi_header *h)
++static void dmi_fixup_type_34(struct dmi_header *h, int display)
+ {
+ 	u8 *p = h->data;
+ 
+@@ -2954,7 +2954,9 @@
+ 	if (h->length == 0x10
+ 	 && is_printable(p + 0x0B, 0x10 - 0x0B))
+ 	{
+-		printf("Invalid entry length (%u). Fixed up to %u.\n", 0x10, 0x0B);
++		if (!(opt.flags & FLAG_QUIET) && display)
++			printf("Invalid entry length (%u). Fixed up to %u.\n",
++				0x10, 0x0B);
+ 		h->length = 0x0B;
+ 	}
+ }
+@@ -4443,7 +4445,7 @@
+ 
+ 		/* Fixup a common mistake */
+ 		if (h.type == 34)
+-			dmi_fixup_type_34(&h);
++			dmi_fixup_type_34(&h, display);
+ 
+ 		/* look for the next handle */
+ 		next = data + h.length;
+@@ -4521,16 +4523,30 @@
  		printf("\n");
  	}
  
@@ -39,7 +68,7 @@
  	{
  		fprintf(stderr, "Table is unreachable, sorry."
  #ifndef USE_MMAP
-@@ -4748,6 +4762,7 @@
+@@ -4748,6 +4764,7 @@
  	int ret = 0;                /* Returned value */
  	int found = 0;
  	off_t fp;
@@ -47,7 +76,7 @@
  	int efi;
  	u8 *buf;
  
-@@ -4817,8 +4832,9 @@
+@@ -4817,8 +4834,9 @@
  	 * contain one of several types of entry points, so read enough for
  	 * the largest one, then determine what type it contains.
  	 */
@@ -58,7 +87,7 @@
  	{
  		if (!(opt.flags & FLAG_QUIET))
  			printf("Getting SMBIOS data from sysfs.\n");
-@@ -4864,8 +4880,17 @@
+@@ -4864,8 +4882,17 @@
  		goto exit_free;
  	}
  
