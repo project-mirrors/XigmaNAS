3e19b3fc0dafd532553c1daa8fc3e7b318f90092
Michael Schneider <ms49434@users.noreply.github.com>
2016-10-31 16:42:01 +0000

    code cleanup of copyright information
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@3078 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/quixplorer/_include/footer.php
+++ www/quixplorer/_include/footer.php
@@ -15,6 +15,7 @@
 
 	1. Redistributions of source code must retain the above copyright notice, this
 	   list of conditions and the following disclaimer.
+
 	2. Redistributions in binary form must reproduce the above copyright notice,
 	   this list of conditions and the following disclaimer in the documentation
 	   and/or other materials provided with the distribution.
@@ -39,7 +40,7 @@
 function show_footer() {
 	echo "</div>";
 	echo "<div id=\"pagefooter\">\n";
-	echo "<span><p><a title=\"www.".get_product_url()."\" href=\"http://".get_product_url()."\" target=\"_blank\"></a> ".str_replace("Copyright (C)","&copy;",get_product_copyright())."</a></p></span>\n";
+	echo "<span><p>", htmlspecialchars(get_product_copyright()), "</p></span>\n";
 	echo "</div>\n";
 	echo "</body>\n";
 	echo "</html>";
