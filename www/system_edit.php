03069d317e292649072fa4288d675e5d6318a3cf
Michael Schneider <ms49434@users.noreply.github.com>
2016-11-10 00:21:18 +0000

    short tags replaced
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@3161 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/system_edit.php
+++ www/system_edit.php
@@ -110,9 +110,8 @@ else
 							<input name="submit" type="submit" class="formbtn" id="Save" value="<?=gtext("Save");?>" />
 							<hr noshade="noshade" />
 							<?php if(isset($_POST['highlight']) && $_POST['highlight'] == "no"): ?>
-							<?=gtext("Rows"); ?>: <input size="3" name="rows" value="<? echo $rows; ?>" />
-							<?=gtext("Cols"); ?>: <input size="3" name="cols" value="<? echo $cols; ?>" />
-							|
+							<?=gtext("Rows"); ?>: <input size="3" name="rows" value="<?=$rows;?>"/>
+							<?=gtext("Cols"); ?>: <input size="3" name="cols" value="<?=$cols;?>"/>
 							<?php endif; ?>
 							<?=gtext("Highlighting"); ?>:
 							<input id="highlighting_enabled" name="highlight" type="radio" value="yes" <?php if($highlight == "yes") echo " checked=\"checked\""; ?> />
