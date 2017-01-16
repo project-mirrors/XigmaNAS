261fe45e11e11bf8b6e38e30543fe037e10398f3
Michael Schneider <ms49434@users.noreply.github.com>
2017-01-16 22:17:32 +0000

    new framework
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@3446 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/access_users.php
+++ www/access_users.php
@@ -31,127 +31,252 @@
 	of the authors and should not be interpreted as representing official policies,
 	either expressed or implied, of the NAS4Free Project.
 */
-require("auth.inc");
-require("guiconfig.inc");
+require 'auth.inc';
+require 'guiconfig.inc';
 
-$pgtitle = array(gtext("Access"), gtext("Users"));
+$sphere_scriptname = basename(__FILE__);
+$sphere_scriptname_child = 'access_users_edit.php';
+$sphere_header = 'Location: '.$sphere_scriptname;
+$sphere_header_parent = $sphere_header;
+$sphere_notifier = 'userdb_user';
+$sphere_array = [];
+$sphere_record = [];
+$checkbox_member_name = 'checkbox_member_array';
+$checkbox_member_array = [];
+$checkbox_member_record = [];
+$gt_record_add = gtext('Add User Account');
+$gt_record_mod = gtext('Edit User Account');
+$gt_record_del = gtext('User account is marked for deletion');
+$gt_record_loc = gtext('User account is locked');
+$gt_selection_delete = gtext('Delete Selected User Accounts');
+$gt_selection_delete_confirm = gtext('Do you want to delete selected user accounts?');
 
-if ($_POST) {
-	$pconfig = $_POST;
-
-	if (isset($_POST['apply']) && $_POST['apply']) {
-		$retval = 0;
-		if (!file_exists($d_sysrebootreqd_path)) {
-			$retval |= updatenotify_process("userdb_user", "userdbuser_process_updatenotification");
-			config_lock();
-			$retval |= rc_exec_service("userdb");
-			$retval |= rc_exec_service("websrv_htpasswd");
-			$retval |= rc_exec_service("fmperm");
-			if (isset($config['samba']['enable'])) {
-				$retval |= rc_exec_service("passdb");
-				$retval |= rc_update_service("samba");
-			}
-			config_unlock();
-		}
-		$savemsg = get_std_save_message($retval);
-		if ($retval == 0) {
-			updatenotify_delete("userdb_user");
-		}
-	}
-}
-
-if (!isset($config['access']['user']) || !is_array($config['access']['user']))
-	$config['access']['user'] = array();
-
-array_sort_key($config['access']['user'], "login");
-$a_user = &$config['access']['user'];
+//	sunrise
+$sphere_array = &array_make_branch($config,'access','user');
+array_sort_key($sphere_array,'login');
 $a_group = system_get_group_list();
 
-if (isset($_GET['act']) && $_GET['act'] === "del") {
-	updatenotify_set("userdb_user", UPDATENOTIFY_MODE_DIRTY, $_GET['uuid']);
-	header("Location: access_users.php");
-	exit;
-}
+if($_POST):
+	if(isset($_POST['apply']) && $_POST['apply']):
+		$retval = 0;
+		if(!file_exists($d_sysrebootreqd_path)):
+			$retval |= updatenotify_process($sphere_notifier,'userdbuser_process_updatenotification');
+			config_lock();
+			$retval |= rc_exec_service('userdb');
+			$retval |= rc_exec_service('websrv_htpasswd');
+			$retval |= rc_exec_service('fmperm');
+			if(isset($config['samba']['enable'])):
+				$retval |= rc_exec_service('passdb');
+				$retval |= rc_update_service('samba');
+			endif;
+			config_unlock();
+		endif;
+		$savemsg = get_std_save_message($retval);
+		if($retval == 0):
+			updatenotify_delete($sphere_notifier);
+		endif;
+//		header($sphere_header);
+//		exit;
+	endif;
+	if(isset($_POST['submit'])):
+		switch($_POST['submit']):
+			case 'rows.delete':
+				$checkbox_member_array = isset($_POST[$checkbox_member_name]) ? $_POST[$checkbox_member_name] : [];
+				foreach($checkbox_member_array as $checkbox_member_record):
+					if(false !== ($index_uuid = array_search_ex($checkbox_member_record,$sphere_array,'uuid'))):
+						$mode_updatenotify = updatenotify_get_mode($sphere_notifier,$sphere_array[$index_uuid]['uuid']);
+						switch ($mode_updatenotify):
+							case UPDATENOTIFY_MODE_NEW:  
+								updatenotify_clear($sphere_notifier,$sphere_array[$index_uuid]['uuid']);
+								updatenotify_set($sphere_notifier,UPDATENOTIFY_MODE_DIRTY_CONFIG,$sphere_array[$index_uuid]['uuid']);
+								break;
+							case UPDATENOTIFY_MODE_MODIFIED:
+								updatenotify_clear($sphere_notifier,$sphere_array[$index_uuid]['uuid']);
+								updatenotify_set($sphere_notifier,UPDATENOTIFY_MODE_DIRTY,$sphere_array[$index_uuid]['uuid']);
+								break;
+							case UPDATENOTIFY_MODE_UNKNOWN:
+								updatenotify_set($sphere_notifier,UPDATENOTIFY_MODE_DIRTY,$sphere_array[$index_uuid]['uuid']);
+								break;
+						endswitch;
+					endif;
+				endforeach;
+				header($sphere_header);
+				exit;
+				break;
+		endswitch;
+	endif;
+endif;
 
-function userdbuser_process_updatenotification($mode, $data) {
+function userdbuser_process_updatenotification($mode,$data) {
 	global $config;
 
 	$retval = 0;
-
-	switch ($mode) {
+	switch($mode):
 		case UPDATENOTIFY_MODE_NEW:
 		case UPDATENOTIFY_MODE_MODIFIED:
 			break;
 		case UPDATENOTIFY_MODE_DIRTY:
-			$index = array_search_ex($data, $config['access']['user'], "uuid");
-			if (false !== $index) {
-				unset($config['access']['user'][$index]);
+			if(false !== ($index_uuid = array_search_ex($data,$config['access']['user'],'uuid'))):
+				unset($config['access']['user'][$index_uuid]);
 				write_config();
-			}
+			endif;
 			break;
-	}
-
+	endswitch;
 	return $retval;
 }
+$pgtitle = [gtext('Access'),gtext('Users')];
 ?>
-<?php include("fbegin.inc");?>
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
-	<tr>
-		<td class="tabnavtbl">
-			<ul id="tabnav">
-				<li class="tabact"><a href="access_users.php" title="<?=gtext('Reload page');?>"><span><?=gtext("Users");?></span></a></li>
-				<li class="tabinact"><a href="access_users_groups.php"><span><?=gtext("Groups");?></span></a></li>
-			</ul>
-		</td>
-	</tr>
-  <tr>
-    <td class="tabcont">
-			<form action="access_users.php" method="post">
-				<?php if (!empty($savemsg)) print_info_box($savemsg);?>
-				<?php if (updatenotify_exists("userdb_user")) print_config_change_box();?>
-				<table width="100%" border="0" cellpadding="0" cellspacing="0">
-					<?php html_titleline2(gtext('User Setup'), 5);?>
-					<tr>
-						<td width="20%" class="listhdrlr"><?=gtext("User");?></td>
-						<td width="25%" class="listhdrr"><?=gtext("Full Name");?></td>
-						<td width="5%" class="listhdrr"><?=gtext("UID");?></td>
-						<td width="40%" class="listhdrr"><?=gtext("Group");?></td>
-						<td width="10%" class="list"></td>
-					</tr>
-					<?php foreach ($a_user as $userv):?>
-					<?php $notificationmode = updatenotify_get_mode("userdb_user", $userv['uuid']);?>
-					<tr>
-						<td class="listlr"><?=htmlspecialchars($userv['login']);?>&nbsp;</td>
-						<td class="listr"><?=htmlspecialchars($userv['fullname']);?>&nbsp;</td>
-						<td class="listr"><?=htmlspecialchars($userv['id']);?>&nbsp;</td>
-						<td class="listr"><?=array_search($userv['primarygroup'], $a_group); 
-							if (is_array($userv['group'])) {
-								for ($i = 0; $i < count($userv['group']); ) {
-									echo ", ".array_search($userv['group'][$i], $a_group); ++$i; 
-								}
-							}?>&nbsp;</td>
-						<?php if (UPDATENOTIFY_MODE_DIRTY != $notificationmode):?>
-						<td valign="middle" nowrap="nowrap" class="list">
-							<a href="access_users_edit.php?uuid=<?=$userv['uuid'];?>"><img src="images/edit.png" title="<?=gtext("Edit user");?>" border="0" alt="<?=gtext("Edit user");?>" /></a>&nbsp;
-							<a href="access_users.php?act=del&amp;uuid=<?=$userv['uuid'];?>" onclick="return confirm('<?=gtext("Do you really want to delete this user?");?>')"><img src="images/delete.png" title="<?=gtext("Delete user");?>" border="0" alt="<?=gtext("Delete user");?>" /></a>
-						</td>
+<?php include 'fbegin.inc';?>
+<script type="text/javascript">
+//<![CDATA[
+$(window).on("load",function() {
+<?php // Init action buttons.?>
+	$("#delete_selected_rows").click(function () {
+		return confirm('<?=$gt_selection_delete_confirm;?>');
+	});
+<?php // Disable action buttons.?>
+	disableactionbuttons(true);
+<?php // Init toggle checkbox.?>
+	$("#togglemembers").click(function() {
+		togglecheckboxesbyname(this,"<?=$checkbox_member_name;?>[]");
+	});
+<?php // Init member checkboxes.?>
+	$("input[name='<?=$checkbox_member_name;?>[]").click(function() {
+		controlactionbuttons(this,'<?=$checkbox_member_name;?>[]');
+	});
+<?php // Init spinner on submit for id form.?>
+	$("#iform").submit(function() { spinner(); });
+}); 
+function disableactionbuttons(ab_disable) {
+	var ab_element;
+	ab_element = document.getElementById('delete_selected_rows'); if((ab_element !== null) && (ab_element.disabled !== ab_disable)) { ab_element.disabled = ab_disable; }
+}
+function togglecheckboxesbyname(ego,triggerbyname) {
+	var a_trigger = document.getElementsByName(triggerbyname);
+	var n_trigger = a_trigger.length;
+	var ab_disable = true;
+	var i = 0;
+	for (; i < n_trigger; i++) {
+		if(a_trigger[i].type === 'checkbox') {
+			if(!a_trigger[i].disabled) {
+				a_trigger[i].checked = !a_trigger[i].checked;
+				if(a_trigger[i].checked) {
+					ab_disable = false;
+				}
+			}
+		}
+	}
+	if(ego.type === 'checkbox') { ego.checked = false; }
+	disableactionbuttons(ab_disable);
+}
+function controlactionbuttons(ego,triggerbyname) {
+	var a_trigger = document.getElementsByName(triggerbyname);
+	var n_trigger = a_trigger.length;
+	var ab_disable = true;
+	var i = 0;
+	for (; i < n_trigger; i++) {
+		if(a_trigger[i].type === 'checkbox') {
+			if(a_trigger[i].checked) {
+				ab_disable = false;
+				break;
+			}
+		}
+	}
+	disableactionbuttons(ab_disable);
+}
+//]]>
+</script>
+<table id="area_navigator"><tbody>
+	<tr><td class="tabnavtbl"><ul id="tabnav">
+		<li class="tabact"><a href="access_users.php" title="<?=gtext('Reload page');?>"><span><?=gtext('Users');?></span></a></li>
+		<li class="tabinact"><a href="access_users_groups.php"><span><?=gtext('Groups');?></span></a></li>
+	</ul></td></tr>
+</tbody></table>
+<table id="area_data"><tbody><tr><td id="area_data_frame"><form action="<?=$sphere_scriptname;?>" method="post" id="iform" name="iform">
+	<?php
+	if(!empty($savemsg)): 
+		print_info_box($savemsg);
+	endif;
+	if(file_exists($d_sysrebootreqd_path)):
+		print_info_box(get_std_save_message(0));
+	endif;
+	if(updatenotify_exists($sphere_notifier)):
+		print_config_change_box();
+	endif;
+	?>
+	<table id="area_data_selection">
+		<colgroup>
+			<col style="width:5%">
+			<col style="width:25%">
+			<col style="width:25%">
+			<col style="width:10%">
+			<col style="width:25%">
+			<col style="width:10%">
+		</colgroup>
+		<thead>
+			<?php html_titleline2(gtext('Overview'),6);?>
+			<tr>
+				<th class="lhelc"><input type="checkbox" id="togglemembers" name="togglemembers" title="<?=gtext('Invert Selection');?>"/></th>
+				<th class="lhell"><?=gtext('User');?></th>
+				<th class="lhell"><?=gtext('Full Name');?></th>
+				<th class="lhell"><?=gtext('UID');?></th>
+				<th class="lhell"><?=gtext('Group');?></th>
+				<th class="lhebl"><?=gtext('Toolbox');?></th>
+			</tr>
+		</thead>
+		<tfoot>
+			<?=html_row_add($sphere_scriptname_child,$gt_record_add,6);?>
+		</tfoot>
+		<tbody>
+			<?php foreach($sphere_array as $sphere_record):?>
+				<tr>
+					<?php
+					$notificationmode = updatenotify_get_mode($sphere_notifier,$sphere_record['uuid']);
+					$notdirty = (UPDATENOTIFY_MODE_DIRTY != $notificationmode) && (UPDATENOTIFY_MODE_DIRTY_CONFIG != $notificationmode);
+					$enabled = true; // isset($sphere_record['enable']);
+					$notprotected = !isset($sphere_record['protected']);
+					?>
+					<td class="<?=$enabled ? "lcelc" : "lcelcd";?>">
+						<?php if($notdirty && $notprotected):?>
+							<input type="checkbox" name="<?=$checkbox_member_name;?>[]" value="<?=$sphere_record['uuid'];?>" id="<?=$sphere_record['uuid'];?>"/>
 						<?php else:?>
-						<td valign="middle" nowrap="nowrap" class="list">
-							<img src="images/delete.png" border="0" alt="" />
-						</td>
+							<input type="checkbox" name="<?=$checkbox_member_name;?>[]" value="<?=$sphere_record['uuid'];?>" id="<?=$sphere_record['uuid'];?>" disabled="disabled"/>
 						<?php endif;?>
-					</tr>
-					<?php endforeach;?>
-					<tr>
-						<td class="list" colspan="4"></td>
-						<td class="list">
-							<a href="access_users_edit.php"><img src="images/add.png" title="<?=gtext("Add user");?>" border="0" alt="<?=gtext("Add user");?>" /></a>
-						</td>
-					</tr>
-				</table>
-				<?php include("formend.inc");?>
-			</form>
-		</td>
-	</tr>
-</table>
-<?php include("fend.inc");?>
+					</td>
+					<td class="<?=$enabled ? "lcell" : "lcelld";?>"><?=htmlspecialchars($sphere_record['login']);?></td>
+					<td class="<?=$enabled ? "lcell" : "lcelld";?>"><?=htmlspecialchars($sphere_record['fullname']);?></td>
+					<td class="<?=$enabled ? "lcell" : "lcelld";?>"><?=htmlspecialchars($sphere_record['id']);?></td>
+					
+					<?php
+					$a_group_key = [];
+					$a_group_key[] = array_search($sphere_record['primarygroup'],$a_group);
+					if(is_array($sphere_record['group'])):
+						foreach($sphere_record['group'] as $needle):
+							$a_group_key[] = array_search($needle,$a_group);
+						endforeach;
+					endif;
+					$helpinghand = implode(', ',$a_group_key);
+					?>
+					<td class="<?=$enabled ? "lcell" : "lcelld";?>"><?=htmlspecialchars($helpinghand);?></td>
+					<td class="lcebld">
+						<table class="area_data_selection_toolbox"><tbody><tr>
+							<?php
+							$helpinghand = sprintf('%s?uuid=%s',$sphere_scriptname_child,$sphere_record['uuid']);
+							echo html_row_toolbox($helpinghand,$gt_record_mod,$gt_record_del,$gt_record_loc,$notprotected,$notdirty);
+							?>
+							<td></td>
+							<td></td>
+						</tr></tbody></table>
+					</td>
+				</tr>
+			<?php endforeach;?>
+		</tbody>
+	</table>
+	<div id="submit">
+		<?php
+		echo html_button_delete_rows($gt_selection_delete);
+		?>
+	</div>
+	<?php include 'formend.inc';?>
+</form></td></tr></tbody></table>
+<?php include 'fend.inc';?>
