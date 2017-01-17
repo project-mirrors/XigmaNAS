f62709ede0838f004f2486c410c0f7bf85cd7427
Michael Schneider <ms49434@users.noreply.github.com>
2017-01-17 17:15:59 +0000

    cosmetic changes
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@3451 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/diag_infos_mount.php
+++ www/diag_infos_mount.php
@@ -34,38 +34,34 @@
 require 'auth.inc';
 require 'guiconfig.inc';
 
-$pgtitle = [gtext('Diagnostics'), gtext('Information'), gtext('Mounts')];
+unset($rawdata);
+exec('/sbin/mount',$rawdata);
+$output = htmlspecialchars(implode("\n",$rawdata));
+
+$pgtitle = [gtext('Diagnostics'),gtext('Information'),gtext('Mounts')];
 ?>
 <?php include 'fbegin.inc';?>
 <table id="area_navigator"><tbody>
-	<tr>
-	<td class="tabnavtbl">
-		<ul id="tabnav">
-			<li class="tabinact"><a href="diag_infos_disks.php"><span><?=gtext("Disks");?></span></a></li>
-			<li class="tabinact"><a href="diag_infos_disksinfo.php"><span><?=gtext("Disks (Info)");?></span></a></li>
-			<li class="tabinact"><a href="diag_infos_part.php"><span><?=gtext('Partitions');?></span></a></li>
-			<li class="tabinact"><a href="diag_infos_smart.php"><span><?=gtext('S.M.A.R.T.');?></span></a></li>
-			<li class="tabinact"><a href="diag_infos_space.php"><span><?=gtext('Space Used');?></span></a></li>
-			<li class="tabact"><a href="diag_infos_mount.php" title="<?=gtext('Reload page');?>"><span><?=gtext('Mounts');?></span></a></li>
-			<li class="tabinact"><a href="diag_infos_raid.php"><span><?=gtext('Software RAID');?></span></a></li>
-		</ul>
-	</td>
-</tr>
-	<tr>
-	<td class="tabnavtbl">
-		<ul id="tabnav2">
-			<li class="tabinact"><a href="diag_infos_iscsi.php"><span><?=gtext('iSCSI Initiator');?></span></a></li>
-			<li class="tabinact"><a href="diag_infos_ad.php"><span><?=gtext('MS Domain');?></span></a></li>
-			<li class="tabinact"><a href="diag_infos_samba.php"><span><?=gtext('CIFS/SMB');?></span></a></li>
-			<li class="tabinact"><a href="diag_infos_ftpd.php"><span><?=gtext('FTP');?></span></a></li>
-			<li class="tabinact"><a href="diag_infos_rsync_client.php"><span><?=gtext('RSYNC Client');?></span></a></li>
-			<li class="tabinact"><a href="diag_infos_swap.php"><span><?=gtext('Swap');?></span></a></li>
-			<li class="tabinact"><a href="diag_infos_sockets.php"><span><?=gtext('Sockets');?></span></a></li>
-			<li class="tabinact"><a href="diag_infos_ipmi.php"><span><?=gtext('IPMI Stats');?></span></a></li>
-			<li class="tabinact"><a href="diag_infos_ups.php"><span><?=gtext('UPS');?></span></a></li>
-		</ul>
-	</td>
-</tr>
+	<tr><td class="tabnavtbl"><ul id="tabnav">
+		<li class="tabinact"><a href="diag_infos_disks.php"><span><?=gtext('Disks');?></span></a></li>
+		<li class="tabinact"><a href="diag_infos_disksinfo.php"><span><?=gtext('Disks (Info)');?></span></a></li>
+		<li class="tabinact"><a href="diag_infos_part.php"><span><?=gtext('Partitions');?></span></a></li>
+		<li class="tabinact"><a href="diag_infos_smart.php"><span><?=gtext('S.M.A.R.T.');?></span></a></li>
+		<li class="tabinact"><a href="diag_infos_space.php"><span><?=gtext('Space Used');?></span></a></li>
+		<li class="tabact"><a href="diag_infos_mount.php" title="<?=gtext('Reload page');?>"><span><?=gtext('Mounts');?></span></a></li>
+		<li class="tabinact"><a href="diag_infos_raid.php"><span><?=gtext('Software RAID');?></span></a></li>
+	</ul></td></tr>
+	<tr><td class="tabnavtbl"><ul id="tabnav2">
+		<li class="tabinact"><a href="diag_infos_iscsi.php"><span><?=gtext('iSCSI Initiator');?></span></a></li>
+		<li class="tabinact"><a href="diag_infos_ad.php"><span><?=gtext('MS Domain');?></span></a></li>
+		<li class="tabinact"><a href="diag_infos_samba.php"><span><?=gtext('CIFS/SMB');?></span></a></li>
+		<li class="tabinact"><a href="diag_infos_ftpd.php"><span><?=gtext('FTP');?></span></a></li>
+		<li class="tabinact"><a href="diag_infos_rsync_client.php"><span><?=gtext('RSYNC Client');?></span></a></li>
+		<li class="tabinact"><a href="diag_infos_swap.php"><span><?=gtext('Swap');?></span></a></li>
+		<li class="tabinact"><a href="diag_infos_sockets.php"><span><?=gtext('Sockets');?></span></a></li>
+		<li class="tabinact"><a href="diag_infos_ipmi.php"><span><?=gtext('IPMI Stats');?></span></a></li>
+		<li class="tabinact"><a href="diag_infos_ups.php"><span><?=gtext('UPS');?></span></a></li>
+	</ul></td></tr>
 </tbody></table>
 <table id="area_data"><tbody><tr><td id="area_data_frame">
 	<table id="area_data_settings">
@@ -79,13 +75,7 @@ $pgtitle = [gtext('Diagnostics'), gtext('Information'), gtext('Mounts')];
 		<tbody>
 			<tr>
 				<td class="celltag"><?=gtext('Information');?></td>
-				<td class="celldata">
-					<?php
-						unset($rawdata);
-						exec('/sbin/mount', $rawdata);
-					?>
-					<pre><?php echo htmlspecialchars(implode("\n", $rawdata));?></pre>
-				</td>
+				<td class="celldata"><pre><?=$output;?></pre></td>
 			</tr>
 		</tbody>
 	</table>
