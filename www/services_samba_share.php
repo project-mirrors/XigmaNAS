ca10b6d940f0fbc79a9cde53d20edd29ac662fff
Michael Schneider <ms49434@users.noreply.github.com>
2017-01-27 22:00:26 +0000

    code changes
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@3661 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/services_samba_share.php
+++ www/services_samba_share.php
@@ -60,11 +60,8 @@ if ($_POST) {
 	}
 }
 
-if (!isset($config['samba']['share']) || !is_array($config['samba']['share']))
-	$config['samba']['share'] = array();
-
-array_sort_key($config['samba']['share'], "name");
-$a_share = &$config['samba']['share'];
+$a_share = &array_make_branch($config,'samba','share');
+array_sort_key($a_share,'name');
 
 if (isset($_GET['act']) && $_GET['act'] === "del") {
 	updatenotify_set("smbshare", UPDATENOTIFY_MODE_DIRTY, $_GET['uuid']);
@@ -95,58 +92,53 @@ function smbshare_process_updatenotification($mode, $data) {
 ?>
 <?php include 'fbegin.inc';?>
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr>
-	<td class="tabnavtbl">
-      		<ul id="tabnav">
-			<li class="tabinact"><a href="services_samba.php"><span><?=gtext("Settings");?></span></a></li>
-			<li class="tabact"><a href="services_samba_share.php" title="<?=gtext('Reload page');?>"><span><?=gtext("Shares");?></span></a></li>
-			</ul>
-	</td>
-</tr>
+	<tr><td class="tabnavtbl"><ul id="tabnav">
+		<li class="tabinact"><a href="services_samba.php"><span><?=gtext("Settings");?></span></a></li>
+		<li class="tabact"><a href="services_samba_share.php" title="<?=gtext('Reload page');?>"><span><?=gtext("Shares");?></span></a></li>
+	</ul></td></tr>
 	<tr> 
-	<td class="tabcont">
+		<td class="tabcont">
 			<form action="services_samba_share.php" method="post">
-			<?php if (!empty($savemsg)) print_info_box($savemsg); ?>
-			<?php if (updatenotify_exists("smbshare")) print_config_change_box();?>
-			<table width="100%" border="0" cellpadding="0" cellspacing="0">
-			<?php html_titleline2(gtext('Overview'), 6);?>
-		<tr>
-			<td width="30%" class="listhdrlr"><?=gtext("Path");?></td>
-			<td width="20%" class="listhdrr"><?=gtext("Name");?></td>
-			<td width="20%" class="listhdrr"><?=gtext("Comment");?></td>
-			<td width="10%" class="listhdrr"><?=gtext("Browseable");?></td>
-			<td width="10%" class="listhdrr"><?=gtext("Guest");?></td>
-			<td width="10%" class="list">
+				<?php if (!empty($savemsg)) print_info_box($savemsg); ?>
+				<?php if (updatenotify_exists("smbshare")) print_config_change_box();?>
+				<table width="100%" border="0" cellpadding="0" cellspacing="0">
+					<?php html_titleline2(gtext('Overview'), 6);?>
+					<tr>
+						<td width="30%" class="listhdrlr"><?=gtext("Path");?></td>
+						<td width="20%" class="listhdrr"><?=gtext("Name");?></td>
+						<td width="20%" class="listhdrr"><?=gtext("Comment");?></td>
+						<td width="10%" class="listhdrr"><?=gtext("Browseable");?></td>
+						<td width="10%" class="listhdrr"><?=gtext("Guest");?></td>
+						<td width="10%" class="list"></td>
+					</tr>
+					<?php foreach($a_share as $sharev):?>
+						<?php $notificationmode = updatenotify_get_mode("smbshare", $sharev['uuid']);?>
+						<tr>
+							<td class="listlr"><?=htmlspecialchars($sharev['path']);?>&nbsp;</td>
+							<td class="listr"><?=htmlspecialchars($sharev['name']);?>&nbsp;</td>
+							<td class="listr"><?=htmlspecialchars($sharev['comment']);?>&nbsp;</td>
+							<td class="listbg"><?=htmlspecialchars(isset($sharev['browseable'])?gtext("Yes"):gtext("No"));?></td>
+							<td class="listbg"><?=htmlspecialchars(isset($sharev['guest'])?gtext("Yes"):gtext("No"));?></td>
+							<?php if (UPDATENOTIFY_MODE_DIRTY != $notificationmode):?>
+								<td valign="middle" nowrap="nowrap" class="list">
+									<a href="services_samba_share_edit.php?uuid=<?=$sharev['uuid'];?>"><img src="images/edit.png" title="<?=gtext("Edit share");?>" border="0" alt="<?=gtext("Edit share");?>" /></a>
+									<a href="services_samba_share.php?act=del&amp;uuid=<?=$sharev['uuid'];?>" onclick="return confirm('<?=gtext("Do you really want to delete this share?");?>')"><img src="images/delete.png" title="<?=gtext("Delete share");?>" border="0" alt="<?=gtext("Delete share");?>" /></a>
+								</td>
+							<?php else:?>
+								<td valign="middle" nowrap="nowrap" class="list">
+									<img src="images/delete.png" border="0" alt="" />
+								</td>
+							<?php endif;?>
+						</tr>
+					<?php endforeach;?>
+					<tr> 
+						<td class="list" colspan="5"></td>
+						<td class="list"><a href="services_samba_share_edit.php"><img src="images/add.png" title="<?=gtext("Add share");?>" border="0" alt="<?=gtext("Add share");?>" /></a></td>
+					</tr>
+				</table>
+				<?php include 'formend.inc';?>
+			</form>
 		</td>
-		</tr>
-			<?php foreach($a_share as $sharev):?>
-			<?php $notificationmode = updatenotify_get_mode("smbshare", $sharev['uuid']);?>
-		<tr>
-  			<td class="listlr"><?=htmlspecialchars($sharev['path']);?>&nbsp;</td>
-			<td class="listr"><?=htmlspecialchars($sharev['name']);?>&nbsp;</td>
-			<td class="listr"><?=htmlspecialchars($sharev['comment']);?>&nbsp;</td>
-			<td class="listbg"><?=htmlspecialchars(isset($sharev['browseable'])?gtext("Yes"):gtext("No"));?></td>
-			<td class="listbg"><?=htmlspecialchars(isset($sharev['guest'])?gtext("Yes"):gtext("No"));?></td>
-			<?php if (UPDATENOTIFY_MODE_DIRTY != $notificationmode):?>
-			<td valign="middle" nowrap="nowrap" class="list">
-			<a href="services_samba_share_edit.php?uuid=<?=$sharev['uuid'];?>"><img src="images/edit.png" title="<?=gtext("Edit share");?>" border="0" alt="<?=gtext("Edit share");?>" /></a>
-			<a href="services_samba_share.php?act=del&amp;uuid=<?=$sharev['uuid'];?>" onclick="return confirm('<?=gtext("Do you really want to delete this share?");?>')"><img src="images/delete.png" title="<?=gtext("Delete share");?>" border="0" alt="<?=gtext("Delete share");?>" /></a>
-			</td>
-			<?php else:?>
-				<td valign="middle" nowrap="nowrap" class="list">
-				<img src="images/delete.png" border="0" alt="" />
-			</td>
-		<?php endif;?>
 	</tr>
-		<?php endforeach;?>
-		<tr> 
-		<td class="list" colspan="5"></td>
-		<td class="list"><a href="services_samba_share_edit.php"><img src="images/add.png" title="<?=gtext("Add share");?>" border="0" alt="<?=gtext("Add share");?>" /></a></td>
-	</tr>
-</table>
-<?php include 'formend.inc';?>
-</form>
-</td>
-</tr>
 </table>
 <?php include 'fend.inc';?>
