ad22f45aabd79cfa762256c43547f99f2f8ddbe8
Michael Schneider <ms49434@users.noreply.github.com>
2017-02-07 21:58:56 +0000

    sensor auto refresh added
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@3804 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/diag_infos_ipmi.php
+++ www/diag_infos_ipmi.php
@@ -39,45 +39,69 @@ $sphere_header = 'Location: '.$sphere_scriptname;
 
 function get_ipmi_sensor() {
 	$a_sensor = [];
-	mwexec2("ipmitool sensor", $a_output);
-	foreach ($a_output as $r_output) {
-		$r_sensor = explode('|', $r_output);
+	mwexec2('ipmitool sensor',$a_output);
+	foreach($a_output as $r_output):
+		$r_sensor = explode('|',$r_output);
 		$c_sensor = count($r_sensor);
-		for ($i = 0; $i < $c_sensor; $i++) {
+		for($i = 0;$i < $c_sensor;$i++):
 			$r_sensor[$i] = trim($r_sensor[$i]);
-		}
+		endfor;
 		$a_sensor[] = $r_sensor;
-	}
+	endforeach;
 	unset($a_output);
 	return $a_sensor;
 }
 
 function get_ipmi_fru() {
 	$a_fru = [];
-	mwexec2("ipmitool fru", $a_output);
-	foreach ($a_output as $r_output) {
-		$r_fru = explode(': ', $r_output, 2); // we need 2 columns only, tag and value
+	mwexec2('ipmitool fru',$a_output);
+	foreach($a_output as $r_output):
+		$r_fru = explode(': ',$r_output,2); // we need 2 columns only, tag and value
 		$c_fru = count($r_fru);
-		for ($i = 0; $i < $c_fru; $i++) {
+		for($i = 0;$i < $c_fru;$i++):
 			$r_fru[$i] = trim($r_fru[$i]);
-		}
+		endfor;
 		$a_fru[] = $r_fru;
-	}
+	endforeach;
 	unset($a_output);
 	return $a_fru;
 }
-
+function diag_infos_ipmi_ajax() {
+	$a_ipmi_sensor = get_ipmi_sensor();
+	$body_output = '';
+	foreach($a_ipmi_sensor as $r_ipmi_sensor):
+		$body_output .= '<tr>';
+		$body_output .= '<td class="lcell">' . htmlspecialchars($r_ipmi_sensor[0]) . '</td>';
+		$body_output .= '<td class="lcell">' . htmlspecialchars($r_ipmi_sensor[1] . ' ' . $r_ipmi_sensor[2]) . '</td>';
+		$body_output .= '<td class="lcell">' . htmlspecialchars($r_ipmi_sensor[3]) . '</td>';
+		$body_output .= '<td class="lcelr">' . htmlspecialchars($r_ipmi_sensor[4]) . '</td>';
+		$body_output .= '<td class="lcelr">' . htmlspecialchars($r_ipmi_sensor[9]) . '</td>';
+		$body_output .= '<td class="lcelr">' . htmlspecialchars($r_ipmi_sensor[6]) . '</td>';
+		$body_output .= '<td class="lcelr">' . htmlspecialchars($r_ipmi_sensor[7]) . '</td>';
+		$body_output .= '<td class="lcelr">' . htmlspecialchars($r_ipmi_sensor[5]) . '</td>';
+		$body_output .= '<td class="lcebr">' . htmlspecialchars($r_ipmi_sensor[8]) . '</td>';
+		$body_output .= '</tr>' . "\n";
+	endforeach;
+	return $body_output;
+}
 $a_ipmi_sensor = get_ipmi_sensor();
 $a_ipmi_fru = get_ipmi_fru();
-
-$pgtitle = [gtext('Diagnostics'), gtext('Information'), gtext('IPMI Stats')];
+if(is_ajax()):
+	$status = diag_infos_ipmi_ajax();
+	render_ajax($status);
+endif;
+$pgtitle = [gtext('Diagnostics'),gtext('Information'),gtext('IPMI Stats')];
+include 'fbegin.inc';
 ?>
-<?php include 'fbegin.inc';?>
 <script type="text/javascript">
 //<![CDATA[
-$(window).on("load", function() {
-	// Init spinner onsubmit()
-	$("#iform").submit(function() { spinner(); });
+$(document).ready(function(){
+	var gui = new GUI;
+	gui.recall(5000, 5000, 'diag_infos_ipmi.php', null, function(data) {
+		if ($('#area_refresh').length > 0) {
+			$('#area_refresh').html(data.data);
+		}
+	});
 });
 //]]>
 </script>
@@ -103,10 +127,29 @@ $(window).on("load", function() {
 		<li class="tabinact"><a href="diag_infos_ups.php"><span><?=gtext('UPS');?></span></a></li>
 	</ul></td></tr>
 </tbody></table>
-<table id="area_data"><tbody><tr><td id="area_data_frame"><form action="<?=$sphere_scriptname;?>" method="post" id="iform" name="iform">
-	<?php if(empty($a_ipmi_sensor)):?>
-		<?php html_remark2('sensor', gtext('System Message'), gtext('No IPMI sensor data available.'));?>
-	<?php else:?>
+<table id="area_data"><tbody><tr><td id="area_data_frame">
+<?php
+	if(empty($a_ipmi_sensor)):
+?>
+		<table class="area_data_settings">
+			<colgroup>
+				<col class="area_data_settings_col_tag">
+				<col class="area_data_settings_col_data">
+			</colgroup>
+			<thead>
+<?php
+				html_titleline2(gtext('Sensor Information'),9);
+?>
+			</thead>
+			<tbody>
+<?php
+				html_text2('sensor',gtext('System Message'),gtext('No IPMI sensor data available.'));
+?>			
+			</tbody>
+		</table>
+<?php
+	else:
+?>
 		<table class="area_data_selection">
 			<colgroup>
 				<col style="width:11%">
@@ -120,7 +163,9 @@ $(window).on("load", function() {
 				<col style="width:12%">
 			</colgroup>
 			<thead>
-				<?php html_titleline2(gtext('Sensor Information'), 9);?>
+<?php
+				html_titleline2(gtext('Sensor Information'),9);
+?>
 				<tr>
 					<td class="lhelc" colspan="3"><?=gtext('Sensor List');?></td>
 					<td class="lhelc" colspan="2"><?=gtext('Non-Recoverable');?></td>
@@ -139,58 +184,67 @@ $(window).on("load", function() {
 					<td class="lhebl"><?=gtext('Upper');?></td>
 				</tr>
 			</thead>
-			<tfoot>
-			</tfoot>
-			<tbody>
-				<?php foreach ($a_ipmi_sensor as $r_ipmi_sensor):?>
-					<tr>
-						<td class="lcell"><?=htmlspecialchars($r_ipmi_sensor[0]);?>&nbsp;</td>
-						<td class="lcell"><?=htmlspecialchars($r_ipmi_sensor[1] . ' ' . $r_ipmi_sensor[2]);?></td>
-						<td class="lcell"><?=htmlspecialchars($r_ipmi_sensor[3]);?>&nbsp;</td>
-						<td class="lcelr"><?=htmlspecialchars($r_ipmi_sensor[4]);?>&nbsp;</td>
-						<td class="lcelr"><?=htmlspecialchars($r_ipmi_sensor[9]);?>&nbsp;</td>
-						<td class="lcelr"><?=htmlspecialchars($r_ipmi_sensor[6]);?>&nbsp;</td>
-						<td class="lcelr"><?=htmlspecialchars($r_ipmi_sensor[7]);?>&nbsp;</td>
-						<td class="lcelr"><?=htmlspecialchars($r_ipmi_sensor[5]);?>&nbsp;</td>
-						<td class="lcebr"><?=htmlspecialchars($r_ipmi_sensor[8]);?>&nbsp;</td>
-					</tr>
-				<?php endforeach;?>
-			</tbody>
+			<tbody id="area_refresh"><?=diag_infos_ipmi_ajax();?></tbody>
 		</table>
-	<?php endif;?>
-	<?php if(empty($a_ipmi_fru)):?>
-		<?php html_remark2('sensor', gtext('System Message'), gtext('No IPMI FRU data available.'));?>
-	<?php else:?>
+<?php
+	endif;
+?>
+<?php
+	if(empty($a_ipmi_fru)):
+?>
 		<table class="area_data_settings">
 			<colgroup>
 				<col class="area_data_settings_col_tag">
 				<col class="area_data_settings_col_data">
 			</colgroup>
 			<thead>
-				<?php
+<?php
 				html_separator2();
-				html_titleline2(gtext('FRU Information'), 2);
-				?>
+				html_titleline2(gtext('FRU Information'),2);
+?>
+			</thead>
+			<tbody>
+<?php
+				html_text2('sensor',gtext('System Message'),gtext('No IPMI FRU data available.'));
+?>				
+			</tbody>
+		</table>
+<?php
+	else:
+?>
+		<table class="area_data_settings">
+			<colgroup>
+				<col class="area_data_settings_col_tag">
+				<col class="area_data_settings_col_data">
+			</colgroup>
+			<thead>
+<?php
+				html_separator2();
+				html_titleline2(gtext('FRU Information'),2);
+?>
 				<tr>
 					<td class="lhell"><?=gtext('Tag');?></td>
 					<td class="lhebl"><?=gtext('Value');?></td>
 				</tr>
 			</thead>
-			<tfoot>
-			</tfoot>
 			<tbody>
-				<?php foreach ($a_ipmi_fru as $r_ipmi_fru):?>
+<?php
+				foreach ($a_ipmi_fru as $r_ipmi_fru):
+?>
 					<tr>
 						<td class="lcell"><?=htmlspecialchars($r_ipmi_fru[0]);?>&nbsp;</td>
 						<td class="lcebl"><?=htmlspecialchars($r_ipmi_fru[1]);?>&nbsp;</td>
 					</tr>
-				<?php endforeach;?>
+<?php
+				endforeach;
+?>
 			</tbody>
 		</table>
-	<?php endif;?>
-	<div id="submit">
-		<input name="Submit" type="submit" class="formbtn" value="<?=gtext('Refresh');?>"/>
-	</div>
-	<?php include 'formend.inc';?>
-</form></td></tr></tbody></table>
-<?php include 'fend.inc';?>
+<?php 
+	endif;
+	include 'formend.inc';
+?>
+</td></tr></tbody></table>
+<?php
+include 'fend.inc';
+?>
