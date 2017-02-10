b8faaf16342ed3f3f8d5b08d5ce2fca132a5ba38
Michael Schneider <ms49434@users.noreply.github.com>
2017-02-10 22:37:48 +0000

    new framework
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@3853 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/interfaces_bridge.php
+++ www/interfaces_bridge.php
@@ -33,100 +33,179 @@
 */
 require 'auth.inc';
 require 'guiconfig.inc';
-
-$a_bridge = &array_make_branch($config,'vinterfaces','bridge');
-array_sort_key($a_bridge, "if");
+require 'co_sphere.php';
 
 function bridge_inuse($ifn) {
 	global $config, $g;
-
-	if ($config['interfaces']['lan']['if'] === $ifn)
+	if(isset($config['interfaces']['lan']['if']) && ($config['interfaces']['lan']['if'] === $ifn)):
 		return true;
-
-	if (isset($config['interfaces']['wan']) && $config['interfaces']['wan']['if'] === $ifn)
+	endif;
+	if(isset($config['interfaces']['wan']['if']) && ($config['interfaces']['wan']['if'] === $ifn)):
 		return true;
-
-	for ($i = 1; isset($config['interfaces']['opt' . $i]); $i++) {
-		if ($config['interfaces']['opt' . $i]['if'] === $ifn)
+	endif;
+	for($i = 1;isset($config['interfaces']['opt' . $i]['if']);$i++):
+		if($config['interfaces']['opt' . $i]['if'] === $ifn):
 			return true;
-	}
-
+		endif;
+	endfor;
 	return false;
 }
-
-if (isset($_GET['act']) && $_GET['act'] === "del") {
-	if (FALSE === ($cnid = array_search_ex($_GET['uuid'], $config['vinterfaces']['bridge'], "uuid"))) {
-		header("Location: interfaces_bridge.php");
-		exit;
-	}
-
-	$bridge = $a_bridge[$cnid];
-
-	// Check if still in use.
-	if (bridge_inuse($bridge['if'])) {
-		$input_errors[] = gtext("This Bridge cannot be deleted because it is still being used as an interface.");
-	} else {
-		mwexec("/usr/local/sbin/rconf attribute remove 'ifconfig_{$bridge['if']}'");
-
-		unset($a_bridge[$cnid]);
-
-		write_config();
-		touch($d_sysrebootreqd_path);
-
-		header("Location: interfaces_bridge.php");
-		exit;
-	}
+function interfaces_bridge_get_sphere() {
+	global $config;
+	$sphere = new co_sphere_grid('interfaces_bridge','php');
+	$sphere->mod = new co_sphere_scriptname($sphere->basename() . '_edit','php');
+	$sphere->notifier('ifbridge');
+	$sphere->row_identifier('uuid');
+	$sphere->enadis(false);
+	$sphere->lock(false);
+	$sphere->sym_add(gtext('Add Bridge'));
+	$sphere->sym_mod(gtext('Edit Bridge'));
+	$sphere->sym_del(gtext('Bridge is marked for deletion'));
+	$sphere->sym_loc(gtext('Bridge is protected'));
+	$sphere->sym_unl(gtext('Bridge is unlocked'));
+	$sphere->cbm_delete(gtext('Delete Selected Bridges'));
+	$sphere->cbm_delete_confirm(gtext('Do you want to delete selected bridges?'));
+	$sphere->grid = &array_make_branch($config,'vinterfaces','bridge');
+	return $sphere;
 }
+$sphere = &interfaces_bridge_get_sphere();
+array_sort_key($sphere->grid,'if');
+if($_POST):
+	if(isset($_POST['submit'])):
+		switch($_POST['submit']):
+			case 'rows.delete':
+				$sphere->cbm_grid = $_POST[$sphere->cbm_name] ?? [];
+				$updateconfig = false;
+				foreach($sphere->cbm_grid as $sphere->cbm_row):
+					if(false !== ($sphere->row_id = array_search_ex($sphere->cbm_row,$sphere->grid,$sphere->row_identifier()))):
+						$sphere->row = $sphere->grid[$sphere->record_id];
+						//	Check if interface is still in use.
+						if(bridge_inuse($sphere->row['if'])):
+							$input_errors[] = htmlspecialchars($sphere->row['if']) . ': ' . gtext('Bridge cannot be deleted because it is still being used as an interface.');
+						else:
+							$cmd = sprintf('/usr/local/sbin/rconf attribute remove %s',escapeshellarg('ifconfig_' . $sphere->row['if']));
+							mwexec($cmd);
+							unset($sphere->grid[$sphere->row_id]);
+							$updateconfig = true;
+						endif;
+					endif;
+				endforeach;
+				if($updateconfig):
+					write_config();
+					touch($d_sysrebootreqd_path);
+					header($sphere->header());
+					exit;
+				endif;
+				break;
+		endswitch;
+	endif;
+endif;
 $pgtitle = [gtext('Network'),gtext('Interface Management'),gtext('Bridge')];
+include 'fbegin.inc';
+echo $sphere->doj();
 ?>
-<?php include 'fbegin.inc';?>
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr>
-	<td class="tabnavtbl">
-		<ul id="tabnav">
-			<li class="tabinact"><a href="interfaces_assign.php"><span><?=gtext("Management");?></span></a></li>
-			<li class="tabinact"><a href="interfaces_wlan.php"><span><?=gtext("WLAN");?></span></a></li>
-			<li class="tabinact"><a href="interfaces_vlan.php"><span><?=gtext("VLAN");?></span></a></li>
-			<li class="tabinact"><a href="interfaces_lagg.php"><span><?=gtext("LAGG");?></span></a></li>
-			<li class="tabact"><a href="interfaces_bridge.php" title="<?=gtext('Reload page');?>"><span><?=gtext("Bridge");?></span></a></li>
-			<li class="tabinact"><a href="interfaces_carp.php"><span><?=gtext("CARP");?></span></a></li>
-		</ul>
-	</td>
-</tr>
-<tr>
-	<td class="tabcont">
-		<form action="interfaces_bridge.php" method="post">
-			<?php if (!empty($input_errors)) print_input_errors($input_errors);?>
-			<?php if (file_exists($d_sysrebootreqd_path)) print_info_box(get_std_save_message(0));?>
-			<table width="100%" border="0" cellpadding="0" cellspacing="0">
-			<?php html_titleline2(gtext('Overview'), 4);?>
+<table id="area_navigator"><tbody>
+	<tr><td class="tabnavtbl"><ul id="tabnav">
+		<li class="tabinact"><a href="interfaces_assign.php"><span><?=gtext('Management');?></span></a></li>
+		<li class="tabinact"><a href="interfaces_wlan.php"><span><?=gtext('WLAN');?></span></a></li>
+		<li class="tabinact"><a href="interfaces_vlan.php"><span><?=gtext('VLAN');?></span></a></li>
+		<li class="tabinact"><a href="interfaces_lagg.php"><span><?=gtext('LAGG');?></span></a></li>
+		<li class="tabact"><a href="<?=$sphere->scriptname;?>" title="<?=gtext('Reload page');?>"><span><?=gtext('Bridge');?></span></a></li>
+		<li class="tabinact"><a href="interfaces_carp.php"><span><?=gtext('CARP');?></span></a></li>
+	</ul></td></tr>
+</tbody></table>
+<form action="<?=$sphere->scriptname;?>" method="post" name="iform" id="iform"><table id="area_data"><tbody><tr><td id="area_data_frame">
+<?php
+	if(file_exists($d_sysrebootreqd_path)):
+		print_info_box(get_std_save_message(0));
+	endif;
+	if(!empty($input_errors)):
+		print_input_errors($input_errors);
+	endif;
+?>
+	<table class="area_data_selection">
+		<colgroup>
+			<col style="width:5%">
+			<col style="width:20%">
+			<col style="width:20%">
+			<col style="width:45%">
+			<col style="width:10%">
+		</colgroup>
+		<thead>
+<?php
+			html_titleline2(gtext('Overview'),5);
+?>
 			<tr>
-				<td width="20%" class="listhdrlr"><?=gtext("Virtual Interface");?></td>
-				<td width="35%" class="listhdrr"><?=gtext("Member Interface");?></td>
-				<td width="35%" class="listhdrr"><?=gtext("Description");?></td>
-				<td width="10%" class="list"></td>
+				<th class="lhelc"><input type="checkbox" id="togglemembers" name="togglemembers" title="<?=gtext('Invert Selection');?>"/></th>
+				<th class="lhell"><?=gtext('Virtual Interface');?></th>
+				<th class="lhell"><?=gtext('Member Interface');?></th>
+				<th class="lhell"><?=gtext('Description');?></th>
+				<th class="lhebl"><?=gtext('Toolbox');?></th>
 			</tr>
-			<?php foreach ($a_bridge as $bridge):?>
-			<tr>
-				<td class="listlr"><?=htmlspecialchars($bridge['if']);?></td>
-				<td class="listr"><?=htmlspecialchars(implode(" ", !empty($bridge['bridgeif']) ? $bridge['bridgeif'] : []));?></td>
-				<td class="listbg"><?=htmlspecialchars($bridge['desc']);?>&nbsp;</td>
-				<td valign="middle" nowrap="nowrap" class="list">
-					<a href="interfaces_bridge_edit.php?uuid=<?=$bridge['uuid'];?>"><img src="images/edit.png" title="<?=gtext("Edit interface");?>" border="0" alt="<?=gtext("Edit interface");?>" /></a>&nbsp;
-					<a href="interfaces_bridge.php?act=del&amp;uuid=<?=$bridge['uuid'];?>" onclick="return confirm('<?=gtext("Do you really want to delete this interface?");?>')"><img src="images/delete.png" title="<?=gtext("Delete interface");?>" border="0" alt="<?=gtext("Delete interface");?>" /></a>
-				</td>
-			</tr>
-			<?php endforeach;?>
-			<tr>
-				<td class="list" colspan="3">&nbsp;</td>
-				<td class="list">
-					<a href="interfaces_bridge_edit.php"><img src="images/add.png" title="<?=gtext("Add interface");?>" border="0" alt="<?=gtext("Add interface");?>" /></a>
-				</td>
-			</tr>
-			</table>
-		<?php include 'formend.inc';?>
-		</form>
-	</td>
-</tr>
-</table>
-<?php include 'fend.inc';?>
+		</thead>
+		<tbody>
+<?php
+			foreach($sphere->grid as $sphere->row):
+				$notificationmode = false;
+				$notdirty = true;
+				$enabled = $sphere->enadis() ? isset($sphere->row['enable']) : true;
+				$notprotected = $sphere->lock() ? !isset($sphere->row['protected']) : true;
+?>
+				<tr>
+					<td class="<?=$enabled ? "lcelc" : "lcelcd";?>">
+<?php
+						if($notdirty && $notprotected && !bridge_inuse($sphere->row['if'])):
+?>
+							<input type="checkbox" name="<?=$sphere->cbm_name;?>[]" value="<?=$sphere->row[$sphere->row_identifier()];?>" id="<?=$sphere->row[$sphere->row_identifier()];?>"/>
+<?php
+						else:
+?>
+							<input type="checkbox" name="<?=$sphere->cbm_name;?>[]" value="<?=$sphere->row[$sphere->row_identifier()];?>" id="<?=$sphere->row[$sphere->row_identifier()];?>" disabled="disabled"/>
+<?php
+						endif;
+?>
+					</td>
+					<td class="<?=$enabled ? "lcell" : "lcelld";?>"><?=htmlspecialchars($sphere->row['if']);?></td>
+					<td class="<?=$enabled ? "lcell" : "lcelld";?>"><?=htmlspecialchars(implode(' ',!empty($sphere->row['bridgeif']) ? $sphere->row['bridgeif'] : []));?></td>
+					<td class="<?=$enabled ? "lcell" : "lcelld";?>"><?=htmlspecialchars($sphere->row['desc']);?></td>
+					<td class="lcebld">
+						<table class="area_data_selection_toolbox"><colgroup><col style="width:33%"><col style="width:34%"><col style="width:33%"></colgroup><tbody><tr>
+<?php
+							$helpinghand = sprintf('%s?%s=%s',$sphere->mod->scriptname(),$sphere->row_identifier(),$sphere->row[$sphere->row_identifier()]);
+							echo html_row_toolbox($helpinghand,$sphere->sym_mod(),$sphere->sym_del(),$sphere->sym_loc(),$notprotected,$notdirty);
+?>
+							<td></td>
+							<td></td>
+						</tr></tbody></table>
+					</td>
+				</tr>
+<?php
+			endforeach;
+?>
+		</tbody>
+		<tfoot>
+<?php
+			echo html_row_add($sphere->mod->scriptname(),$sphere->sym_add(),5);
+?>
+		</tfoot>
+	</table>
+	<div id="submit">
+<?php
+		if($sphere->enadis()):
+			if($sphere->toggle()):
+				echo html_button_toggle_rows($sphere->cbm_toggle());
+			else:
+				echo html_button_enable_rows($sphere->cbm_enable());
+				echo html_button_disable_rows($sphere->cbm_disable());
+			endif;
+		endif;
+		echo html_button_delete_rows($sphere->cbm_delete());
+?>
+	</div>
+<?php
+	include 'formend.inc';
+?>
+</td></tr></tbody></table></form>
+<?php
+include 'fend.inc';
+?>
