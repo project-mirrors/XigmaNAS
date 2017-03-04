a4a82dd77c2d115ee14e68327792ceb908b23914
Michael Schneider <ms49434@users.noreply.github.com>
2017-03-04 18:41:11 +0000

    fix illegal offset
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@3977 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/system_swap.php
+++ www/system_swap.php
@@ -34,8 +34,7 @@
 require 'auth.inc';
 require 'guiconfig.inc';
 
-$pgtitle = [gtext('System'),gtext('Advanced'),gtext('Swap')];
-
+array_make_branch($config,'system','swap');
 $pconfig['enable'] = isset($config['system']['swap']['enable']);
 $pconfig['type'] = $config['system']['swap']['type'];
 $pconfig['mountpoint'] = !empty($config['system']['swap']['mountpoint']) ? $config['system']['swap']['mountpoint'] : "";
@@ -50,56 +49,51 @@ $pconfig['size'] = !empty($config['system']['swap']['size']) ? $config['system']
 //		$infomsg = sprintf("%s (%s)", gtext("This server uses default swap."), $swapdevice);
 //}
 
-if ($_POST) {
+if($_POST):
 	unset($input_errors);
 	$pconfig = $_POST;
-
-	if (isset($_POST['enable'])) {
+	if(isset($_POST['enable'])):
 		$reqdfields = ['type'];
 		$reqdfieldsn = [gtext('Type')];
 		$reqdfieldst = ['string'];
-
-		if ("device" === $_POST['type']) {
-			$reqdfields = array_merge($reqdfields, ['devicespecialfile']);
-			$reqdfieldsn = array_merge($reqdfieldsn, [gtext('Device')]);
-			$reqdfieldst = array_merge($reqdfieldst, ['string']);
-		} else {
-			$reqdfields = array_merge($reqdfields, ['mountpoint','size']);
-			$reqdfieldsn = array_merge($reqdfieldsn, [gtext('Mount Point'),gtext('Size')]);
-			$reqdfieldst = array_merge($reqdfieldst, ['string','numeric']);
-		}
-
-		do_input_validation($_POST, $reqdfields, $reqdfieldsn, $input_errors);
-		do_input_validation_type($_POST, $reqdfields, $reqdfieldsn, $reqdfieldst, $input_errors);
-	}
-
-	if (empty($input_errors)) {
+		if('device' === $_POST['type']):
+			$reqdfields = array_merge($reqdfields,['devicespecialfile']);
+			$reqdfieldsn = array_merge($reqdfieldsn,[gtext('Device')]);
+			$reqdfieldst = array_merge($reqdfieldst,['string']);
+		else:
+			$reqdfields = array_merge($reqdfields,['mountpoint','size']);
+			$reqdfieldsn = array_merge($reqdfieldsn,[gtext('Mount Point'),gtext('Size')]);
+			$reqdfieldst = array_merge($reqdfieldst,['string','numeric']);
+		endif;
+		do_input_validation($_POST,$reqdfields,$reqdfieldsn,$input_errors);
+		do_input_validation_type($_POST,$reqdfields,$reqdfieldsn,$reqdfieldst,$input_errors);
+	endif;
+	if(empty($input_errors)):
 		$config['system']['swap']['enable'] = isset($_POST['enable']) ? true : false;
 		$config['system']['swap']['type'] = $_POST['type'];
 		$config['system']['swap']['mountpoint'] = $_POST['mountpoint'];
 		$config['system']['swap']['devicespecialfile'] = $_POST['devicespecialfile'];
 		$config['system']['swap']['size'] = $_POST['size'];
-
 		write_config();
-
 		$retval = 0;
-		if (!file_exists($d_sysrebootreqd_path)) {
+		if(!file_exists($d_sysrebootreqd_path)):
 			config_lock();
-			$retval |= rc_update_service("swap");
+			$retval |= rc_update_service('swap');
 			config_unlock();
-			if (isset($_POST['enable']) && (false !== preg_match('/\S/', $_POST['devicespecialfile']))) {
+			if(isset($_POST['enable']) && (false !== preg_match('/\S/',$_POST['devicespecialfile']))):
 				$cmd = sprintf('swapon %s', escapeshellarg($_POST['devicespecialfile']));
 				write_log($cmd);
 				mwexec($cmd);
-			}
-		}
+			endif;
+		endif;
 		$savemsg = get_std_save_message($retval);
-	}
-}
+	endif;
+endif;
+$pgtitle = [gtext('System'),gtext('Advanced'),gtext('Swap')];
+include 'fbegin.inc';
 ?>
-<?php include 'fbegin.inc';?>
 <script type="text/javascript">
-<!--
+//<![CDATA[
 function enable_change(enable_change) {
 	var endis = !(document.iform.enable.checked || enable_change);
 	document.iform.type.disabled = endis;
@@ -107,7 +101,6 @@ function enable_change(enable_change) {
 	document.iform.size.disabled = endis;
 	document.iform.devicespecialfile.disabled = endis;
 }
-
 function type_change() {
 	switch (document.iform.type.value) {
 	case "file":
@@ -123,47 +116,61 @@ function type_change() {
 		break;
 	}
 }
-//-->
+//]]>
 </script>
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
-	<tr>
-	<td class="tabnavtbl">
-		<ul id="tabnav">
-			<li class="tabinact"><a href="system_advanced.php"><span><?=gtext("Advanced");?></span></a></li>
-			<li class="tabinact"><a href="system_email.php"><span><?=gtext("Email");?></span></a></li>
-			<li class="tabinact"><a href="system_email_reports.php"><span><?=gtext("Email Reports");?></span></a></li>
-			<li class="tabinact"><a href="system_monitoring.php"><span><?=gtext("Monitoring");?></span></a></li>
-			<li class="tabact"><a href="system_swap.php" title="<?=gtext('Reload page');?>"><span><?=gtext("Swap");?></span></a></li>
-			<li class="tabinact"><a href="system_rc.php"><span><?=gtext("Command Scripts");?></span></a></li>
-			<li class="tabinact"><a href="system_cron.php"><span><?=gtext("Cron");?></span></a></li>
-			<li class="tabinact"><a href="system_loaderconf.php"><span><?=gtext("loader.conf");?></span></a></li>
-			<li class="tabinact"><a href="system_rcconf.php"><span><?=gtext("rc.conf");?></span></a></li>
-			<li class="tabinact"><a href="system_sysctl.php"><span><?=gtext("sysctl.conf");?></span></a></li>
-			</ul>
-		</td>
-	</tr>
-	<tr>
-		<td class="tabcont">
-			<form action="system_swap.php" method="post" name="iform" id="iform" onsubmit="spinner()">
-				<?php if (!empty($input_errors)) print_input_errors($input_errors); ?>
-				<?php if (!empty($infomsg)) print_info_box($infomsg); ?>
-				<?php if (!empty($savemsg)) print_info_box($savemsg); ?>
-				<table width="100%" border="0" cellpadding="6" cellspacing="0">
-					<?php html_titleline_checkbox("enable", gtext("Swap Memory"), !empty($pconfig['enable']) ? true : false, gtext("Enable"), "enable_change(false)");?>
-					<?php $swapinfo = system_get_swap_info(); if (!empty($swapinfo)):?>
-					<tr>
-					<td width="25%" class="vncellt"><?=gtext("This server uses default swap!");?></td>
-					<td width="75%" style="background-color:#EEEEEE;" class="listr">
+<table id="area_navigator"><tbody>
+	<tr><td class="tabnavtbl"><ul id="tabnav">
+		<li class="tabinact"><a href="system_advanced.php"><span><?=gtext('Advanced');?></span></a></li>
+		<li class="tabinact"><a href="system_email.php"><span><?=gtext('Email');?></span></a></li>
+		<li class="tabinact"><a href="system_email_reports.php"><span><?=gtext('Email Reports');?></span></a></li>
+		<li class="tabinact"><a href="system_monitoring.php"><span><?=gtext('Monitoring');?></span></a></li>
+		<li class="tabact"><a href="system_swap.php" title="<?=gtext('Reload page');?>"><span><?=gtext('Swap');?></span></a></li>
+		<li class="tabinact"><a href="system_rc.php"><span><?=gtext('Command Scripts');?></span></a></li>
+		<li class="tabinact"><a href="system_cron.php"><span><?=gtext('Cron');?></span></a></li>
+		<li class="tabinact"><a href="system_loaderconf.php"><span><?=gtext('loader.conf');?></span></a></li>
+		<li class="tabinact"><a href="system_rcconf.php"><span><?=gtext('rc.conf');?></span></a></li>
+		<li class="tabinact"><a href="system_sysctl.php"><span><?=gtext('sysctl.conf');?></span></a></li>
+	</ul></td></tr>
+</tbody></table>
+<form action="system_swap.php" method="post" name="iform" id="iform" onsubmit="spinner()"><table id="area_data"><tbody><tr><td id="area_data_frame">
+<?php
+	if(!empty($input_errors)):
+		print_input_errors($input_errors);
+	endif;
+	if(!empty($infomsg)):
+		print_info_box($infomsg);
+	endif;
+	if(!empty($savemsg)):
+		print_info_box($savemsg);
+	endif;
+?>
+	<table class="area_data_settings">
+		<colgroup>
+			<col class="area_data_settings_col_tag">
+			<col class="area_data_settings_col_data">
+		</colgroup>
+		<thead>
+<?php
+			html_titleline_checkbox2('enable',gtext('Swap Memory'),!empty($pconfig['enable']) ? true : false,gtext('Enable'),'enable_change(false)');
+?>
+		</thead>
+		<tbody>
+<?php
+			$swapinfo = system_get_swap_info();
+			if (!empty($swapinfo)):
+?>
+				<tr>
+					<td class="area_data_settings_col_tag"><?=gtext('This server uses default swap!');?></td>
+					<td class="area_data_settings_col_data" style="background-color:#EEEEEE;">
 						<table width="100%" border="0" cellspacing="10" cellpadding="1">
-							<?php
-							array_sort_key($swapinfo, "device");
+<?php
+							array_sort_key($swapinfo,'device');
 							$ctrlid = 0;
-							foreach ($swapinfo as $swapk => $swapv) {
+							foreach ($swapinfo as $swapk => $swapv):
 								$ctrlid++;
 								$percent_used = rtrim($swapv['capacity'], "%");
 								$tooltip_used = sprintf(gtext("%sB used of %sB"), $swapv['used'], $swapv['total']);
 								$tooltip_available = sprintf(gtext("%sB available of %sB"), $swapv['avail'], $swapv['total']);
-
 								echo "<tr><td><div id='swapusage'>";
 								echo "<img src='images/bar_left.gif' class='progbarl' alt='' />";
 								echo "<img src='images/bar_blue.gif' name='swapusage_{$ctrlid}_bar_used' id='swapusage_{$ctrlid}_bar_used' width='{$percent_used}' class='progbarcf' title='{$tooltip_used}' alt='' />";
@@ -179,32 +186,40 @@ function type_change() {
 									"<span name='swapusage_{$ctrlid}_used' id='swapusage_{$ctrlid}_used' class='used'>{$swapv['used']}</span>",
 									"<span name='swapusage_{$ctrlid}_free' id='swapusage_{$ctrlid}_free' class='free'>{$swapv['avail']}</span>");
 								echo "</div></td></tr>";
-
-								if ($ctrlid < count($swapinfo))
-										echo "<tr><td><hr size='1' /></td></tr>";
-							}?>
+								if ($ctrlid < count($swapinfo)):
+									echo "<tr><td><hr size='1' /></td></tr>";
+								endif;
+							endforeach;
+?>
 						</table>
 					</td>
 				</tr>
-				<?php endif;?>
-				<tr>
-					<?php html_combobox("type", gtext("Type"), $pconfig['type'], array("file" => gtext("File"), "device" => gtext("Device")), "", true, false, "type_change()");?>
-					<?php html_mountcombobox("mountpoint", gtext("Mount Point"), $pconfig['mountpoint'], gtext("Select mount point where to create the swap file."), true);?>
-					<?php html_inputbox("size", gtext("Size"), $pconfig['size'], gtext("The size of the swap file in MB."), true, 10);?>
-					<?php html_inputbox("devicespecialfile", gtext("Device"), $pconfig['devicespecialfile'], sprintf(gtext("Name of the device to use as swap device, e.g. %s."), "/dev/da0s2b"), true, 20);?>
-				</table>
-				<div id="submit">
-					<input name="Submit" type="submit" class="formbtn" value="<?=gtext("Save");?>" onclick="enable_change(true)" />
-				</div>
-				<?php include 'formend.inc';?>
-			</form>
-		</td>
-	</tr>
-</table>
+<?php
+			endif;
+			$l_type = [
+				'file' => gtext('File'),
+				'device' => gtext('Device')
+			];
+			html_combobox2('type',gtext('Type'),$pconfig['type'],$l_type,'',true,false,'type_change()');
+			html_mountcombobox2('mountpoint',gtext('Mount Point'),$pconfig['mountpoint'],gtext('Select mount point where to create the swap file.'),true);
+			html_inputbox2('size',gtext('Size'),$pconfig['size'],gtext('The size of the swap file in MB.'),true,10);
+			html_inputbox2('devicespecialfile',gtext('Device'),$pconfig['devicespecialfile'],sprintf(gtext('Name of the device to use as swap device, e.g. %s.'),'/dev/da0s2b'),true,20);
+?>
+		</tbody>
+	</table>
+	<div id="submit">
+		<input name="Submit" type="submit" class="formbtn" value="<?=gtext('Save');?>" onclick="enable_change(true)" />
+	</div>
+<?php
+	include 'formend.inc';
+?>
+</td></tr></tbody></table></form>
 <script type="text/javascript">
-<!--
+//<![CDATA[
 enable_change(false);
 type_change(false);
-//-->
+//]]>
 </script>
-<?php include 'fend.inc';?>
+<?php
+include 'fend.inc';
+?>
