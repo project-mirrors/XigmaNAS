e1a5119a44c04b90be4f08cbf73465f0dd60fd1a
Daisuke Aoyama <daoyama@users.noreply.github.com>
2017-07-03 10:45:47 +0000

    support ffmpeg 3.3.x
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/10.3.0.3@4431 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/ports/fuppes/files/extra-patch-src__lib__Common__Common.cpp
+++ build/ports/fuppes/files/extra-patch-src__lib__Common__Common.cpp
@@ -1,5 +1,5 @@
 --- src/lib/Common/Common.cpp.orig	2011-09-22 17:45:44.000000000 +0900
-+++ src/lib/Common/Common.cpp	2014-12-30 06:59:01.000000000 +0900
++++ src/lib/Common/Common.cpp	2017-07-02 12:39:52.815080000 +0900
 @@ -46,6 +46,7 @@
  #include <algorithm>
  #include <cctype>
@@ -8,3 +8,12 @@
  
  #ifndef WIN32
  #include <dlfcn.h>
+@@ -412,7 +413,7 @@
+ 		icv = iconv_open("UTF-8", p_sEncoding.c_str());
+ 	//}
+ 	
+-  if(icv < 0)  
++  if(icv == (iconv_t) -1)  
+     return p_sValue;  
+   
+   size_t nInbytes  = p_sValue.length(); 
