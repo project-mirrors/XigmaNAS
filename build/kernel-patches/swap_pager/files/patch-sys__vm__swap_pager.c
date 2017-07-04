d429bcb92ff86a3f58fd19749321a857ba98254e
Michael Zoon <zoon01@users.noreply.github.com>
2017-07-04 14:57:46 +0000

    prepare trunk for 11.1.0.4 series
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@4455 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/kernel-patches/swap_pager/files/patch-sys__vm__swap_pager.c
+++ build/kernel-patches/swap_pager/files/patch-sys__vm__swap_pager.c
@@ -1,6 +1,6 @@
---- sys/vm/swap_pager.c.orig	2017-06-19 03:40:54.137695000 +0200
-+++ sys/vm/swap_pager.c	2017-06-19 04:27:13.000000000 +0200
-@@ -847,7 +847,7 @@
+--- sys/vm/swap_pager.c.orig	2017-07-02 00:36:55.329851000 +0200
++++ sys/vm/swap_pager.c	2017-07-02 00:52:05.000000000 +0200
+@@ -837,7 +837,7 @@
  	VM_OBJECT_WLOCK(object);
  	while (size) {
  		if (n == 0) {
