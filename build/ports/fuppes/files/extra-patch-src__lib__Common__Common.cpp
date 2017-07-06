e1b0ff5e6c7fb5f60cb2b6b72123cc338b4ab495
Michael Zoon <zoon01@users.noreply.github.com>
2017-07-06 19:10:58 +0000

    same as r4463
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@4464 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/ports/fuppes/files/extra-patch-src__lib__Common__Common.cpp
+++ build/ports/fuppes/files/extra-patch-src__lib__Common__Common.cpp
@@ -1,5 +1,5 @@
---- src/lib/Common/Common.cpp.orig	2011-09-22 10:45:44.000000000 +0200
-+++ src/lib/Common/Common.cpp	2017-07-02 00:33:28.000000000 +0200
+--- src/lib/Common/Common.cpp.orig	2011-09-22 17:45:44.000000000 +0900
++++ src/lib/Common/Common.cpp	2017-07-02 12:39:52.815080000 +0900
 @@ -46,6 +46,7 @@
  #include <algorithm>
  #include <cctype>
@@ -8,18 +8,12 @@
  
  #ifndef WIN32
  #include <dlfcn.h>
-@@ -408,11 +409,11 @@
- 	else {*/
-     if(p_sEncoding.compare("UTF-8") == 0)
-       return p_sValue;
--  
-+
+@@ -412,7 +413,7 @@
  		icv = iconv_open("UTF-8", p_sEncoding.c_str());
  	//}
--	
+ 	
 -  if(icv < 0)  
-+
-+  if(icv == (iconv_t) -1)
++  if(icv == (iconv_t) -1)  
      return p_sValue;  
    
    size_t nInbytes  = p_sValue.length(); 
