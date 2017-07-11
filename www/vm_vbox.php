2bcf2868b0fdf478f9c044142aeeaeda57b9a041
Michael Schneider <ms49434@users.noreply.github.com>
2017-07-11 20:35:18 +0000

    Add option to make USB devices available in vbox.
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@4472 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/vm_vbox.php
+++ www/vm_vbox.php
@@ -37,140 +37,159 @@ require 'guiconfig.inc';
 array_make_branch($config,'vbox');
 $pconfig['enable'] = isset($config['vbox']['enable']);
 $pconfig['homedir'] = $config['vbox']['homedir'] ?? '';
-$vbox_user = rc_getenv_ex("vbox_user", "vboxusers");
-$vbox_group = rc_getenv_ex("vbox_group", "vboxusers");
-
-if ($_POST) {
+$pconfig['allowusb'] = isset($config['vbox']['allowusb']);
+$vbox_user = rc_getenv_ex('vbox_user','vboxusers');
+$vbox_group = rc_getenv_ex('vbox_group','vboxusers');
+if($_POST):
 	unset($input_errors);
 	unset($errormsg);
-
 	$pconfig = $_POST;
-
-	if (isset($_POST['enable'])) {
+	if(isset($_POST['enable'])):
 		$reqdfields = ['homedir'];
 		$reqdfieldsn = [gtext('Home Directory')];
 		$reqdfieldst = ['string'];
-		do_input_validation($_POST, $reqdfields, $reqdfieldsn, $input_errors);
-		do_input_validation_type($_POST, $reqdfields, $reqdfieldsn, $reqdfieldst, $input_errors);
-	} else {
+		do_input_validation($_POST,$reqdfields,$reqdfieldsn,$input_errors);
+		do_input_validation_type($_POST,$reqdfields,$reqdfieldsn,$reqdfieldst,$input_errors);
+	else:
 		// disable VirtualBox
 		config_lock();
-		$retval |= rc_exec_script("/etc/rc.d/vbox onestop");
+		$retval |= rc_exec_script('/etc/rc.d/vbox onestop');
 		config_unlock();
-	}
-
-	if (empty($input_errors)) {
+	endif;
+	if(empty($input_errors)):
 		$config['vbox']['enable'] = isset($_POST['enable']) ? true : false;
 		$config['vbox']['homedir'] = $_POST['homedir'];
-
+		$config['vbox']['allowusb'] = isset($_POST['allowusb']) ? true : false;
 		$dir = $config['vbox']['homedir'];
-		if ($dir == '' || !file_exists($dir))
-			$dir = "/nonexistent";
-
+		if($dir == '' || !file_exists($dir)):
+			$dir = '/nonexistent';
+		endif;
 		// update homedir
 		$user = $vbox_user;
 		$group = $vbox_group;
-		$opt = "-c \"Virtualbox user\" -d \"{$dir}\" -s /usr/sbin/nologin";
-		$index = array_search_ex($user, $config['system']['usermanagement']['user'], "name");
-		if ($index != false) {
+		$opt = sprintf('-c "Virtualbox user" -d "%1$s" -s /usr/sbin/nologin',$dir);
+		$index = array_search_ex($user,$config['system']['usermanagement']['user'],'name');
+		if($index != false):
 			$config['system']['usermanagement']['user'][$index]['extraoptions'] = $opt;
-		}
-
+		endif;
 		write_config();
 		$retval = 0;
 		config_lock();
-		$retval |= rc_exec_service("userdb");
+		$retval |= rc_exec_service('userdb');
 		config_unlock();
-
-		if ($dir != "/nonexistent" && file_exists($dir)) {
+		if($dir != '/nonexistent' && file_exists($dir)):
 			// adjust permission
-			chmod($dir, 0755);
-			chown($dir, $user);
-			chgrp($dir, $group);
-
+			chmod($dir,0755);
+			chown($dir,$user);
+			chgrp($dir,$group);
 			// update auth method
-			$cmd = "/usr/local/bin/sudo -u {$user} /usr/local/bin/VBoxManage setproperty websrvauthlibrary null";
-			mwexec2("$cmd 2>&1", $rawdata, $result);
-
-			if (!file_exists($d_sysrebootreqd_path)) {
+			$cmd = sprintf('/usr/local/bin/sudo -u %1$s /usr/local/bin/VBoxManage setproperty websrvauthlibrary null 2>&1',$user);
+			mwexec2($cmd,$rawdata,$result);
+			if(!file_exists($d_sysrebootreqd_path)):
 				config_lock();
-				$retval |= rc_update_service("vbox");
+				$retval |= rc_update_service('vbox');
 				config_unlock();
-			}
-		}
-
+			endif;
+		endif;
 		$savemsg = get_std_save_message($retval);
-	}
-}
+	endif;
+endif;
 $pgtitle = [gtext('Virtualization'),gtext('VirtualBox')];
+include 'fbegin.inc';
 ?>
-<?php include 'fbegin.inc';?>
-<script type="text/javascript">//<![CDATA[
-$(document).ready(function(){
-	function enable_change(enable_change) {
-		var endis = !($('#enable').prop('checked') || enable_change);
-		$('#homedir').prop('disabled', endis);
-		$('#homedirbrowsebtn').prop('disabled', endis);
-		if (endis) {
-			$('#a_url1').on('click', function(){ return false; });
-			$('#a_url2').on('click', function(){ return false; });
-		} else {
-			$('#a_url1').off('click');
-			$('#a_url2').off('click');
-		}
-	}
-	$('#enable').click(function(){
-		enable_change(false);
-	});
-	$('input:submit').click(function(){
-		enable_change(true);
-	});
-	enable_change(false);
-});
+<script type="text/javascript">
+//<![CDATA[
+$(window).on("load", function() {
+<?php
+	//	Init spinner on submit for id iform.
+?>
+	$("#iform").submit(function() { spinner(); });
+}); 
 //]]>
 </script>
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
-	<tr>
-		<td class="tabcont">
-			<form action="vm_vbox.php" method="post" name="iform" id="iform" onsubmit="spinner()">
-				<?php
-				if(!empty($errormsg)):
-					print_error_box($errormsg);
-				endif;
-				if(!empty($input_errors)):
-					print_input_errors($input_errors);
-				endif;
-				if(!empty($savemsg)):
-					print_info_box($savemsg);
-				endif;
-				$enabled = isset($config['vbox']['enable']);
-				?>
-				<table width="100%" border="0" cellpadding="6" cellspacing="0">
-					<?php
-					html_titleline_checkbox("enable", gtext("VirtualBox"), !empty($pconfig['enable']) ? true : false, gtext("Enable"), "");
-					html_filechooser("homedir", gtext("Home Directory"), $pconfig['homedir'], gtext("Enter the path to the home directory of VirtualBox. VM config and HDD image will be created under the specified directory."), $g['media_path'], false, 60);
-					if ($enabled):
-						html_separator();
-						html_titleline(sprintf("%s (%s)", gtext("Administrative WebGUI"), gtext("phpVirtualBox")));
-						$if = get_ifname($config['interfaces']['lan']['if']);
-						$ipaddr = get_ipaddr($if);
-						$url = htmlspecialchars("/phpvirtualbox/index.html");
-						$text = "<a href='${url}' id='a_url1' target='_blank'>{$url}</a>";
-						html_text("url1", gtext("URL"), $text);
-						html_separator();
-						html_titleline(sprintf("%s (%s)", gtext("Administrative WebGUI"), gtext("noVNC")));
-						$url = htmlspecialchars("/novnc/vnc.html");
-						$text = "<a href='${url}?host=$ipaddr' id='a_url2' target='_blank'>{$url}</a>";
-						html_text("url2", gtext("URL"), $text);
-					endif;
-					?>
-				</table>
-				<div id="submit">
-					<input name="Submit" type="submit" class="formbtn" value="<?=gtext("Save & Restart");?>" />
-				</div>
-				<?php include 'formend.inc';?>
-			</form>
-		</td>
-	</tr>
-</table>
-<?php include 'fend.inc';?>
+<form action="vm_vbox.php" method="post" name="iform" id="iform"><table id="area_data"><tbody><tr><td id="area_data_frame">
+<?php
+	if(!empty($errormsg)):
+		print_error_box($errormsg);
+	endif;
+	if(!empty($input_errors)):
+		print_input_errors($input_errors);
+	endif;
+	if(!empty($savemsg)):
+		print_info_box($savemsg);
+	endif;
+	$enabled = isset($config['vbox']['enable']);
+?>
+	<table class="area_data_settings">
+		<colgroup>
+			<col class="area_data_settings_col_tag">
+			<col class="area_data_settings_col_data">
+		</colgroup>
+		<thead>
+<?php
+			html_titleline_checkbox2('enable',gtext('VirtualBox'),!empty($pconfig['enable']) ? true : false,gtext('Enable'),'');
+?>
+		</thead>
+		<tbody>
+<?php			
+			html_filechooser2('homedir',gtext('Home Directory'),$pconfig['homedir'],gtext('Enter the path to the home directory of VirtualBox. VM config and HDD image will be created under the specified directory.'),$g['media_path'],false,60);
+			$desc = '<strong><font color="red">' . gtext('Security Warning') . '!</font> ' . gtext('Be careful enabling this option.') . '</strong>';
+			html_checkbox2('allowusb',gtext('Allow USB'),!empty($pconfig['allowusb']),gtext('Enable this option to make host USB available to clients.'),$desc);
+?>
+		</tbody>
+	</table>
+<?php			
+	if($enabled):
+?>
+		<table class="area_data_settings">
+			<colgroup>
+				<col class="area_data_settings_col_tag">
+				<col class="area_data_settings_col_data">
+			</colgroup>
+			<thead>
+<?php
+				html_separator2();
+				html_titleline2(sprintf('%s (%s)',gtext('Administrative WebGUI'),gtext('phpVirtualBox')));
+?>
+			</thead>
+			<tbody>
+<?php
+				$url = htmlspecialchars('/phpvirtualbox/index.html');
+				$link = sprintf('<a href="%1$s" id="a_url1" target="_blank">%1$s</a>',$url);
+				html_text2('url1',gtext('URL'),$link);
+?>
+			</tbody>
+		</table>
+		<table class="area_data_settings">
+			<colgroup>
+				<col class="area_data_settings_col_tag">
+				<col class="area_data_settings_col_data">
+			</colgroup>
+			<thead>
+<?php			
+				html_separator2();
+				html_titleline2(sprintf('%s (%s)',gtext('Administrative WebGUI'),gtext('noVNC')));
+?>
+			</thead>
+			<tbody>
+<?php
+				$if = get_ifname($config['interfaces']['lan']['if']);
+				$ipaddr = get_ipaddr($if);
+				$url = htmlspecialchars('/novnc/vnc.html');
+				$link = sprintf('<a href="%1$s?host=%2$s" id="a_url2" target="_blank">%1$s</a>',$url,$ipaddr);
+				html_text2('url2',gtext('URL'),$link);
+?>
+			</tbody>
+		</table>
+<?php
+	endif;
+?>
+	<div id="submit">
+		<input name="Submit" type="submit" class="formbtn" value="<?=gtext('Save');?>"/>
+	</div>
+<?php
+	include 'formend.inc';?>
+</td></tr></tbody></table></form>
+<?php
+include 'fend.inc';
+?>
