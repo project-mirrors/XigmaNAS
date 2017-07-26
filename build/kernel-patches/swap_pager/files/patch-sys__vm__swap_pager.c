6f0571be1dfb55457a1a75a2fc068832c7d8e3fb
Michael Zoon <zoon01@users.noreply.github.com>
2017-07-26 16:20:35 +0000

    update patches
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@4493 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/kernel-patches/swap_pager/files/patch-sys__vm__swap_pager.c
+++ build/kernel-patches/swap_pager/files/patch-sys__vm__swap_pager.c
@@ -1,5 +1,5 @@
---- sys/vm/swap_pager.c.orig	2017-07-02 00:36:55.329851000 +0200
-+++ sys/vm/swap_pager.c	2017-07-02 00:52:05.000000000 +0200
+--- sys/vm/swap_pager.c.orig	2017-07-26 18:05:48.978682000 +0200
++++ sys/vm/swap_pager.c	2017-07-26 18:18:05.000000000 +0200
 @@ -837,7 +837,7 @@
  	VM_OBJECT_WLOCK(object);
  	while (size) {
