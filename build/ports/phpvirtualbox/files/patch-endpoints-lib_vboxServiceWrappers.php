08295f5de676fc7fb7888a0d61655d36f98719fa
Michael Zoon <zoon01@users.noreply.github.com>
2017-08-16 22:30:28 +0000

    update patch
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@4527 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/ports/phpvirtualbox/files/patch-endpoints-lib_vboxServiceWrappers.php
+++ build/ports/phpvirtualbox/files/patch-endpoints-lib_vboxServiceWrappers.php
@@ -1,5 +1,5 @@
---- endpoints/lib/vboxServiceWrappers.php.orig	2017-07-30 19:41:58.570678000 +0200
-+++ endpoints/lib/vboxServiceWrappers.php	2017-08-02 21:20:01.000000000 +0200
+--- endpoints/lib/vboxServiceWrappers.php.orig	2017-08-13 04:21:15.906121000 +0200
++++ endpoints/lib/vboxServiceWrappers.php	2017-08-16 00:58:19.000000000 +0200
 @@ -222,7 +222,7 @@
  {
      protected $_handle;
@@ -48,7 +48,27 @@
          $request->device = $arg_device;
          $response = $this->connection->__soapCall('IMachine_setBootOrder', array((array)$request));
          return ;
-@@ -2623,6 +2632,9 @@
+@@ -1978,6 +1987,9 @@
+         $request->name = $arg_name;
+         $request->controllerPort = $arg_controllerPort;
+         $request->device = $arg_device;
++		if( $arg_nonRotational == null ) {
++			$arg_nonRotational = false;
++		}
+         $request->nonRotational = $arg_nonRotational;
+         $response = $this->connection->__soapCall('IMachine_nonRotationalDevice', array((array)$request));
+         return ;
+@@ -2002,6 +2014,9 @@
+         $request->name = $arg_name;
+         $request->controllerPort = $arg_controllerPort;
+         $request->device = $arg_device;
++		if( $arg_hotPluggable == null ) {
++			$arg_hotPluggable = false;
++		}
+         $request->hotPluggable = $arg_hotPluggable;
+         $response = $this->connection->__soapCall('IMachine_setHotPluggableForDevice', array((array)$request));
+         return ;
+@@ -2623,6 +2638,9 @@
          $request->_this = $this->handle;
          $request->name = $arg_name;
          $request->description = $arg_description;
@@ -58,7 +78,7 @@
          $request->pause = $arg_pause;
          $response = $this->connection->__soapCall('IMachine_takeSnapshot', array((array)$request));
          return array(new IProgress ($this->connection, $response->returnval), (string)$response->id);
-@@ -8869,7 +8881,7 @@
+@@ -8869,7 +8887,7 @@
          return ;
      }
  
