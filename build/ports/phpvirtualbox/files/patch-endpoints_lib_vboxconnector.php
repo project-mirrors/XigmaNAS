7cce11a9a7f12130c704803c6bf679583b1ef385
Michael Zoon <zoon01@users.noreply.github.com>
2017-09-01 18:24:46 +0000

    update patch
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@4551 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/ports/phpvirtualbox/files/patch-endpoints_lib_vboxconnector.php
+++ build/ports/phpvirtualbox/files/patch-endpoints_lib_vboxconnector.php
@@ -1,6 +1,6 @@
---- endpoints/lib/vboxconnector.php.orig	2017-07-27 16:54:58 UTC
-+++ endpoints/lib/vboxconnector.php
-@@ -1126,7 +1126,7 @@ class vboxconnector {
+--- /usr/local/nas4free/svn/build/ports/phpvirtualbox/work/phpvirtualbox-65ebced/endpoints/lib/vboxconnector.php.orig	2017-08-31 00:23:08.867384000 +0200
++++ /usr/local/nas4free/svn/build/ports/phpvirtualbox/work/phpvirtualbox-65ebced/endpoints/lib/vboxconnector.php	2017-09-01 20:15:19.000000000 +0200
+@@ -1126,7 +1126,7 @@
  			// Try to register medium.
  			foreach($checks as $iso) {
  				try {
@@ -9,7 +9,7 @@
  					break;
  				} catch (Exception $e) {
  					// Ignore
-@@ -1358,7 +1358,7 @@ class vboxconnector {
+@@ -1358,7 +1358,7 @@
  			$src = $nsrc->machine;
  		}
  		/* @var $m IMachine */
@@ -18,7 +18,7 @@
  		$sfpath = $m->settingsFilePath;
  
  		/* @var $cm CloneMode */
-@@ -1522,7 +1522,7 @@ class vboxconnector {
+@@ -1522,7 +1522,7 @@
  									$md->releaseRemote();
  								}
  							} else {
@@ -27,7 +27,7 @@
  							}
  						} else {
  							$med = null;
-@@ -1591,7 +1591,7 @@ class vboxconnector {
+@@ -1591,7 +1591,7 @@
  			if($state != 'Saved') {
  
  				// Network properties
@@ -36,7 +36,7 @@
  				$eprops = array_combine($eprops[1],$eprops[0]);
  				$iprops = array_map(create_function('$a','$b=explode("=",$a); return array($b[0]=>$b[1]);'),preg_split('/[\r|\n]+/',$args['networkAdapters'][$i]['properties']));
  				$inprops = array();
-@@ -2028,7 +2028,7 @@ class vboxconnector {
+@@ -2028,7 +2028,7 @@
  						}
  					} else {
  						/* @var $med IMedium */
@@ -45,7 +45,7 @@
  					}
  				} else {
  					$med = null;
-@@ -2111,7 +2111,7 @@ class vboxconnector {
+@@ -2111,7 +2111,7 @@
  			*/
  
  			// Network properties
@@ -54,7 +54,7 @@
  			$eprops = array_combine($eprops[1],$eprops[0]);
  			$iprops = array_map(create_function('$a','$b=explode("=",$a); return array($b[0]=>$b[1]);'),preg_split('/[\r|\n]+/',$args['networkAdapters'][$i]['properties']));
  			$inprops = array();
-@@ -2519,7 +2519,7 @@ class vboxconnector {
+@@ -2519,7 +2519,7 @@
  	 */
  	public function remote_vboxGetEnumerationMap($args) {
  
@@ -63,7 +63,20 @@
  		return (@isset($args['ValueMap']) ? $c->ValueMap : $c->NameMap);
  	}
  
-@@ -3697,7 +3697,7 @@ class vboxconnector {
+@@ -3390,8 +3390,10 @@
+ 		/*
+ 		 * Processors
+ 		 */
+-		for($i = 0; $i < $host->processorCount; $i++) {
+-			$response['cpus'][$i] = $host->getProcessorDescription($i);
++		// TODO https://github.com/phpvirtualbox/phpvirtualbox/issues/53
++		$response['cpus'][0] = $host->getProcessorDescription(0);
++		for($i = 1; $i < $host->processorCount; $i++) {
++			$response['cpus'][$i] = $response['cpus'][0];
+ 		}
+ 
+ 		/*
+@@ -3697,7 +3699,7 @@
  			$hds = array();
  			$delete = $machine->unregister('DetachAllReturnHardDisksOnly');
  			foreach($delete as $hd) {
@@ -72,7 +85,7 @@
  			}
  
  			/* @var $progress IProgress */
-@@ -3772,7 +3772,7 @@ class vboxconnector {
+@@ -3772,7 +3774,7 @@
  			$args['name'] = $_SESSION['user'] . '_' . $args['name'];
  
  		/* Check if file exists */
@@ -81,7 +94,7 @@
  
  		if($this->remote_fileExists(array('file'=>$filename))) {
  			return array('exists' => $filename);
-@@ -3874,7 +3874,7 @@ class vboxconnector {
+@@ -3874,7 +3876,7 @@
  
  				$sc->releaseRemote();
  
@@ -90,7 +103,7 @@
  
  				$this->session->machine->attachDevice(trans($HDbusType,'UIMachineSettingsStorage'),0,0,'HardDisk',$m->handle);
  
-@@ -3941,7 +3941,7 @@ class vboxconnector {
+@@ -3941,7 +3943,7 @@
  			if($at == 'NAT') $nd = $n->NATEngine; /* @var $nd INATEngine */
  			else $nd = null;
  
@@ -99,7 +112,7 @@
  			$props = implode("\n",array_map(create_function('$a,$b','return "$a=$b";'),$props[1],$props[0]));
  
  			$adapters[] = array(
-@@ -4381,7 +4381,7 @@ class vboxconnector {
+@@ -4381,7 +4383,7 @@
  	        }
  
      	    try {
@@ -108,7 +121,7 @@
      	        $response['accepted'][] = $creds['id'];
      		} catch (Exception $e) {
      		    $response['failed'][] = $creds['id'];
-@@ -4690,7 +4690,7 @@ class vboxconnector {
+@@ -4690,7 +4692,7 @@
  			$machine->lockMachine($this->session->handle, ((string)$machine->sessionState == 'Unlocked' ? 'Write' : 'Shared'));
  
  			/* @var $progress IProgress */
@@ -117,7 +130,7 @@
  
  			// Does an exception exist?
  			try {
-@@ -4853,7 +4853,7 @@ class vboxconnector {
+@@ -4853,7 +4855,7 @@
  	    // Connect to vboxwebsrv
  	    $this->connect();
  
@@ -126,7 +139,7 @@
  
  	    $retval = $m->checkEncryptionPassword($args['password']);
  
-@@ -4874,7 +4874,7 @@ class vboxconnector {
+@@ -4874,7 +4876,7 @@
  	    // Connect to vboxwebsrv
  	    $this->connect();
  
@@ -135,7 +148,7 @@
  
  	    /* @var $progress IProgress */
  	    $progress = $m->changeEncryption($args['old_password'],
-@@ -4915,7 +4915,7 @@ class vboxconnector {
+@@ -4915,7 +4917,7 @@
  		// Connect to vboxwebsrv
  		$this->connect();
  
@@ -144,7 +157,7 @@
  
  		/* @var $progress IProgress */
  		$progress = $m->resize($args['bytes']);
-@@ -4953,7 +4953,7 @@ class vboxconnector {
+@@ -4953,7 +4955,7 @@
  		$mid = $target->id;
  
  		/* @var $src IMedium */
@@ -153,7 +166,7 @@
  
  		$type = array(($args['type'] == 'fixed' ? 'Fixed' : 'Standard'));
  		if($args['split']) $type[] = 'VmdkSplit2G';
-@@ -4991,7 +4991,7 @@ class vboxconnector {
+@@ -4991,7 +4993,7 @@
  		$this->connect();
  
  		/* @var $m IMedium */
@@ -162,7 +175,7 @@
  		$m->type = $args['type'];
  		$m->releaseRemote();
  
-@@ -5074,7 +5074,7 @@ class vboxconnector {
+@@ -5074,7 +5076,7 @@
  		// Connect to vboxwebsrv
  		$this->connect();
  
@@ -171,7 +184,7 @@
  
  	}
  
-@@ -5129,7 +5129,7 @@ class vboxconnector {
+@@ -5129,7 +5131,7 @@
  		$this->connect();
  
  		/* @var $m IMedium */
@@ -180,7 +193,7 @@
  		$mediumid = $m->id;
  
  		// connected to...
-@@ -5211,7 +5211,7 @@ class vboxconnector {
+@@ -5211,7 +5213,7 @@
  		if(!$args['type']) $args['type'] = 'HardDisk';
  
  		/* @var $m IMedium */
@@ -189,7 +202,7 @@
  
  		if($args['delete'] && @$this->settings->deleteOnRemove && (string)$m->deviceType == 'HardDisk') {
  
-@@ -5380,7 +5380,7 @@ class vboxconnector {
+@@ -5380,7 +5382,7 @@
  			// Normal medium
  			} else {
  				/* @var $med IMedium */
@@ -198,7 +211,7 @@
  			}
  		}
  
-@@ -5445,7 +5445,7 @@ class vboxconnector {
+@@ -5445,7 +5447,7 @@
  		}
  
  		// For $fixed value
