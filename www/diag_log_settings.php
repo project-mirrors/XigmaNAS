b6d6e406fac47c9e324aac8116366133cf3a8b0c
Michael Schneider <ms49434@users.noreply.github.com>
2017-10-06 21:24:18 +0000

    improve validation
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@4676 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/diag_log_settings.php
+++ www/diag_log_settings.php
@@ -49,13 +49,18 @@ $property = new properties_diag_log_settings();
 $sphere = &get_sphere_diag_log_settings();
 $input_errors = [];
 $savemsg = '';
-//	request method
-$server_request_method = filter_input(INPUT_SERVER,'REQUEST_METHOD',FILTER_VALIDATE_REGEXP,['flags' => FILTER_REQUIRE_SCALAR,'options' => ['default' => NULL,'regexp' => '/^POST$/']]);
+//	determine server request method
+$allowed_srm = ['POST'];
+$allowed_srmr = sprintf('/^(%s)$/',implode('|',$allowed_srm));
+$srm = filter_input(INPUT_SERVER,'REQUEST_METHOD',FILTER_VALIDATE_REGEXP,['flags' => FILTER_REQUIRE_SCALAR,'options' => ['default' => NULL,'regexp' => $allowed_srmr]]);
 //	determine page mode
 $page_mode = PAGE_MODE_VIEW;
-switch($server_request_method):
+switch($srm):
 	case 'POST':
-		$action = filter_input(INPUT_POST,'submit',FILTER_VALIDATE_REGEXP,['flags' => FILTER_REQUIRE_SCALAR,'options' => ['default' => '','regexp' => '/^(edit|save)$/']]);
+		//	determine allowed POST actions
+		$apa = ['edit','save','cancel'];
+		$apar = sprintf('/^(%s)$/',implode('|',$apa));
+		$action = filter_input(INPUT_POST,'submit',FILTER_VALIDATE_REGEXP,['flags' => FILTER_REQUIRE_SCALAR,'options' => ['default' => '','regexp' => $apar]]);
 		switch($action):
 			case 'edit':
 				$page_mode = PAGE_MODE_EDIT;
@@ -70,12 +75,36 @@ switch($page_mode):
 	case PAGE_MODE_VIEW:
 	case PAGE_MODE_EDIT:
 		$sphere->row['reverse'] = $property->reverse->validate_config($sphere->grid);
-		$sphere->row['nentries'] = $property->nentries->validate_array_element($sphere->grid) ?? $property->nentries->get_defaultvalue();
+		$ref = 'nentries';
+		$sphere->row[$ref] = $property->$ref->validate_array_element($sphere->grid);
+		if(is_null($sphere->row[$ref])):
+			$input_errors[] = $property->$ref->get_message_error();
+			if(array_key_exists($ref,$sphere->grid) && is_scalar($sphere->grid[$ref])): 
+				$sphere->row[$ref] = $sphere->grid[$ref];
+			else:
+				$sphere->row[$ref] = $property->$ref->get_defaultvalue();
+			endif;
+		endif;
 		$sphere->row['resolve'] = $property->resolve->validate_config($sphere->grid);
 		$sphere->row['disablecomp'] = $property->disablecomp->validate_config($sphere->grid);
 		$sphere->row['disablesecure'] = $property->disablesecure->validate_config($sphere->grid);
 		$sphere->row['enable'] = $property->enable->validate_config($sphere->grid['remote']);
-		$sphere->row['ipaddr'] = $property->ipaddr->validate_array_element($sphere->grid['remote']) ?? $property->ipaddr->get_defaultvalue();
+		$ref = 'ipaddr';
+		$sphere->row[$ref] = $property->$ref->validate_array_element($sphere->grid['remote']);
+		if(is_null($sphere->row[$ref])):
+			$throw_error = $sphere->row['enable'];
+			if(array_key_exists($ref,$sphere->grid['remote']) && is_string($sphere->grid['remote'][$ref])):
+				$sphere->row[$ref] = $sphere->grid['remote'][$ref];
+				if(preg_match('/\S/',$sphere->grid['remote'][$ref])):
+					$throw_error = true;
+				endif;
+			else:
+				$sphere->row[$ref] = $property->$ref->get_defaultvalue();
+			endif;
+			if($throw_error):
+				$input_errors[] = $property->$ref->get_message_error();
+			endif;
+		endif;
 		$sphere->row['daemon'] = $property->daemon->validate_config($sphere->grid['remote']);
 		$sphere->row['ftp'] = $property->ftp->validate_config($sphere->grid['remote']);
 		$sphere->row['rsyncd'] = $property->rsyncd->validate_config($sphere->grid['remote']);
@@ -85,21 +114,36 @@ switch($page_mode):
 		break;
 	case PAGE_MODE_POST:
 		$sphere->row['reverse'] = $property->reverse->validate_input();
-		$sphere->row['nentries'] = $property->nentries->validate_input();
-		if(is_null($sphere->row['nentries'])):
-			$input_errors[] = $property->nentries->get_message_error();
-			$sphere->row['nentries'] = $_POST['nentries'];
+		$ref = 'nentries';
+		$sphere->row[$ref] = $property->$ref->validate_input();
+		if(is_null($sphere->row[$ref])):
+			$input_errors[] = $property->$ref->get_message_error();
+			if(array_key_exists($ref,$_POST) && is_scalar($_POST[$ref])): 
+				$sphere->row[$ref] = $_POST[$ref];
+			else:
+				$sphere->row[$ref] = $property->$ref->get_defaultvalue();
+			endif;
 		endif;
 		$sphere->row['resolve'] = $property->resolve->validate_input();
 		$sphere->row['disablecomp'] = $property->disablecomp->validate_input();
 		$sphere->row['disablesecure'] = $property->disablesecure->validate_input();
 		$sphere->row['enable'] = $property->enable->validate_input();
-		$sphere->row['ipaddr'] = $property->ipaddr->validate_input();
-		if(is_null($sphere->row['ipaddr'])):
-			if($sphere->row['enable']):
-				$input_errors[] = $property->ipaddr->get_message_error();
+		$ref = 'ipaddr';
+		$sphere->row[$ref] = $property->$ref->validate_input();
+		if(is_null($sphere->row[$ref])):
+			$throw_error = $sphere->row['enable'];
+			if(array_key_exists($ref,$_POST) && is_string($_POST[$ref])):
+				$sphere->row[$ref] = $_POST[$ref];
+				if(preg_match('/\S/',$_POST[$ref])):
+					$throw_error = true;
+				endif;
+			else:
+				$sphere->row[$ref] = $property->$ref->get_defaultvalue();
+				$throw_error = true;
 			endif;
-			$sphere->row['ipaddr'] = $_POST['ipaddr'];
+			if($throw_error):
+				$input_errors[] = $property->$ref->get_message_error();
+			endif;
 		endif;
 		$sphere->row['daemon'] = $property->daemon->validate_input();
 		$sphere->row['ftp'] = $property->ftp->validate_input();
@@ -135,121 +179,91 @@ switch($page_mode):
 		endif;
 		break;
 endswitch;
-//	determine final page mode
+//	determine final page mode and calculate readonly flag
 switch($page_mode):
 	case PAGE_MODE_EDIT:
+		$is_readonly = false;
 		break;
 	default:
 		if(isset($config['system']['skipviewmode']) && (is_bool($config['system']['skipviewmode']) ? $config['system']['skipviewmode'] : true)):
 			$page_mode = PAGE_MODE_EDIT;
+			$is_readonly = false;
 		else:
 			$page_mode = PAGE_MODE_VIEW;
+			$is_readonly = true;
 		endif;
 		break;
 endswitch;
 $jcode = [];
-$jcode[PAGE_MODE_EDIT] = <<<EOJ
-$(window).on("load", function() {
-	$("#enable").on("click",function() { enable_change(false) });
-	$("#button_save").on("click",function() { enable_change(true) });
-	enable_change(false);
-});
-function enable_change(enable_change) {
-	var endis = !(document.iform.enable.checked || enable_change);
-	document.iform.ipaddr.disabled = endis;
-	document.iform.sshd.disabled = endis;
-	document.iform.system.disabled = endis;
-	document.iform.ftp.disabled = endis;
-	document.iform.rsyncd.disabled = endis;
-	document.iform.smartd.disabled = endis;
-	document.iform.daemon.disabled = endis;
-}
-EOJ;
-$jcode[PAGE_MODE_VIEW] = '';
+$jcode[PAGE_MODE_EDIT] = NULL;
+$jcode[PAGE_MODE_VIEW] = NULL;
+//	create document
 $document = new_page([gtext('Diagnostics'),gtext('Log'),gtext('Settings')],$sphere->scriptname());
+//	get areas
 $body = $document->getElementById('main');
 $pagecontent = $document->getElementById('pagecontent');
-if(isset($jcode[$page_mode]) && preg_match('/\S/',$jcode[$page_mode])):
+//	add additional javascript code
+if(isset($jcode[$page_mode])):
 	$body->addJavaScript($jcode[$page_mode]);
 endif;
+//	add tab navigation
 $document->
 	add_area_tabnav()->
 		add_tabnav_upper()->
 			mount_tabnav_record('diag_log.php',gtext('Log'))->
 			mount_tabnav_record('diag_log_settings.php',gtext('Settings'),gtext('Reload page'),true);
+//	create data area
 $content = $pagecontent->add_area_data();
+//	display information, warnings and errors
 $content->
 	mount_input_errors($input_errors)->
 	mount_info_box($savemsg);
+//	add content
+$content->
+	add_table_data_settings()->
+		mount_colgroup_data_settings()->
+		addTHEAD()->
+			c2_titleline(gtext('Log Settings'))->
+			parentNode->
+		addTBODY()->
+			c2_checkbox($property->reverse,$sphere->row['reverse'],false,$is_readonly)->
+			c2_input_text($property->nentries,htmlspecialchars($sphere->row['nentries']),false,$is_readonly,4)->
+			c2_checkbox($property->resolve,$sphere->row['resolve'],false,$is_readonly)->
+			c2_checkbox($property->disablecomp,$sphere->row['disablecomp'],false,$is_readonly)->
+			c2_checkbox($property->disablesecure,$sphere->row['disablesecure'],false,$is_readonly);
+$content->
+	add_table_data_settings()->
+		mount_colgroup_data_settings()->
+		addTHEAD()->
+			c2_separator()->
+			c2_titleline_with_checkbox($property->enable,$sphere->row['enable'],false,$is_readonly)->
+			parentNode->
+		addTBODY()->
+			c2_input_text($property->ipaddr,htmlspecialchars($sphere->row['ipaddr']),false,$is_readonly,60,45)->
+			c2_checkbox($property->system,$sphere->row['system'],false,$is_readonly)->
+			c2_checkbox($property->ftp,$sphere->row['ftp'],false,$is_readonly)->
+			c2_checkbox($property->rsyncd,$sphere->row['rsyncd'],false,$is_readonly)->
+			c2_checkbox($property->sshd,$sphere->row['sshd'],false,$is_readonly)->
+			c2_checkbox($property->smartd,$sphere->row['smartd'],false,$is_readonly)->
+			c2_checkbox($property->daemon,$sphere->row['daemon'],false,$is_readonly);
+//	add buttons
 switch($page_mode):
 	case PAGE_MODE_VIEW:
-		$content->
-			add_table_data_settings()->
-				mount_colgroup_data_settings()->
-				addTHEAD()->
-					mount_titleline(gtext('Log Settings'))->
-					parentNode->
-				addTBODY()->
-					mount_checkbox($property->reverse,$sphere->row['reverse'],false,true)->
-					mount_input_text($property->nentries,htmlspecialchars($sphere->row['nentries']),false,true,4)->
-					mount_checkbox($property->resolve,$sphere->row['resolve'],false,true)->
-					mount_checkbox($property->disablecomp,$sphere->row['disablecomp'],false,true)->
-					mount_checkbox($property->disablesecure,$sphere->row['disablesecure'],false,true);
-		$content->
-			add_table_data_settings()->
-				mount_colgroup_data_settings()->
-				addTHEAD()->
-					mount_separator()->
-					mount_titleline($property->enable->get_title())->
-					parentNode->
-				addTBODY()->
-					mount_textinfo($property->enable->get_id(),gtext('Service Enabled'),$sphere->row['enable'] ? gtext('Yes') : gtext('No'))->
-					mount_input_text($property->ipaddr,htmlspecialchars($sphere->row['ipaddr']),false,true,60,false,45)->
-					mount_checkbox($property->system,$sphere->row['system'],false,true)->
-					mount_checkbox($property->ftp,$sphere->row['ftp'],false,true)->
-					mount_checkbox($property->rsyncd,$sphere->row['rsyncd'],false,true)->
-					mount_checkbox($property->sshd,$sphere->row['sshd'],false,true)->
-					mount_checkbox($property->smartd,$sphere->row['smartd'],false,true)->
-					mount_checkbox($property->daemon,$sphere->row['daemon'],false,true);
 		$document->add_area_buttons()->
 			mount_button_edit();
 		break;
 	case PAGE_MODE_EDIT:
-		$content->
-			add_table_data_settings()->
-				mount_colgroup_data_settings()->
-				addTHEAD()->
-					mount_titleline(gtext('Log Settings'))->
-					parentNode->
-				addTBODY()->
-					mount_checkbox($property->reverse,$sphere->row['reverse'])->
-					mount_input_text($property->nentries,htmlspecialchars($sphere->row['nentries']),false,false,4)->
-					mount_checkbox($property->resolve,$sphere->row['resolve'])->
-					mount_checkbox($property->disablecomp,$sphere->row['disablecomp'])->
-					mount_checkbox($property->disablesecure,$sphere->row['disablesecure']);
-		$content->
-			add_table_data_settings()->
-				mount_colgroup_data_settings()->
-				addTHEAD()->
-					mount_separator()->
-					mount_titleline_with_checkbox($property->enable,$sphere->row['enable'])->
-					parentNode->
-				addTBODY()->
-					mount_input_text($property->ipaddr,htmlspecialchars($sphere->row['ipaddr']),false,false,60,false,45)->
-					mount_checkbox($property->system,$sphere->row['system'])->
-					mount_checkbox($property->ftp,$sphere->row['ftp'])->
-					mount_checkbox($property->rsyncd,$sphere->row['rsyncd'])->
-					mount_checkbox($property->sshd,$sphere->row['sshd'])->
-					mount_checkbox($property->smartd,$sphere->row['smartd'])->
-					mount_checkbox($property->daemon,$sphere->row['daemon']);
 		$document->add_area_buttons()->
 			mount_button_save()->
 			mount_button_cancel();
 		break;
 endswitch;
+//	add remarks
 $content->add_area_remarks()->
 	mount_remark('note',gtext('Note'),sprintf(gtext('Syslog sends UDP datagrams to port 514 on the specified remote syslog server. Be sure to set syslogd on the remote server to accept syslog messages from this server.')));
+//	add auth token
 $content->
 	mount_authtoken();
+//	done
 $document->render();
 ?>
