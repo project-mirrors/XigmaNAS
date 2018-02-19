7ccd51540a8ef7618ce94d5f3244426b9f7b50cb
Michael Schneider <ms49434@users.noreply.github.com>
2018-02-19 22:58:21 +0000

    refactor method names
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@5206 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/interfaces_bridge.php
+++ www/interfaces_bridge.php
@@ -54,8 +54,8 @@ function interfaces_bridge_get_sphere() {
 	global $config;
 	$sphere = new co_sphere_grid('interfaces_bridge','php');
 	$sphere->modify->set_basename($sphere->get_basename() . '_edit');
-	$sphere->notifier('ifbridge');
-	$sphere->row_identifier('uuid');
+	$sphere->set_notifier('ifbridge');
+	$sphere->set_row_identifier('uuid');
 	$sphere->enadis(false);
 	$sphere->lock(false);
 	$sphere->sym_add(gtext('Add Bridge'));
@@ -77,7 +77,7 @@ if($_POST):
 				$sphere->cbm_grid = $_POST[$sphere->cbm_name] ?? [];
 				$updateconfig = false;
 				foreach($sphere->cbm_grid as $sphere->cbm_row):
-					if(false !== ($sphere->row_id = array_search_ex($sphere->cbm_row,$sphere->grid,$sphere->row_identifier()))):
+					if(false !== ($sphere->row_id = array_search_ex($sphere->cbm_row,$sphere->grid,$sphere->get_row_identifier()))):
 						$sphere->row = $sphere->grid[$sphere->record_id];
 						//	Check if interface is still in use.
 						if(bridge_inuse($sphere->row['if'])):
@@ -203,4 +203,3 @@ echo $sphere->doj();
 </td></tr></tbody></table></form>
 <?php
 include 'fend.inc';
-?>
