06e3625220ae4e6d979af19cf4d14cdf392f0e39
Michael Schneider <ms49434@users.noreply.github.com>
2018-03-14 16:49:40 +0000

    remove unused properties and fix volmode property
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@5310 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/properties_disks_zfs_volume.php
+++ etc/inc/properties_disks_zfs_volume.php
@@ -37,25 +37,19 @@ class properties_disks_zfs_volume extends co_property_container {
 	protected $x_uuid;
 	protected $x_enabled;
 	protected $x_protected;
+	protected $x_description;
 	protected $x_toolbox;
 
-	protected $x_available;
 	protected $x_checksum;
 	protected $x_compression;
-	protected $x_copies;
 	protected $x_dedup;
-	protected $x_description;
 	protected $x_logbias;
 	protected $x_name;
 	protected $x_pool;
 	protected $x_primarycache;
-	protected $x_readonly;
-	protected $x_redundant_metadata;
-	protected $x_refreservation;
-	protected $x_reservation;
 	protected $x_secondarycache;
 	protected $x_sparse;
-	protected $x_used;
+	protected $x_sync;
 	protected $x_volblocksize;
 	protected $x_volmode;
 	protected $x_volsize;
@@ -64,28 +58,38 @@ class properties_disks_zfs_volume extends co_property_container {
 		return $this->x_uuid ?? $this->init_uuid();
 	}
 	public function init_uuid() {
-		$this->x_uuid = new property_uuid($this);
-		return $this->x_uuid;
+		$property = $this->x_uuid = new property_uuid($this);
+		return $property;
 	}
-		public function get_enabled() {
+	public function get_enabled() {
 		return $this->x_enabled ?? $this->init_enabled();
 	}
 	public function init_enabled() {
-		$this->x_enabled = new properties_bool($this);
-		$this->x_enabled->
+		$property = $this->x_enabled = new properties_bool($this);
+		$property->
 			set_title(gtext('Enabled'))->
 			set_name('enabled');
-		return $this->x_enabled;
+		return $property;
 	}
 	public function get_protected() {
 		return $this->x_protected ?? $this->init_protected();
 	}
 	public function init_protected() {
-		$this->x_protected = new properties_bool($this);
-		$this->x_protected->
+		$property = $this->x_protected = new properties_bool($this);
+		$property->
 			set_title(gtext('Protected'))->
 			set_name('protected');
-		return $this->x_protected;
+		return $property;
+	}
+	public function get_description() {
+		return $this->x_description ?? $this->init_description();
+	}
+	public function init_description() {
+		$property = $this->x_description = new properties_text($this);
+		$property->
+			set_title(gtext('Description'))->
+			set_name('desc');
+		return $property;
 	}
 	public function get_toolbox() {
 		return $this->x_toolbox ?? $this->init_toolbox();
@@ -94,222 +98,134 @@ class properties_disks_zfs_volume extends co_property_container {
 		$property = $this->x_toolbox = new property_toolbox($this);
 		return $property;
 	}
-	public function get_available() {
-		return $this->x_available ?? $this->init_available();
-	}
-	public function init_available() {
-		$this->x_available = new properties_text($this);
-		$this->x_available->
-			set_title(gtext('Available'))->
-			set_name('avail');
-		return $this->x_available;
-	}
 	public function get_checksum() {
 		return $this->x_checksum ?? $this->init_checksum();
 	}
 	public function init_checksum() {
-		$this->x_checksum = new properties_list($this);
-		$this->x_checksum->
+		$property = $this->x_checksum = new properties_list($this);
+		$property->
 			set_title(gtext('Checksum'))->
 			set_name('checksum');
-		return $this->x_checksum;
+		return $property;
 	}
 	public function get_compression() {
 		return $this->x_compression ?? $this->init_compression();
 	}
 	public function init_compression() {
-		$this->x_compression = new properties_list($this);
-		$this->x_compression->
+		$property = $this->x_compression = new properties_list($this);
+		$property->
 			set_title(gtext('Compression'))->
 			set_name('compression');
-		return $this->x_compression;
-	}
-	public function get_copies() {
-		return $this->x_copies ?? $this->init_copies();
-	}
-	public function init_copies() {
-		$this->x_copies = new properties_list($this);
-		$this->x_copies->
-			set_title(gtext('Copies'))->
-			set_name('copies');
-		return $this->x_copies;
+		return $property;
 	}
 	public function get_dedup() {
 		return $this->x_dedup ?? $this->init_dedup();
 	}
 	public function init_dedup() {
-		$this->x_dedup = new properties_list($this);
-		$this->x_dedup->
+		$property = $this->x_dedup = new properties_list($this);
+		$property->
 			set_title(gtext('Dedup Method'))->
 			set_name('dedup');
-		return $this->x_dedup;
-	}
-	public function get_description() {
-		return $this->x_description ?? $this->init_description();
-	}
-	public function init_description() {
-		$this->x_description = new properties_text($this);
-		$this->x_description->
-			set_title(gtext('Description'))->
-			set_name('desc');
-		return $this->x_description;
+		return $property;
 	}
 	public function get_logbias() {
 		return $this->x_logbias ?? $this->init_logbias();
 	}
 	public function init_logbias() {
-		$this->x_logbias = new properties_list($this);
-		$this->x_logbias->
+		$property = $this->x_logbias = new properties_list($this);
+		$property->
 			set_title(gtext('Logbias'))->
 			set_name('logbias');
-		return $this->x_logbias;
+		return $property;
 	}
 	public function get_name() {
 		return $this->x_name ?? $this->init_name();
 	}
 	public function init_name() {
-		$this->x_name = new properties_text($this);
-		$this->x_name->
+		$property = $this->x_name = new properties_text($this);
+		$property->
 			set_title(gtext('Name'))->
 			set_name('name');
-		return $this->x_name;
+		return $property;
 	}
 	public function get_pool() {
 		return $this->x_pool ?? $this->init_pool();
 	}
 	public function init_pool() {
-		$this->x_pool = new properties_text($this);
-		$this->x_pool->
+		$property = $this->x_pool = new properties_text($this);
+		$property->
 			set_title(gtext('Pool'))->
 			set_name('pool');
-		return $this->x_pool;
+		return $property;
 	}
 	public function get_primarycache() {
 		return $this->x_primarycache ?? $this->init_primarycache();
 	}
 	public function init_primarycache() {
-		$this->x_primarycache = new properties_list($this);
-		$this->x_primarycache->
+		$property = $this->x_primarycache = new properties_list($this);
+		$property->
 			set_title(gtext('Primary Cache'))->
 			set_name('primarycache');
-		return $this->x_primarycache;
-	}
-	public function get_readonly() {
-		return $this->x_readonly ?? $this->init_readonly();
-	}
-	public function init_readonly() {
-		$this->x_readonly = new properties_list($this);
-		$this->x_readonly->
-			set_title(gtext('Read Only'))->
-			set_name('readonly');
-		return $this->x_readonly;
-	}
-	public function get_redundant_metadata() {
-		return $this->x_redundant_metadata ?? $this->init_redundant_metadata();
-	}
-	public function init_redundant_metadata() {
-		$this->x_redundant_metadata = new properties_list($this);
-		$this->x_redundant_metadata->
-			set_title(gtext('Redundant Metadata'))->
-			set_name('redundant_metadata');
-		return $this->x_redundant_metadata;
-	}
-	public function get_refreservation() {
-		return $this->x_refreservation ?? $this->init_refreservation();
-	}
-	public function init_refreservation() {
-		$this->x_refreservation = new properties_text($this);
-		$this->x_refreservation->
-			set_title(gtext('Refreservation'))->
-			set_name('refreservation');
-		return $this->x_refreservation;
-	}
-	public function get_reservation() {
-		return $this->x_reservation ?? $this->init_reservation();
-	}
-	public function init_reservation() {
-		$this->x_reservation = new properties_text($this);
-		$this->x_reservation->
-			set_title(gtext('Reservation'))->
-			set_name('reservation');
-		return $this->x_reservation;
+		return $property;
 	}
 	public function get_secondarycache() {
 		return $this->x_secondarycache ?? $this->init_secondarycache();
 	}
 	public function init_secondarycache() {
-		$this->x_secondarycache = new properties_list($this);
-		$this->x_secondarycache->
+		$property = $this->x_secondarycache = new properties_list($this);
+		$property->
 			set_title(gtext('Secondary Cache'))->
 			set_name('secondarycache');
-		return $this->x_secondarycache;
+		return $property;
 	}
 	public function get_sparse() {
 		return $this->x_sparse ?? $this->init_sparse();
 	}
 	public function init_sparse() {
-		$this->x_sparse = new properties_bool();
-		$this->x_sparse->
+		$property = $this->x_sparse = new properties_bool();
+		$property->
 			set_title(gtext('Sparse'))->
 			set_name('sparse');
-		return $this->x_sparse;
+		return $property;
 	}
 	public function get_sync() {
 		return $this->x_sync ?? $this->init_sync();
 	}
 	public function init_sync() {
-		$this->x_sync = new properties_list($this);
-		$this->x_sync->
+		$property = $this->x_sync = new properties_list($this);
+		$property->
 			set_title(gtext('Sync'))->
 			set_name('sync');
-		return $this->x_sync;
-	}
-	public function get_type() {
-		return $this->x_type ?? $this->init_type();
-	}
-	public function init_type() {
-		$this->x_type = new properties_list($this);
-		$this->x_type->
-			set_title(gtext('Dataset Type'))->
-			set_name('type');
-		return $this->x_type;
-	}
-	public function get_used() {
-		return $this->x_used ?? $this->init_used();
-	}
-	public function init_used() {
-		$this->x_used = new properties_text($this);
-		$this->x_used->
-			set_title(gtext('Used Space'))->
-			set_name('used');
-		return $this->x_used;
+		return $property;
 	}
 	public function get_volblocksize() {
 		return $this->x_volblocksize ?? $this->init_volblocksize();
 	}
 	public function init_volblocksize() {
-		$this->x_volblocksize = new properties_text($this);
-		$this->x_volblocksize->
+		$property = $this->x_volblocksize = new properties_list($this);
+		$property->
 			set_title(gtext('Block Size'))->
 			set_name('volblocksize');
-		return $this->x_volblocksize;
+		return $property;
 	}
-	public function set_volmode() {
-		$this->x_volmode = new properties_list($this);
-		$this->x_volmode->
+	public function get_volmode() {
+		return $this->x_volmode ?? $this->init_volmode($this);
+	}
+	public function init_volmode() {
+		$property = $this->x_volmode = new properties_list($this);
+		$property->
 			set_title(gtext('Volume Mode'))->
 			set_name('volmode');
-		return $this->x_volmode;
+		return $property;
 	}
-
 	public function get_volsize() {
 		return $this->x_volsize ?? $this->init_volsize();
 	}
 	public function init_volsize() {
-		$this->x_volsize = new properties_text($this);
-		$this->x_volsize->
+		$property = $this->x_volsize = new properties_text($this);
+		$property->
 			set_title(gtext('Size'))->
 			set_name('volsize');
-		return $this->x_volsize;
+		return $property;
 	}
 }
