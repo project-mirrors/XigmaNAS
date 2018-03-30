d61f841729609b8971526b3fdb856e120849a74e
Michael Schneider <ms49434@users.noreply.github.com>
2018-03-30 13:27:59 +0000

    call method instead of directly accessing property.
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@5338 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/interfaces_bridge.php
+++ www/interfaces_bridge.php
@@ -74,7 +74,7 @@ if($_POST):
 	if(isset($_POST['submit'])):
 		switch($_POST['submit']):
 			case 'rows.delete':
-				$sphere->cbm_grid = $_POST[$sphere->cbm_name] ?? [];
+				$sphere->cbm_grid = $_POST[$sphere->get_cbm_name()] ?? [];
 				$updateconfig = false;
 				foreach($sphere->cbm_grid as $sphere->cbm_row):
 					if(false !== ($sphere->row_id = array_search_ex($sphere->cbm_row,$sphere->grid,$sphere->get_row_identifier()))):
