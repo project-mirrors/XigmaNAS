11fa043c78ee16fbea0d10584a0d0297d63734ec
Michael Schneider <ms49434@users.noreply.github.com>
2018-04-09 21:36:44 +0000

    Update to G4
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@5384 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/diag_infos_swap.php
+++ www/diag_infos_swap.php
@@ -34,14 +34,33 @@
 require_once 'auth.inc';
 require_once 'guiconfig.inc';
 
-$swapdevice = 'NONE';
-$swapinfo = sprintf('%s/swapdevice',$g['etc_path']);
-if(file_exists($swapinfo)):
-	$swapdevice = trim(file_get_contents($swapinfo));
+function diag_infos_swap_ajax() {
+	global $g,$config;
+	
+	$swapdevice = 'NONE';
+	$swapinfo = sprintf('%s/swapdevice',$g['etc_path']);
+	if(file_exists($swapinfo)):
+		$swapdevice = trim(file_get_contents($swapinfo));
+	endif;
+	if($swapdevice == 'NONE' && !isset($config['system']['swap']['enable'])):
+		return gtext('Swap is disabled.');
+	else:
+		$cmd = '/usr/sbin/swapinfo';
+		unset($rawdata);
+		exec($cmd,$rawdata);
+		return implode(PHP_EOL,$rawdata);
+	endif;
+}
+if(is_ajax()):
+	$status['area_refresh'] = diag_infos_swap_ajax();
+	render_ajax($status);
 endif;
 $pgtitle = [gtext('Diagnostics'),gtext('Information'),gtext('Swap')];
-include 'fbegin.inc';
-$document = new co_DOMDocument();
+$document = new_page($pgtitle);
+//	get areas
+$body = $document->getElementById('main');
+$pagecontent = $document->getElementById('pagecontent');
+//	add tab navigation
 $document->
 	add_area_tabnav()->
 		add_tabnav_upper()->
@@ -62,39 +81,29 @@ $document->
 			ins_tabnav_record('diag_infos_sockets.php',gtext('Sockets'))->
 			ins_tabnav_record('diag_infos_ipmi.php',gtext('IPMI Stats'))->
 			ins_tabnav_record('diag_infos_ups.php',gtext('UPS'));
+$pagecontent->
+	add_area_data()->
+		add_table_data_settings()->
+			push()->
+			ins_colgroup_data_settings()->
+			addTHEAD()->
+				c2_titleline(gtext('Swap Information & Status'))->
+			pop()->
+			addTBODY()->
+				addTR()->
+					insTDwC('celltag',gtext('Information'))->
+					addTDwC('celldata')->
+						addElement('pre',['class' => 'cmdoutput'])->
+							addElement('span',['id' => 'area_refresh'],diag_infos_swap_ajax());
+//	add additional javascript code
+$js_document_ready = <<<'EOJ'
+	var gui = new GUI;
+	gui.recall(5000,5000,'diag_infos_swap.php',null,function(data) {
+		if($('#area_refresh').length > 0) {
+			$('#area_refresh').text(data.area_refresh);
+		}
+	});
+EOJ;
+$body->
+	add_js_document_ready($js_document_ready);
 $document->render();
-?>
-<table id="area_data"><tbody><tr><td id="area_data_frame">
-	<table class="area_data_settings">
-		<colgroup>
-			<col class="area_data_settings_col_tag">
-			<col class="area_data_settings_col_data">
-		</colgroup>
-		<thead>
-<?php
-			html_titleline2(gtext('Swap Information & Status'));
-?>
-		</thead>
-		<tbody>
-			<tr>
-				<td class="celltag"><?=gtext('Information');?></td>
-				<td class="celldata">
-<?php
-					echo '<pre>';
-					if ($swapdevice == "NONE" && !isset($config['system']['swap']['enable'])):
-						echo gtext('Swap is disabled.');
-					else:
-						unset($rawdata);
-						exec('/usr/sbin/swapinfo',$rawdata);
-						echo htmlspecialchars(implode("\n",$rawdata));
-					endif;
-					echo '</pre>';
-?>
-				</td>
-			</tr>
-		</tbody>
-	</table>
-</td></tr></tbody></table>
-<?php
-include 'fend.inc';
-?>
