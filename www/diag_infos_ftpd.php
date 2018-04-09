9d02e95ccbec05d55cd897e68d516e1698481b0f
Michael Schneider <ms49434@users.noreply.github.com>
2018-04-09 22:15:44 +0000

    Update to G4
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@5387 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/diag_infos_ftpd.php
+++ www/diag_infos_ftpd.php
@@ -35,32 +35,27 @@ require_once 'auth.inc';
 require_once 'guiconfig.inc';
 
 function diag_infos_ftpd_ajax() {
-	$cmd = '/usr/local/bin/ftpwho -v';
-	mwexec2($cmd,$rawdata);
-	$rawdata = array_slice($rawdata,1);
-	return implode(PHP_EOL,$rawdata);
+	global $config;
+
+	if(is_bool($test = $config['ftpd']['enable'] ?? false) ? $test : true):
+		$cmd = '/usr/local/bin/ftpwho -v';
+		mwexec2($cmd,$rawdata);
+		$rawdata = array_slice($rawdata,1);
+		return implode(PHP_EOL,$rawdata);
+	else:
+		return gtext('FTP service is disabled.');
+	endif;
 }
 if(is_ajax()):
 	$status['area_refresh'] = diag_infos_ftpd_ajax();
 	render_ajax($status);
 endif;
 $pgtitle = [gtext('Diagnostics'),gtext('Information'),gtext('FTP')];
-include 'fbegin.inc';
-?>
-<script type="text/javascript">
-//<![CDATA[
-$(document).ready(function(){
-	var gui = new GUI;
-	gui.recall(5000, 5000, 'diag_infos_ftpd.php', null, function(data) {
-		if ($('#area_refresh').length > 0) {
-			$('#area_refresh').text(data.area_refresh);
-		}
-	});
-});
-//]]>
-</script>
-<?php
-$document = new co_DOMDocument();
+$document = new_page($pgtitle);
+//	get areas
+$body = $document->getElementById('main');
+$pagecontent = $document->getElementById('pagecontent');
+//	add tab navigation
 $document->
 	add_area_tabnav()->
 		add_tabnav_upper()->
@@ -81,57 +76,30 @@ $document->
 			ins_tabnav_record('diag_infos_sockets.php',gtext('Sockets'))->
 			ins_tabnav_record('diag_infos_ipmi.php',gtext('IPMI Stats'))->
 			ins_tabnav_record('diag_infos_ups.php',gtext('UPS'));
-$document->render();
-?>
-<table id="area_data"><tbody><tr><td id="area_data_frame">
-<?php 
-if (!isset($config['ftpd']['enable'])):
-?>
-	<table class="area_data_settings">
-		<colgroup>
-			<col class="area_data_settings_col_tag">
-			<col class="area_data_settings_col_data">
-		</colgroup>
-		<thead>
-<?php 
-			html_titleline2(gtext('FTP User Information & Status'));
-?>
-		</thead>
-		<tbody><tr>
-			<td class="celltag"><?=gtext('Information');?></td>
-			<td class="celldata">
-<?php
-			echo '<pre>';
-			echo gtext('FTP service is disabled.');
-			echo '</pre>';
-?>
-			</td>
-		</tr></tbody>
-	</table>
-<?php 
-else:
-?>
-	<table class="area_data_settings">
-		<colgroup>
-			<col class="area_data_settings_col_tag">
-			<col class="area_data_settings_col_data">
-		</colgroup>
-		<thead>
-<?php
-			html_titleline2(gtext('FTP User Information & Status'));
-?>
-		</thead>
-		<tbody><tr>
-			<td class="celltag"><?=gtext('Information');?></td>
-			<td class="celldata">
-				<pre><span id="area_refresh"><?=diag_infos_ftpd_ajax();?></span></pre>
-			</td>
-		</tr></tbody>
-	</table>
-<?php
-endif;
-?>
-</td></tr></tbody></table>
-<?php
-include 'fend.inc';
-?>
+$pagecontent->
+	add_area_data()->
+		add_table_data_settings()->
+			push()->
+			ins_colgroup_data_settings()->
+			addTHEAD()->
+				c2_titleline(gtext('FTP User Information & Status'))->
+			pop()->
+			addTBODY()->
+				addTR()->
+					insTDwC('celltag',gtext('Information'))->
+					addTDwC('celldata')->
+						addElement('pre',['class' => 'cmdoutput'])->
+							addElement('span',['id' => 'area_refresh'],diag_infos_ftpd_ajax());
+//	add additional javascript code
+$js_document_ready = <<<'EOJ'
+	var gui = new GUI;
+	gui.recall(5000,5000,'diag_infos_ftpd.php',null,function(data) {
+		if($('#area_refresh').length > 0) {
+			$('#area_refresh').text(data.area_refresh);
+		}
+	});
+EOJ;
+$body->
+	add_js_document_ready($js_document_ready);
+$document->
+	render();
