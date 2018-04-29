b2a6feac784d0683fe4a31691488f951cb00f205
Michael Schneider <ms49434@users.noreply.github.com>
2018-04-29 21:10:05 +0000

    optimizations
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@5460 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/services_ctl_target.php
+++ www/services_ctl_target.php
@@ -83,6 +83,114 @@ function ctl_target_process_updatenotification($mode,$data) {
 	endswitch;
 	return $retval;
 }
+function ctl_target_selection($cop,$sphere) {
+	global $d_sysrebootreqd_path;
+	global $savemsg;
+
+	$input_errors = [];
+	$errormsg = '';
+	$pgtitle = [gtext('Services'),gtext('CAM Target Layer'),gtext('Targets')];
+	$record_exists = count($sphere->grid) > 0;
+	$use_tablesort = count($sphere->grid) > 1;
+	$a_col_width = ['5%','25%','10%','15%','10%','25%','10%'];
+	$n_col_width = count($a_col_width);
+	if($use_tablesort):
+		$document = new_page($pgtitle,$sphere->get_scriptname(),'tablesort');
+	else:
+		$document = new_page($pgtitle,$sphere->get_scriptname());
+	endif;
+	//	get areas
+	$body = $document->getElementById('main');
+	$pagecontent = $document->getElementById('pagecontent');
+	//	add tab navigation
+	$document->
+		add_area_tabnav()->
+			add_tabnav_upper()->
+				ins_tabnav_record('services_ctl.php',gtext('Global Settings'))->
+				ins_tabnav_record('services_ctl_target.php',gtext('Targets'),gtext('Reload page'),true)->
+				ins_tabnav_record('services_ctl_lun.php',gtext('LUNs'))->
+				ins_tabnav_record('services_ctl_portal_group.php',gtext('Portal Groups'))->
+				ins_tabnav_record('services_ctl_auth_group.php',gtext('Auth Groups'));
+	//	create data area
+	$content = $pagecontent->add_area_data();
+	//	display information, warnings and errors
+	$content->
+		ins_input_errors($input_errors)->
+		ins_info_box($savemsg)->
+		ins_error_box($errormsg);
+	if(file_exists($d_sysrebootreqd_path)):
+		$content->ins_info_box(get_std_save_message(0));
+	endif;
+	if(updatenotify_exists($sphere->get_notifier())):
+		$content->ins_config_has_changed_box();
+	endif;
+	//	add content
+	$table = $content->add_table_data_selection();
+	$table->ins_colgroup_with_styles('width',$a_col_width);
+	$thead = $table->addTHEAD();
+	$tbody = $table->addTBODY();
+	$tfoot = $table->addTFOOT();
+	$thead->ins_titleline(gtext('Overview'),$n_col_width);
+	$tr = $thead->addTR();
+	if($record_exists):
+		$tr->
+			push()->
+			addTHwC('lhelc sorter-false parser-false')->
+				ins_cbm_checkbox_toggle($sphere)->
+			pop()->
+			insTHwC('lhell',$cop->get_name()->get_title())->
+			insTHwC('lhelc sorter-false parser-false',gtext('Status'))->
+			insTHwC('lhell',$cop->get_redirect()->get_title())->
+			insTHwC('lhell',$cop->get_alias()->get_title())->
+			insTHwC('lhell',$cop->get_description()->get_title())->
+			insTHwC('lhebl sorter-false parser-false',gtext('Toolbox'));
+	else:
+		$tr->
+			insTHwC('lhelc')->
+			insTHwC('lhell',$cop->get_name()->get_title())->
+			insTHwC('lhelc',gtext('Status'))->
+			insTHwC('lhell',$cop->get_redirect()->get_title())->
+			insTHwC('lhell',$cop->get_alias()->get_title())->
+			insTHwC('lhell',$cop->get_description()->get_title())->
+			insTHwC('lhebl',gtext('Toolbox'));
+	endif;
+	if($record_exists):
+		foreach($sphere->grid as $sphere->row_id => $sphere->row):
+			$notificationmode = updatenotify_get_mode($sphere->get_notifier(),$sphere->get_row_identifier_value());
+			$is_notdirty = (UPDATENOTIFY_MODE_DIRTY != $notificationmode) && (UPDATENOTIFY_MODE_DIRTY_CONFIG != $notificationmode);
+			$is_enabled = $sphere->enadis() ? (is_bool($test = $sphere->row[$cop->get_enable()->get_name()] ?? false) ? $test : true): true;
+			$is_notprotected = $sphere->lock() ? !(is_bool($test = $sphere->row[$cop->get_protected()->get_name()] ?? false) ? $test : true) : true;
+			$dc = $is_enabled ? '' : 'd';
+			$tbody->
+				addTR()->
+					push()->
+					addTDwC('lcelc' . $dc)->
+						ins_cbm_checkbox($sphere,!($is_notdirty && $is_notprotected))->
+					pop()->
+					insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_name()->get_name()] ?? ''))->
+					ins_enadis_icon($is_enabled)->
+					insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_redirect()->get_name()] ?? ''))->
+					insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_alias()->get_name()] ?? ''))->
+					insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_description()->get_name()] ?? ''))->
+					add_toolbox_area()->
+						ins_toolbox($sphere,$is_notprotected,$is_notdirty)->
+						ins_maintainbox($sphere,false)->
+						ins_informbox($sphere,false);
+		endforeach;
+	else:
+		$tbody->ins_no_records_found($n_col_width);
+	endif;
+	$tfoot->ins_record_add($sphere,$n_col_width);
+	$document->
+		add_area_buttons()->
+			ins_cbm_button_enadis($sphere)->
+			ins_cbm_button_delete($sphere);
+	//	additional javascript code
+	$body->addJavaScript($sphere->get_js());
+	$body->add_js_on_load($sphere->get_js_on_load());
+	$body->add_js_document_ready($sphere->get_js_document_ready());
+	$document->render();
+}
 //	init properties and sphere
 $cop = new ctl_target_properties();
 $sphere = &ctl_target_sphere();
@@ -101,6 +209,13 @@ endif;
 $rmo->set_default('GET','view',PAGE_MODE_VIEW);
 list($page_method,$page_action,$page_mode) = $rmo->validate();
 switch($page_method):
+	case 'GET':
+		switch($page_action):
+			case 'view':
+				ctl_target_selection($cop,$sphere);
+				break;
+		endswitch;
+		break;
 	case 'POST':
 		switch($page_action):
 			case 'apply':
@@ -147,125 +262,3 @@ switch($page_method):
 		endswitch;
 		break;
 endswitch;
-$input_errors = [];
-$prerequisites_ok = true;
-$errormsg = '';
-$pgtitle = [gtext('Services'),gtext('CAM Target Layer'),gtext('Targets')];
-$record_exists = count($sphere->grid) > 0;
-$a_col_width = ['5%','25%','10%','15%','10%','25%','10%'];
-$n_col_width = count($a_col_width);
-if($record_exists):
-	$document = new_page($pgtitle,$sphere->get_scriptname(),'tablesort');
-else:
-	$document = new_page($pgtitle,$sphere->get_scriptname());
-endif;
-//	get areas
-$body = $document->getElementById('main');
-$pagecontent = $document->getElementById('pagecontent');
-//	add tab navigation
-$document->
-	add_area_tabnav()->
-		push()->
-		add_tabnav_upper()->
-			ins_tabnav_record('services_ctl.php',gtext('Global Settings'))->
-			ins_tabnav_record('services_ctl_target.php',gtext('Targets'),gtext('Reload page'),true)->
-			ins_tabnav_record('services_ctl_lun.php',gtext('LUNs'))->
-			ins_tabnav_record('services_ctl_portal_group.php',gtext('Portal Groups'))->
-			ins_tabnav_record('services_ctl_auth_group.php',gtext('Auth Groups'));
-//	create data area
-$content = $pagecontent->add_area_data();
-//	display information, warnings and errors
-$content->
-	ins_input_errors($input_errors)->
-	ins_info_box($savemsg)->
-	ins_error_box($errormsg);
-if(file_exists($d_sysrebootreqd_path)):
-	$content->ins_info_box(get_std_save_message(0));
-endif;
-if(updatenotify_exists($sphere->get_notifier())):
-	$content->ins_config_has_changed_box();
-endif;
-//	add content
-$table = $content->add_table_data_selection();
-$table->ins_colgroup_with_styles('width',$a_col_width);
-$thead = $table->addTHEAD();
-$tbody = $table->addTBODY();
-$tfoot = $table->addTFOOT();
-$thead->ins_titleline(gtext('Overview'),$n_col_width);
-$tr = $thead->addTR();
-if($record_exists):
-	$tr->
-		push()->
-		addTHwC('lhelc sorter-false parser-false')->
-			ins_cbm_checkbox_toggle($sphere)->
-		pop()->
-		insTHwC('lhell',$cop->get_name()->get_title())->
-		insTHwC('lhelc sorter-false parser-false',gtext('Status'))->
-		insTHwC('lhell',$cop->get_redirect()->get_title())->
-		insTHwC('lhell',$cop->get_alias()->get_title())->
-		insTHwC('lhell',$cop->get_description()->get_title())->
-		insTHwC('lhebl sorter-false parser-false',gtext('Toolbox'));
-else:
-	$tr->
-		insTHwC('lhelc')->
-		insTHwC('lhell',$cop->get_name()->get_title())->
-		insTHwC('lhelc',gtext('Status'))->
-		insTHwC('lhell',$cop->get_redirect()->get_title())->
-		insTHwC('lhell',$cop->get_alias()->get_title())->
-		insTHwC('lhell',$cop->get_description()->get_title())->
-		insTHwC('lhebl',gtext('Toolbox'));
-endif;
-if($record_exists):
-	$gt_enabled = gtext('Enabled');
-	$gt_disabled = gtext('Disabled');
-	foreach($sphere->grid as $sphere->row_id => $sphere->row):
-		$notificationmode = updatenotify_get_mode($sphere->get_notifier(),$sphere->get_row_identifier_value());
-		$is_notdirty = (UPDATENOTIFY_MODE_DIRTY != $notificationmode) && (UPDATENOTIFY_MODE_DIRTY_CONFIG != $notificationmode);
-		$is_enabled = $sphere->enadis() ? isset($sphere->row[$cop->get_enable()->get_name()]) : true;
-		$is_notprotected = $sphere->lock() ? !$sphere->row[$cop->get_protected()->get_name()] : true;
-		if($is_enabled):
-			$src = $g_img['ena'];
-			$title = $gt_enabled;
-			$dc = '';
-		else:
-			$src = $g_img['dis'];
-			$title = $gt_disabled;
-			$dc = 'd';
-		endif;
-		$tbody->
-			addTR()->
-				push()->
-				addTDwC('lcelc' . $dc)->
-					ins_cbm_checkbox($sphere,!($is_notdirty && $is_notprotected))->
-				pop()->
-				insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_name()->get_name()] ?? ''))->
-				push()->
-				addTDwC('lcelc' . $dc)->
-					addA(['title' => $title])->
-						insIMG(['src' => $src,'alt' => ''])->
-				pop()->
-				insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_redirect()->get_name()] ?? ''))->
-				insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_alias()->get_name()] ?? ''))->
-				insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_description()->get_name()] ?? ''))->
-				add_toolbox_area()->
-					ins_toolbox($sphere,$is_notprotected,$is_notdirty)->
-					ins_maintainbox($sphere,false)->
-					ins_informbox($sphere,false);
-	endforeach;
-else:
-	$tbody->ins_no_records_found($n_col_width);
-endif;
-$tfoot->ins_record_add($sphere,$n_col_width);
-$document->
-	add_area_buttons()->
-		ins_cbm_button_enadis($sphere)->
-		ins_cbm_button_delete($sphere);
-//	additional javascript code
-$body->
-	addJavaScript($sphere->get_js());
-$body->
-	add_js_on_load($sphere->get_js_on_load());
-$body->
-	add_js_document_ready($sphere->get_js_document_ready());
-$document->
-	render();
