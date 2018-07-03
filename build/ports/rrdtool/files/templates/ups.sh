7c6730a76f4da4585e14fce24ed1179bc1f178c9
Michael Zoon <zoon01@users.noreply.github.com>
2018-07-03 22:10:33 +0000

    Change ups graph battery.voltage to output.voltage. battery.voltage not working for all brands.
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@5682 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/ports/rrdtool/files/templates/ups.sh
+++ build/ports/rrdtool/files/templates/ups.sh
@@ -1,4 +1,4 @@
-# DS: $charge:$load:$bvoltage:$ivoltage:$runtime:$OL:$OF:$OB:$CG
+# DS: $charge:$load:$ovoltage:$ivoltage:$runtime:$OL:$OF:$OB:$CG
 /usr/local/bin/rrdtool graph $WORKING_DIR/rrd/rrd-${GRAPH}_${GRAPH_NAME}.png \
 "-v UPS usage" \
 "-s" "$START_TIME" \
@@ -11,7 +11,7 @@ $BACKGROUND \
 "--alt-autoscale-max" \
 "DEF:charge=$STORAGE_PATH/rrd/ups.rrd:charge:AVERAGE" \
 "DEF:load=$STORAGE_PATH/rrd/ups.rrd:load:AVERAGE" \
-"DEF:bvoltage=$STORAGE_PATH/rrd/ups.rrd:bvoltage:AVERAGE" \
+"DEF:ovoltage=$STORAGE_PATH/rrd/ups.rrd:ovoltage:AVERAGE" \
 "DEF:ivoltage=$STORAGE_PATH/rrd/ups.rrd:ivoltage:AVERAGE" \
 "DEF:runtime=$STORAGE_PATH/rrd/ups.rrd:runtime:AVERAGE" \
 "DEF:OL=$STORAGE_PATH/rrd/ups.rrd:OL:AVERAGE" \
@@ -24,12 +24,6 @@ $BACKGROUND \
 "GPRINT:charge:AVERAGE:Avg\\:%6.1lf" \
 "GPRINT:charge:LAST:Last\\:%6.1lf" \
 "COMMENT:\n" \
-"LINE2:bvoltage#0000A0:Battery voltage     [V]" \
-"GPRINT:bvoltage:MIN:Min\\:%6.1lf" \
-"GPRINT:bvoltage:MAX:Max\\:%6.1lf" \
-"GPRINT:bvoltage:AVERAGE:Avg\\:%6.1lf" \
-"GPRINT:bvoltage:LAST:Last\\:%6.1lf" \
-"COMMENT:\n" \
 "LINE1:load#FF0000:Battery load        [%]" \
 "GPRINT:load:MIN:Min\\:%6.1lf" \
 "GPRINT:load:MAX:Max\\:%6.1lf" \
@@ -41,6 +35,12 @@ $BACKGROUND \
 "GPRINT:runtime:MAX:Max\\:%6.1lf" \
 "GPRINT:runtime:AVERAGE:Avg\\:%6.1lf" \
 "GPRINT:runtime:LAST:Last\\:%6.1lf" \
+"COMMENT:\n" \
+"LINE2:ovoltage#0000A0:Output voltage     [V]" \
+"GPRINT:ovoltage:MIN:Min\\:%6.1lf" \
+"GPRINT:ovoltage:MAX:Max\\:%6.1lf" \
+"GPRINT:ovoltage:AVERAGE:Avg\\:%6.1lf" \
+"GPRINT:ovoltage:LAST:Last\\:%6.1lf" \
 "COMMENT:\n" \
 "LINE1:ivoltage#5050F0:Input voltage       [V]" \
 "GPRINT:ivoltage:MIN:Min\\:%6.1lf" \
