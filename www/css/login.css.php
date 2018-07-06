41669639798af7d6924ba3a1486fd485c58632bb
Michael Schneider <ms49434@users.noreply.github.com>
2018-07-06 15:34:05 +0000

    prepare for custom css
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@5706 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

---
+++ www/css/login.css.php
@@ -0,0 +1,64 @@
+<?php
+/*
+	login.css.php
+
+	Part of XigmaNAS (http://www.xigmanas.com).
+	Copyright (c) 2018 The XigmaNAS Project <info@xigmanas.com>.
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice, this
+	   list of conditions and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the above copyright notice,
+	   this list of conditions and the following disclaimer in the documentation
+	   and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+	The views and conclusions contained in the software and documentation are those
+	of the authors and should not be interpreted as representing official policies,
+	either expressed or implied, of the XigmaNAS Project.
+*/
+require_once 'config.inc';
+
+header('Content-type: text/css');
+echo '/*',PHP_EOL;
+$config_subkey = 'cssloginfile';
+$css_default_filename = sprintf('%1$s/css/%2$s',$g['www_path'],'login.css');
+$webgui_settings = &array_make_branch($config,'system','webgui');
+$css_override_filename = filter_var($webgui_settings[$config_subkey],FILTER_VALIDATE_REGEXP,['flags' => FILTER_REQUIRE_SCALAR,'options' => ['default' => NULL,'regexp' => '/\S/']]);
+$css_content = NULL;
+if(is_null($css_content) && isset($css_override_filename) && file_exists($css_override_filename)):
+	$css_content = file_get_contents($css_override_filename);
+	if(false === $css_content):
+		$css_content = NULL;
+	else:
+		echo "\t",'Using custom CSS file.',PHP_EOL;
+	endif;
+endif;
+if(is_null($css_content) && isset($css_default_filename) && file_exists($css_default_filename)):
+	$css_content = file_get_contents($css_default_filename);
+	if(false === $css_content):
+		$css_content = NULL;
+	else:
+		echo "\t",'Using default CSS file.',PHP_EOL;
+	endif;
+endif;
+if(is_null($css_content)):
+	$css_content = '';
+	echo "\t",'No CSS file found.',PHP_EOL;
+endif;
+echo '*/',PHP_EOL;
+echo $css_content;
