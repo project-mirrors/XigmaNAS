e14961918ea4307ea2d4d62f61abd928d6a210e9
Michael Schneider <ms49434@users.noreply.github.com>
2018-08-11 13:40:42 +0000

    gettext changes
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@5812 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/diag_infos_iscsi.php
+++ www/diag_infos_iscsi.php
@@ -41,23 +41,23 @@ $document = new co_DOMDocument();
 $document->
 	add_area_tabnav()->
 		add_tabnav_upper()->
-			ins_tabnav_record('diag_infos_disks.php',gtext('Disks'))->
-			ins_tabnav_record('diag_infos_disks_info.php',gtext('Disks (Info)'))->
-			ins_tabnav_record('diag_infos_part.php',gtext('Partitions'))->
-			ins_tabnav_record('diag_infos_smart.php',gtext('S.M.A.R.T.'))->
-			ins_tabnav_record('diag_infos_space.php',gtext('Space Used'))->
-			ins_tabnav_record('diag_infos_swap.php',gtext('Swap'))->
-			ins_tabnav_record('diag_infos_mount.php',gtext('Mounts'))->
-			ins_tabnav_record('diag_infos_raid.php',gtext('Software RAID'))->
-			ins_tabnav_record('diag_infos_iscsi.php',gtext('iSCSI Initiator'),gtext('Reload page'),true)->
-			ins_tabnav_record('diag_infos_ad.php',gtext('MS Domain'))->
-			ins_tabnav_record('diag_infos_samba.php',gtext('CIFS/SMB'))->
-			ins_tabnav_record('diag_infos_ftpd.php',gtext('FTP'))->
-			ins_tabnav_record('diag_infos_rsync_client.php',gtext('RSYNC Client'))->
-			ins_tabnav_record('diag_infos_netstat.php',gtext('Netstat'))->
-			ins_tabnav_record('diag_infos_sockets.php',gtext('Sockets'))->
-			ins_tabnav_record('diag_infos_ipmi.php',gtext('IPMI Stats'))->
-			ins_tabnav_record('diag_infos_ups.php',gtext('UPS'));
+			ins_tabnav_record('diag_infos_disks.php',gettext('Disks'))->
+			ins_tabnav_record('diag_infos_disks_info.php',gettext('Disks (Info)'))->
+			ins_tabnav_record('diag_infos_part.php',gettext('Partitions'))->
+			ins_tabnav_record('diag_infos_smart.php',gettext('S.M.A.R.T.'))->
+			ins_tabnav_record('diag_infos_space.php',gettext('Space Used'))->
+			ins_tabnav_record('diag_infos_swap.php',gettext('Swap'))->
+			ins_tabnav_record('diag_infos_mount.php',gettext('Mounts'))->
+			ins_tabnav_record('diag_infos_raid.php',gettext('Software RAID'))->
+			ins_tabnav_record('diag_infos_iscsi.php',gettext('iSCSI Initiator'),gettext('Reload page'),true)->
+			ins_tabnav_record('diag_infos_ad.php',gettext('MS Domain'))->
+			ins_tabnav_record('diag_infos_samba.php',gettext('CIFS/SMB'))->
+			ins_tabnav_record('diag_infos_ftpd.php',gettext('FTP'))->
+			ins_tabnav_record('diag_infos_rsync_client.php',gettext('RSYNC Client'))->
+			ins_tabnav_record('diag_infos_netstat.php',gettext('Netstat'))->
+			ins_tabnav_record('diag_infos_sockets.php',gettext('Sockets'))->
+			ins_tabnav_record('diag_infos_ipmi.php',gettext('IPMI Stats'))->
+			ins_tabnav_record('diag_infos_ups.php',gettext('UPS'));
 $document->render();
 ?>
 <table id="area_data"><tbody><tr><td id="area_data_frame">
@@ -71,7 +71,7 @@ if(0 >= count($a_disk)):
 		</colgroup>
 		<thead>
 <?php
-			html_titleline2(gtext('iSCSI Targets Information & Status'));
+			html_titleline2(gettext('iSCSI Targets Information & Status'));
 ?>
 		</thead>
 		<tbody><tr>
@@ -95,7 +95,7 @@ else:
 		</colgroup>
 		<thead>
 <?php
-			html_titleline2(gtext('iSCSI Targets Information & Status'));
+			html_titleline2(gettext('iSCSI Targets Information & Status'));
 ?>
 		</thead>
 		<tbody><tr>
@@ -104,14 +104,14 @@ else:
 <?php
 				echo '<pre>';
 				foreach($a_disk as $r_disk):
-					echo htmlspecialchars(sprintf(gtext('Discovered iSCSI target for %s'),$r_disk['targetaddress']));
+					echo sprintf(gtext('Discovered iSCSI target for %s'),htmlspecialchars($r_disk['targetaddress']));
 					echo '<br />';
 					unset($rawdata);
 					$cmd = '/sbin/iscontrol';
 					$cmd .= ' -d targetaddress=' . $r_disk['targetaddress'];
 					$cmd .= ' initiatorname=' . $r_disk['initiatorname'];
 					exec($cmd,$rawdata);
-					echo htmlspecialchars(implode("\n",$rawdata));
+					echo htmlspecialchars(implode(PHP_EOL,$rawdata));
 					unset($rawdata);
 				endforeach;
 				echo '</pre>';
