9b94a84f31631b81fd183dea4cd146db9d4bc780
Michael Schneider <ms49434@users.noreply.github.com>
2018-08-19 22:39:16 +0000

    remove conversion
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@5903 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/disks_zfs_dataset.php
+++ www/disks_zfs_dataset.php
@@ -222,10 +222,10 @@ if($record_exists):
 				addTDwC('lcelc' . $dc)->
 					ins_cbm_checkbox($sphere,!($is_notdirty && $is_notprotected))->
 				pop()->
-				insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_pool()->get_name()][0] ?? ''))->
-				insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_name()->get_name()] ?? ''))->
-				insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_compression()->get_name()] ?? ''))->
-				insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_description()->get_name()] ?? ''))->
+				insTDwC('lcell' . $dc,$sphere->row[$cop->get_pool()->get_name()][0] ?? '')->
+				insTDwC('lcell' . $dc,$sphere->row[$cop->get_name()->get_name()] ?? '')->
+				insTDwC('lcell' . $dc,$sphere->row[$cop->get_compression()->get_name()] ?? '')->
+				insTDwC('lcell' . $dc,$sphere->row[$cop->get_description()->get_name()] ?? '')->
 				add_toolbox_area()->
 					ins_toolbox($sphere,$is_notprotected,$is_notdirty)->
 					ins_maintainbox($sphere,false)->
