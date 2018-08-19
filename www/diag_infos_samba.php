e79058ab802cbcdcd992ad73065b5add7a71ab80
Michael Schneider <ms49434@users.noreply.github.com>
2018-08-19 11:01:59 +0000

    gettext changes
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@5866 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/diag_infos_samba.php
+++ www/diag_infos_samba.php
@@ -73,7 +73,7 @@ if(is_ajax()):
 	$status['area_refresh_3'] = diag_infos_samba_3_ajax();
 	render_ajax($status);
 endif;
-$pgtitle = [gtext('Diagnostics'),gtext('Information'),gtext('CIFS/SMB')];
+$pgtitle = [gettext('Diagnostics'),gettext('Information'),gettext('CIFS/SMB')];
 $document = new_page($pgtitle);
 //	get areas
 $body = $document->getElementById('main');
@@ -82,23 +82,23 @@ $pagecontent = $document->getElementById('pagecontent');
 $document->
 	add_area_tabnav()->
 		add_tabnav_upper()->
-			ins_tabnav_record('diag_infos_disks.php',gtext('Disks'))->
-			ins_tabnav_record('diag_infos_disks_info.php',gtext('Disks (Info)'))->
-			ins_tabnav_record('diag_infos_part.php',gtext('Partitions'))->
-			ins_tabnav_record('diag_infos_smart.php',gtext('S.M.A.R.T.'))->
-			ins_tabnav_record('diag_infos_space.php',gtext('Space Used'))->
-			ins_tabnav_record('diag_infos_swap.php',gtext('Swap'))->
-			ins_tabnav_record('diag_infos_mount.php',gtext('Mounts'))->
-			ins_tabnav_record('diag_infos_raid.php',gtext('Software RAID'))->
-			ins_tabnav_record('diag_infos_iscsi.php',gtext('iSCSI Initiator'))->
-			ins_tabnav_record('diag_infos_ad.php',gtext('MS Domain'))->
-			ins_tabnav_record('diag_infos_samba.php',gtext('CIFS/SMB'),gtext('Reload page'),true)->
-			ins_tabnav_record('diag_infos_ftpd.php',gtext('FTP'))->
-			ins_tabnav_record('diag_infos_rsync_client.php',gtext('RSYNC Client'))->
-			ins_tabnav_record('diag_infos_netstat.php',gtext('Netstat'))->
-			ins_tabnav_record('diag_infos_sockets.php',gtext('Sockets'))->
-			ins_tabnav_record('diag_infos_ipmi.php',gtext('IPMI Stats'))->
-			ins_tabnav_record('diag_infos_ups.php',gtext('UPS'));
+			ins_tabnav_record('diag_infos_disks.php',gettext('Disks'))->
+			ins_tabnav_record('diag_infos_disks_info.php',gettext('Disks (Info)'))->
+			ins_tabnav_record('diag_infos_part.php',gettext('Partitions'))->
+			ins_tabnav_record('diag_infos_smart.php',gettext('S.M.A.R.T.'))->
+			ins_tabnav_record('diag_infos_space.php',gettext('Space Used'))->
+			ins_tabnav_record('diag_infos_swap.php',gettext('Swap'))->
+			ins_tabnav_record('diag_infos_mount.php',gettext('Mounts'))->
+			ins_tabnav_record('diag_infos_raid.php',gettext('Software RAID'))->
+			ins_tabnav_record('diag_infos_iscsi.php',gettext('iSCSI Initiator'))->
+			ins_tabnav_record('diag_infos_ad.php',gettext('MS Domain'))->
+			ins_tabnav_record('diag_infos_samba.php',gettext('CIFS/SMB'),gettext('Reload page'),true)->
+			ins_tabnav_record('diag_infos_ftpd.php',gettext('FTP'))->
+			ins_tabnav_record('diag_infos_rsync_client.php',gettext('RSYNC Client'))->
+			ins_tabnav_record('diag_infos_netstat.php',gettext('Netstat'))->
+			ins_tabnav_record('diag_infos_sockets.php',gettext('Sockets'))->
+			ins_tabnav_record('diag_infos_ipmi.php',gettext('IPMI Stats'))->
+			ins_tabnav_record('diag_infos_ups.php',gettext('UPS'));
 $pagecontent->
 	add_area_data()->
 		push()->
@@ -106,42 +106,42 @@ $pagecontent->
 			push()->
 			ins_colgroup_data_settings()->
 			addTHEAD()->
-				c2_titleline(gtext('Active Users'))->
+				c2_titleline(gettext('Active Users'))->
 			pop()->
 			addTBODY()->
 				addTR()->
-					insTDwC('celltag',gtext('Information'))->
+					insTDwC('celltag',gettext('Information'))->
 					addTDwC('celldata')->
 						addElement('pre',['class' => 'cmdoutput'])->
-							addElement('span',['id' => 'area_refresh_1'],htmlspecialchars(diag_infos_samba_1_ajax()))->
+							addElement('span',['id' => 'area_refresh_1'],diag_infos_samba_1_ajax())->
 		last()->
 		add_table_data_settings()->
 			push()->
 			ins_colgroup_data_settings()->
 			addTHEAD()->
 				c2_separator()->
-				c2_titleline(gtext('Active Shares'))->
+				c2_titleline(gettext('Active Shares'))->
 			pop()->
 			addTBODY()->
 				addTR()->
-					insTDwC('celltag',gtext('Information'))->
+					insTDwC('celltag',gettext('Information'))->
 					addTDwC('celldata')->
 						addElement('pre',['class' => 'cmdoutput'])->
-							addElement('span',['id' => 'area_refresh_2'],htmlspecialchars(diag_infos_samba_2_ajax()))->
+							addElement('span',['id' => 'area_refresh_2'],diag_infos_samba_2_ajax())->
 		pop()->
 		add_table_data_settings()->
 			push()->
 			ins_colgroup_data_settings()->
 			addTHEAD()->
 				c2_separator()->
-				c2_titleline(gtext('Locked Files'))->
+				c2_titleline(gettext('Locked Files'))->
 			pop()->
 			addTBODY()->
 				addTR()->
-					insTDwC('celltag',gtext('Information'))->
+					insTDwC('celltag',gettext('Information'))->
 					addTDwC('celldata')->
 						addElement('pre',['class' => 'cmdoutput'])->
-							addElement('span',['id' => 'area_refresh_3'],htmlspecialchars(diag_infos_samba_3_ajax()));
+							addElement('span',['id' => 'area_refresh_3'],diag_infos_samba_3_ajax());
 //	add additional javascript code
 $js_document_ready = <<<'EOJ'
 	var gui = new GUI;
@@ -157,7 +157,5 @@ $js_document_ready = <<<'EOJ'
 		}
 	});
 EOJ;
-	$body->
-		add_js_document_ready($js_document_ready);
-$document->
-	render();
+$body->add_js_document_ready($js_document_ready);
+$document->render();
