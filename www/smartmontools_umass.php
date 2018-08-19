14d521330cc42cfad3d2cf297e4e857d2bb9c156
Michael Schneider <ms49434@users.noreply.github.com>
2018-08-19 22:40:03 +0000

    remove conversion
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@5904 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/smartmontools_umass.php
+++ www/smartmontools_umass.php
@@ -170,10 +170,10 @@ function smartmontools_umass_selection($cop,$sphere) {
 					addTDwC('lcelc' . $dc)->
 						ins_cbm_checkbox($sphere,!($is_notdirty && $is_notprotected))->
 					pop()->
-					insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_name()->get_name()] ?? ''))->
-					insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_type()->get_name()] ?? ''))->
+					insTDwC('lcell' . $dc,$sphere->row[$cop->get_name()->get_name()] ?? '')->
+					insTDwC('lcell' . $dc,$sphere->row[$cop->get_type()->get_name()] ?? '')->
 					ins_enadis_icon($is_enabled)->
-					insTDwC('lcell' . $dc,htmlspecialchars($sphere->row[$cop->get_description()->get_name()] ?? ''))->
+					insTDwC('lcell' . $dc,$sphere->row[$cop->get_description()->get_name()] ?? '')->
 					add_toolbox_area()->
 						ins_toolbox($sphere,$is_notprotected,$is_notdirty)->
 						ins_maintainbox($sphere,false)->
