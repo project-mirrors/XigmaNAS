259d15d153b19cf0e9dcec2a3897b8cb1585f526
Michael Zoon <zoon01@users.noreply.github.com>
2018-08-23 18:58:36 +0000

    updates for samba 4.8.4
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@5924 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/make.sh
+++ build/make.sh
@@ -1842,12 +1842,17 @@ copy_files() {
 			echo "===> Copy istgt-20180521.tar.gz done!"
 			cp -f ${XIGMANAS_SVNDIR}/build/ports/distfiles/fuppes-0.692.tar.gz /usr/ports/distfiles
 			echo "===> Copy fuppes-0.692.tar.gz done!"
-
 			# Copy required ports to FreeBSD ports directory.
 			echo;
 			echo "----------------------------------------------------------";
 			echo ">>> Copy new files to ports directory FreeBSD usr/ports/*.";
 			echo "----------------------------------------------------------";
+			echo "===> Delete current port ldb13 from usr/ports/databases/"
+			rm -rf /usr/ports/databases/ldb13
+			echo "===> done!"
+			echo "===> Start copy new port ldb13 to ports/databases/"
+			cp -Rpv ${XIGMANAS_SVNDIR}/build/ports/copy-ports/files/ldb13 /usr/ports/databases
+			echo "===> Copy new files to /usr/ports/databases/ldb13 done!"
 			echo "===> Start copy new port php72-APCu files to ports/devel/"
 			cp -Rpv ${XIGMANAS_SVNDIR}/build/ports/copy-ports/files/php72-APCu /usr/ports/devel
 			echo "===> Copy new files to /usr/ports/devel/php72-APCu done!"
