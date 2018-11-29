1cae71c623b4d582c39f3d52261fcf376630f5d1
Michael Zoon <zoon01@users.noreply.github.com>
2018-11-29 01:13:38 +0000

    update patch dmidecode.c
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@6220 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/ports/dmidecode/files/patch-dmidecode.c
+++ build/ports/dmidecode/files/patch-dmidecode.c
@@ -1,5 +1,5 @@
---- dmidecode.c.orig	2018-09-14 13:52:12 UTC
-+++ dmidecode.c
+--- dmidecode.c.orig	2018-09-14 15:52:12.000000000 +0200
++++ dmidecode.c	2018-11-29 02:09:33.000000000 +0100
 @@ -66,6 +66,7 @@
  #include <stdlib.h>
  #include <unistd.h>
@@ -8,3 +8,12 @@
  
  #ifdef __FreeBSD__
  #include <errno.h>
+@@ -3609,7 +3610,7 @@
+ 		hname = out_of_spec;
+ 		hlen = strlen(out_of_spec);
+ 	}
+-	printf("%s\t\tRedfish Service Hostname: %*s\n", prefix, hlen, hname);
++	printf("%s\t\tRedfish Service Hostname: %.*s\n", prefix, hlen, hname);
+ }
+ 
+ /*
