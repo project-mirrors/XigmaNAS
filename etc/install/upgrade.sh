751ad971d5ae321af313fb7c82656300203ba218
Michael Zoon <zoon01@users.noreply.github.com>
2018-12-17 20:48:38 +0000

    define zroot name
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@6283 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/install/upgrade.sh
+++ etc/install/upgrade.sh
@@ -16,6 +16,7 @@ stop_cmd=":"
 clean_cmd="upgrade_clean"
 
 # Check for ZFS Boot Environments and set default path.
+ZROOT="zroot"
 BEPATH=""
 if zpool list -H -o bootfs | grep -qw "${ZROOT}/ROOT"; then
 	BEPATH="/var/tmp/be_upgrade"
