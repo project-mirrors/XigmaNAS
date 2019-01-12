6c9b9e88528e3ff5c979803ab5525536420afbe0
Michael Zoon <zoon01@users.noreply.github.com>
2019-01-12 21:36:28 +0000

    Upgrade bash to 5.0.
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@6383 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/ports/bash/files/patch-config-top.h
+++ build/ports/bash/files/patch-config-top.h
@@ -1,6 +1,6 @@
---- config-top.h.orig	2016-05-19 18:34:02 UTC
-+++ config-top.h
-@@ -63,18 +63,18 @@
+--- config-top.h.orig	2018-10-12 21:35:23.000000000 +0200
++++ config-top.h	2019-01-12 21:58:22.000000000 +0100
+@@ -63,7 +63,7 @@
  /* The default value of the PATH variable. */
  #ifndef DEFAULT_PATH_VALUE
  #define DEFAULT_PATH_VALUE \
@@ -8,7 +8,8 @@
 +  "/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:%%LOCALBASE%%/sbin:%%LOCALBASE%%/bin"
  #endif
  
- /* The value for PATH when invoking `command -p'.  This is only used when
+ /* If you want to unconditionally set a value for PATH in every restricted
+@@ -74,11 +74,11 @@
     the Posix.2 confstr () function, or CS_PATH define are not present. */
  #ifndef STANDARD_UTILS_PATH
  #define STANDARD_UTILS_PATH \
