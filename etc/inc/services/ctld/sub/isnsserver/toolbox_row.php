eb98a4197a590ac099550553c2d4d67a84d59d89
Michael Schneider <ms49434@users.noreply.github.com>
2019-02-14 00:12:39 +0000

    code change
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@6468 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/services/ctld/sub/isnsserver/toolbox_row.php
+++ etc/inc/services/ctld/sub/isnsserver/toolbox_row.php
@@ -46,7 +46,7 @@ class toolbox_row {
 	public static function init_sphere() {
 		global $config;
 
-		$sphere = new co_sphere_row('services_ctl_sub_isnsserver_edit','php');
+		$sphere = new mys\row('services_ctl_sub_isnsserver_edit','php');
 		$sphere->get_parent()->set_basename('services_ctl_sub_isnsserver');
 		$sphere->
 			set_notifier(__NAMESPACE__)->
