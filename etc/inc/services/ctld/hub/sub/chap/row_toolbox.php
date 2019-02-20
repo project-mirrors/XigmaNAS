e47a77392bd26ed3b44022b2127c31f9b84e8389
Michael Schneider <ms49434@users.noreply.github.com>
2019-02-20 21:23:01 +0000

    code changes and refactoring
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@6520 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

---
+++ etc/inc/services/ctld/hub/sub/chap/row_toolbox.php
@@ -0,0 +1,71 @@
+<?php
+/*
+	row_toolbox.php
+
+	Part of XigmaNAS (https://www.xigmanas.com).
+	Copyright © 2018-2019 XigmaNAS <info@xigmanas.com>.
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice, this
+	   list of conditions and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the above copyright notice,
+	   this list of conditions and the following disclaimer in the documentation
+	   and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+	The views and conclusions contained in the software and documentation are those
+	of the authors and should not be interpreted as representing official policies
+	of XigmaNAS, either expressed or implied.
+*/
+namespace services\ctld\hub\sub\chap;
+use common\sphere as mys;
+use services\ctld\hub\row_hub as hub;
+/**
+ *	Wrapper class for autoloading functions
+ */
+final class row_toolbox {
+/**
+ *	Create the sphere object
+ *	@return \common\sphere\row The sphere object
+ */
+	public static function init_sphere() {
+		$sphere = new mys\row;
+		shared_toolbox::init_sphere($sphere);
+		$sphere->
+			set_script('services_ctl_sub_chap_edit')->
+			set_parent('services_ctl_sub_chap');
+		return $sphere;
+	}
+/**
+ *	Create the request method object
+ *	@param \services\ctld\hub\sub\chap\row_properties $cop
+ *	@param \common\sphere\row $sphere
+ *	@return \common\rmo\rmo The request method object
+ */
+	public static function init_rmo(row_properties $cop,mys\row $sphere) {
+		$rmo = hub::get_std_rmo($cop,$sphere);
+		return $rmo;
+	}
+/**
+ *	Create the properties object
+ *	@return \services\ctld\hub\sub\chap\row_properties The properties object
+ */
+	public static function init_properties() {
+		$cop = new row_properties();
+		return $cop;
+	}
+}
