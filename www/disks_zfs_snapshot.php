9b0f315e8a302636417b3b5321e43d4682f1933a
Michael Schneider <ms49434@users.noreply.github.com>
2019-07-09 22:14:41 +0000

    update filter
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/11.2.0.4@6783 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/disks_zfs_snapshot.php
+++ www/disks_zfs_snapshot.php
@@ -35,6 +35,167 @@ require_once 'auth.inc';
 require_once 'guiconfig.inc';
 require_once 'zfs.inc';
 
+function get_zfs_snapshots(): array {
+	$result = [];
+	$cmd = 'zfs list -pH -o name,used,creation -t snapshot 2>&1';
+	mwexec2($cmd,$rawdata);
+	foreach($rawdata as $line):
+		$a = preg_split('/\t/',$line);
+		$r = [];
+		$name = $a[0];
+		$r['snapshot'] = $name;
+//		the following regex splits the snapshot name into
+//		1: [pool name]
+//		2: /[dataset name | volume name]
+//		3: [dataset name | volume name]
+//		4: [snapshot name]
+		if(preg_match('/^([^\/\@]+)(\/([^\@]+))?\@(.*)$/',$name,$m)):
+			$r['pool'] = $m[1];
+			$r['name'] = $m[4];
+			$r['path'] = $m[1].$m[2];
+		else:
+			$r['pool'] = 'unknown'; // XXX
+			$r['name'] = 'unknown'; // XXX
+			$r['path'] = $name;
+		endif;
+		$r['used'] = $a[1];
+		$r['creation'] = $a[2];
+		$result[] = $r;
+	endforeach;
+	return $result;
+}
+function get_timestamp_values_from_time_id(string $time_id = ''): array {
+	$filter = [
+		'lo' => null,
+		'hi' => null
+	];
+	switch($time_id):
+		default:
+//			no time range
+			break;
+		case 'dc':
+//			current day
+			$filter['lo'] = 'today';
+			break;
+		case 'dp':
+//			previous day
+			$filter['lo'] = 'yesterday';
+			$filter['hi'] = 'today';
+			break;
+		case 'dotp':
+//			older than previous day
+			$filter['hi'] = 'yesterday';
+			break;
+		case 'wc':
+//			current week
+			$filter['lo'] = 'mon this week';
+			break;
+		case 'wp':
+//			previous week
+			$filter['lo'] = 'mon last week';
+			$filter['hi'] = 'mon this week';
+			break;
+		case 'wotp':
+//			older than previous week
+			$filter['hi'] = 'mon last week';
+			break;
+		case 'mc':
+//			current month
+			$filter['lo'] = 'first day of this month midnight';
+			break;
+		case 'mp':
+//			previous month
+			$filter['lo'] = 'first day of last month midnight';
+			$filter['hi'] = 'first day of this month midnight';
+			break;
+		case 'mcp':
+//			current and previous month
+			$filter['lo'] = 'first day of last month midnight';
+			break;
+		case 'motp':
+//			older than previous month
+			$filter['hi'] = 'first day of last month midnight';
+			break;
+		case 'qc':
+//			current quarter
+			$moq = (date('n') - 1) % 3;
+			$filter['lo'] = sprintf('first day of this month midnight -%u months',$moq);
+			break;
+		case 'qp':
+//			previous quarter
+			$moq = (date('n') - 1) % 3;
+			$filter['lo'] = sprintf('first day of this month midnight -%u months',$moq + 3);
+			$filter['hi'] = sprintf('first day of this month midnight -%u months',$moq);
+			break;
+		case 'qotp':
+//			older than previous quarter
+			$moq = (date('n') - 1) % 3;
+			$filter['hi'] = sprintf('first day of this month midnight -%u months',$moq + 3);
+			break;
+		case 'yc':
+//			current year
+			$filter['lo'] = 'first day of jan this year';
+			break;
+		case 'yp':
+//			previous year
+			$filter['lo'] = 'first day of jan last year';
+			$filter['hi'] = 'first day of jan this year';
+			break;
+		case 'ycp':
+//			current and previous year
+			$filter['lo'] = 'first day of jan last year';
+			break;
+		case 'yotp':
+//			older than previous year
+			$filter['hi'] = 'first day of jan last year';
+			break;
+	endswitch;
+	return $filter;
+}
+function get_zfs_snapshots_filter(array $snapshots,string $filter_time_id = '0'): array {
+	$result = [];
+	$filter = get_timestamp_values_from_time_id($filter_time_id);
+	if(!is_null($filter['lo'])):
+		$time_from = strtotime($filter['lo']);
+		if(false === $time_from):
+			$filter['lo'] = null;
+		endif;
+	endif;
+	if(!is_null($filter['hi'])):
+		$time_to = strtotime($filter['hi']);
+		if(false === $time_to):
+			$filter['hi'] = null;
+		endif;
+	endif;
+	foreach($snapshots as $snapshot):
+		if((is_null($filter['lo']) || $snapshot['creation'] >= $time_from) && (is_null($filter['hi']) || $snapshot['creation'] < $time_to)):
+			$result[] = $snapshot;
+		endif;
+	endforeach;
+	return $result;
+}
+function zfssnapshot_process_updatenotification($mode,$data) {
+	global $config;
+	$ret = [
+		'output' => [],
+		'retval' => 0
+	];
+	switch($mode):
+		case UPDATENOTIFY_MODE_NEW:
+			$data = unserialize($data);
+			$ret = zfs_snapshot_configure($data);
+			break;
+		case UPDATENOTIFY_MODE_MODIFIED:
+			$data = unserialize($data);
+			$ret = zfs_snapshot_properties($data);
+			break;
+		case UPDATENOTIFY_MODE_DIRTY:
+			$data = unserialize($data);
+			$ret = zfs_snapshot_destroy($data);
+			break;
+	endswitch;
+	return $ret;
+}
 $sphere_scriptname = basename(__FILE__);
 $sphere_scriptname_child = 'disks_zfs_snapshot_edit.php';
 $sphere_header = 'Location: '.$sphere_scriptname;
@@ -68,82 +229,44 @@ $img_path = [
 	'mup' => 'images/up.png',
 	'mdn' => 'images/down.png'
 ];
-
-function get_zfs_snapshots() {
-	$result = [];
-	$cmd = 'zfs list -pH -o name,used,creation -t snapshot 2>&1';
-	mwexec2($cmd,$rawdata);
-	foreach($rawdata as $line):
-		$a = preg_split('/\t/',$line);
-		$r = [];
-		$name = $a[0];
-		$r['snapshot'] = $name;
-		// the following regex splits the snapshot name into
-		// 1: [pool name]
-		// 2: /[dataset name | volume name]
-		// 3: [dataset name | volume name]
-		// 4: [snapshot name]
-		if(preg_match('/^([^\/\@]+)(\/([^\@]+))?\@(.*)$/', $name, $m)):
-			$r['pool'] = $m[1];
-			$r['name'] = $m[4];
-			$r['path'] = $m[1].$m[2];
-		else:
-			$r['pool'] = 'unknown'; // XXX
-			$r['name'] = 'unknown'; // XXX
-			$r['path'] = $name;
-		endif;
-		$r['used'] = $a[1];
-		$r['creation'] = $a[2];
-		$result[] = $r;
-	endforeach;
-	return $result;
-}
 $a_snapshot = get_zfs_snapshots();
-
-if(isset($_SESSION['filter_time'])):
-	$filter_time = $_SESSION['filter_time'];
+if(isset($_SESSION['filter_time_id'])):
+	$filter_time_id = $_SESSION['filter_time_id'];
 else:
-	$filter_time = '1week';
+	$filter_time_id = '0';
 endif;
-$l_filter_time = [
-    '1week' => sprintf(gettext('%d week'),1),
-    '2weeks' => sprintf(gettext('%d weeks'),2),
-    '30days' => sprintf(gettext('%d days'),30),
-    '60days' => sprintf(gettext('%d days'),60),
-    '90days' => sprintf(gettext('%d days'),90),
-    '180days' => sprintf(gettext('%d days'),180),
-    '0' => gettext('All')
+$l_filter_time_id = [
+	'0' => gettext('All'),
+	'dc' => gettext('Today'),
+	'dp' => gettext('Yesterday'),
+	'dotp' => gettext('Older than yesterday'),
+	'wc' => gettext('This week'),
+	'wp' => gettext('Last week'),
+	'wotp' => gettext('Older than last week'),
+	'mc' => gettext('This month'),
+	'mp' => gettext('Last month'),
+	'mcp' => gettext('Current and previous month'),
+	'motp' => gettext('Older than last month'),
+	'qc' => gettext('Current quarter'),
+	'qp' => gettext('Previous quarter'),
+	'qotp' => gettext('Older than previous quarter'),
+	'yc' => gettext('Current year'),
+	'yp' => gettext('Previous year'),
+	'ycp' => gettext('Current and previous year'),
+	'yotp' => gettext('Older than previous year')
 ];
-
-function get_zfs_snapshots_filter($snapshots,$filter) {
-	if($filter['time'] == 0):
-		return $snapshots;
-	else:
-		$now = time() / 86400;
-		$now *= 86400;
-		$f_time = strtotime(sprintf('-%s',$filter['time']),$now);
-		$result = [];
-		foreach($snapshots as $snapshot):
-			if($snapshot['creation'] >= $f_time):
-				$result[] = $snapshot;
-			endif;
-		endforeach;
-	endif;
-	return $result;
-}
-$sphere_array = get_zfs_snapshots_filter($a_snapshot,['time' => $filter_time]);
-
+$sphere_array = get_zfs_snapshots_filter($a_snapshot,$filter_time_id);
 if($_POST):
 	if(isset($_POST['filter']) && $_POST['filter']):
-		$_SESSION['filter_time'] = $_POST['filter_time'];
+		$_SESSION['filter_time_id'] = $_POST['filter_time_id'];
 		header($sphere_header);
 		exit;
 	endif;
 	if(isset($_POST['apply']) && $_POST['apply']):
-		$ret = array('output' => [], 'retval' => 0);
+		$ret = array('output' => [],'retval' => 0);
 		if(!file_exists($d_sysrebootreqd_path)):
-			// Process notifications
-			$ret = zfs_updatenotify_process($sphere_notifier, $sphere_notifier_processor);
+//			Process notifications
+			$ret = zfs_updatenotify_process($sphere_notifier,$sphere_notifier_processor);
 		endif;
 		$savemsg = get_std_save_message($ret['retval']);
 		if($ret['retval'] == 0):
@@ -152,26 +275,26 @@ if($_POST):
 			exit;
 		endif;
 		updatenotify_delete($sphere_notifier);
-		$errormsg = implode("\n", $ret['output']);
+		$errormsg = implode("\n",$ret['output']);
 	endif;
 	if(isset($_POST['delete_selected_rows']) && $_POST['delete_selected_rows']):
 		$checkbox_member_array = isset($_POST[$checkbox_member_name]) ? $_POST[$checkbox_member_name] : [];
 		foreach($checkbox_member_array as $checkbox_member_record):
-			if(false !== ($index = array_search_ex($checkbox_member_record, $sphere_array, 'snapshot'))):
-				$identifier = serialize(['snapshot' => $checkbox_member_record, 'recursive' => false]);
+			if(false !== ($index = array_search_ex($checkbox_member_record,$sphere_array,'snapshot'))):
+				$identifier = serialize(['snapshot' => $checkbox_member_record,'recursive' => false]);
 				if(!isset($sphere_array[$index]['protected'])):
-					$mode_updatenotify = updatenotify_get_mode($sphere_notifier, $identifier);
+					$mode_updatenotify = updatenotify_get_mode($sphere_notifier,$identifier);
 					switch($mode_updatenotify):
-						case UPDATENOTIFY_MODE_NEW:  
-							updatenotify_clear($sphere_notifier, $identifier);
-							updatenotify_set($sphere_notifier, UPDATENOTIFY_MODE_DIRTY_CONFIG, $identifier);
+						case UPDATENOTIFY_MODE_NEW:
+							updatenotify_clear($sphere_notifier,$identifier);
+							updatenotify_set($sphere_notifier,UPDATENOTIFY_MODE_DIRTY_CONFIG,$identifier);
 							break;
 						case UPDATENOTIFY_MODE_MODIFIED:
-							updatenotify_clear($sphere_notifier, $identifier);
-							updatenotify_set($sphere_notifier, UPDATENOTIFY_MODE_DIRTY, $identifier);
+							updatenotify_clear($sphere_notifier,$identifier);
+							updatenotify_set($sphere_notifier,UPDATENOTIFY_MODE_DIRTY,$identifier);
 							break;
 						case UPDATENOTIFY_MODE_UNKNOWN:
-							updatenotify_set($sphere_notifier, UPDATENOTIFY_MODE_DIRTY, $identifier);
+							updatenotify_set($sphere_notifier,UPDATENOTIFY_MODE_DIRTY,$identifier);
 							break;
 					endswitch;
 				endif;
@@ -181,30 +304,7 @@ if($_POST):
 		exit;
 	endif;
 endif;
-
-function zfssnapshot_process_updatenotification($mode, $data) {
-	global $config;
-	$ret = [
-		'output' => [],
-		'retval' => 0
-	];
-	switch ($mode):
-		case UPDATENOTIFY_MODE_NEW:
-			$data = unserialize($data);
-			$ret = zfs_snapshot_configure($data);
-			break;
-		case UPDATENOTIFY_MODE_MODIFIED:
-			$data = unserialize($data);
-			$ret = zfs_snapshot_properties($data);
-			break;
-		case UPDATENOTIFY_MODE_DIRTY:
-			$data = unserialize($data);
-			$ret = zfs_snapshot_destroy($data);
-			break;
-	endswitch;
-	return $ret;
-}
-$pgtitle = [gtext('Disks'), gtext('ZFS'), gtext('Snapshots'), gtext('Snapshot')];
+$pgtitle = [gtext('Disks'),gtext('ZFS'),gtext('Snapshots'),gtext('Snapshot')];
 include 'fbegin.inc';
 ?>
 <script type="text/javascript">
@@ -310,7 +410,7 @@ $document->render();
 		</thead>
 		<tbody>
 <?php
-			html_combobox2('filter_time', gettext('Age'), $filter_time, $l_filter_time, '');
+			html_combobox2('filter_time_id',gettext('Display'),$filter_time_id,$l_filter_time_id,'');
 ?>
 		</tbody>
 	</table>
@@ -329,11 +429,11 @@ $document->render();
 		<thead>
 <?php
 			html_separator2();
-			html_titleline2(gettext('Overview'), 6);
+			html_titleline2(gettext('Overview'),6);
 ?>
 			<tr>
 				<th class="lhelc"><input type="checkbox" id="togglemembers" name="togglemembers" title="<?=gtext('Invert Selection');?>"/></th>
-				<th class="lhell"><?=sprintf('%1$s (%2$d/%3$d)', gtext('Path'), count($sphere_array), count($a_snapshot));?></th>
+				<th class="lhell"><?=sprintf('%s (%d/%d)',gtext('Path'),count($sphere_array),count($a_snapshot));?></th>
 				<th class="lhell"><?=gtext('Name');?></th>
 				<th class="lhell"><?=gtext('Used');?></th>
 				<th class="lhell"><?=gtext('Create Date');?></th>
@@ -342,16 +442,16 @@ $document->render();
 		</thead>
 		<tbody>
 <?php
-			foreach ($sphere_array as $sphere_record):
-				$identifier = serialize(['snapshot' => $sphere_record['snapshot'], 'recursive'=> false]);
-				$notificationmode = updatenotify_get_mode($sphere_notifier, $identifier);
+			foreach($sphere_array as $sphere_record):
+				$identifier = serialize(['snapshot' => $sphere_record['snapshot'],'recursive'=> false]);
+				$notificationmode = updatenotify_get_mode($sphere_notifier,$identifier);
 				$notdirty = (UPDATENOTIFY_MODE_DIRTY != $notificationmode) && (UPDATENOTIFY_MODE_DIRTY_CONFIG != $notificationmode);
 				$notprotected = !isset($sphere_record['protected']);
 ?>
 				<tr>
 					<td class="lcelc">
 <?php
-						if ($notdirty && $notprotected):
+						if($notdirty && $notprotected):
 ?>
 							<input type="checkbox" name="<?=$checkbox_member_name;?>[]" value="<?=$sphere_record['snapshot'];?>" id="<?=$sphere_record['snapshot'];?>"/>
 <?php
@@ -375,7 +475,7 @@ $document->render();
 									<a href="<?=$sphere_scriptname_child;?>?snapshot=<?=urlencode($sphere_record['snapshot']);?>"><img src="<?=$img_path['mod'];?>" title="<?=$gt_record_mod;?>" alt="<?=$gt_record_mod;?>"  class="spin oneemhigh"/></a>
 <?php
 								else:
-									if ($notprotected):
+									if($notprotected):
 ?>
 										<img src="<?=$img_path['del'];?>" title="<?=$gt_record_del;?>" alt="<?=$gt_record_del;?>"/>
 <?php
