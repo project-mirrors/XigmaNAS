03e98297a403c4cf2dd5fd0318a92d636f9eaea5
Michael Zoon <zoon01@users.noreply.github.com>
2019-09-23 16:24:16 +0000

    update kernel-patch
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/11.3.0.4@6893 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/kernel-patches/swap_pager/files/patch-sys__vm__swap_pager.c
+++ build/kernel-patches/swap_pager/files/patch-sys__vm__swap_pager.c
@@ -1,5 +1,5 @@
---- sys/vm/swap_pager.c.orig	2018-06-11 01:21:24.378069000 +0200
-+++ sys/vm/swap_pager.c	2018-06-11 02:07:55.000000000 +0200
+--- sys/vm/swap_pager.c.orig	2019-09-23 17:46:32.489287000 +0200
++++ sys/vm/swap_pager.c	2019-09-23 18:22:22.000000000 +0200
 @@ -860,7 +860,7 @@
  	VM_OBJECT_WLOCK(object);
  	while (size) {
