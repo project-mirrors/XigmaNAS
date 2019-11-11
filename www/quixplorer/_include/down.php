bbd0afdebaad9781e4ddd0dad4d052560ec56a46
Michael Schneider <ms49434@users.noreply.github.com>
2019-11-11 16:43:10 +0000

    offload download to lighttpd
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/11.3.0.4@7099 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/quixplorer/_include/down.php
+++ www/quixplorer/_include/down.php
@@ -38,84 +38,65 @@
 require_once '_include/archive.php';
 require_once '_include/permissions.php';
 require_once 'qxpage.php';
+
 /**
- * download_selected
- * @return void
+ *	download_selected
+ *	@param string $dir
  **/
 function download_selected($dir) {
 	require_once '_include/archive.php';
 	$items = qxpage_selected_items();
 	_download_items($dir,$items);
 }
-// download file
+/**
+ *	download file
+ *	@param string $dir
+ *	@param string $item
+ */
 function download_item($dir,$item) {
 	_download_items($dir,[$item]);
 }
-
+/**
+ *	_download_items
+ *	@param string $dir
+ *	@param array $items
+ */
 function _download_items($dir,$items) {
-	// check if user selected any items to download
+//	check if user selected any items to download
 	_debug("count items: '$items[0]'");
 	if(count($items) == 0):
 		show_error($GLOBALS['error_msg']['miscselitems']);
 	endif;
-	// check if user has permissions to download this file
+//	check if user has permissions to download this file
 	if(! _is_download_allowed($dir,$items)):
 		show_error( $GLOBALS['error_msg']['accessitem']);
 	endif;
-	// if we have exactly one file and this is a real file we directly download it
+//	if we have exactly one file and this is a real file, we directly download
 	if(count($items) == 1 && get_is_file($dir,$items[0])):
 		$abs_item = get_abs_item($dir,$items[0]);
 		_download($abs_item,$items[0]);
 	endif;
-	// otherwise we do the zip download
+//	otherwise we do the zip download
 	zip_download(get_abs_dir($dir),$items);
 }
-function _download_header($filename,$filesize = 0) {
-	$browser=id_browser();
-	header('Content-Type: ' . (($browser=='IE' || $browser=='OPERA') ? 'application/octetstream' : 'application/octet-stream'));
-	header('Expires: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-	header('Content-Transfer-Encoding: binary');
-	if ($filesize != 0):
-		header('Content-Length: ' . $filesize);
-	endif;
-	header(sprintf('Content-Disposition: attachment; filename="%s"',$filename));
-	if($browser == 'IE'):
-		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-		header('Pragma: public');
-	else:
-		header('Cache-Control: no-cache, must-revalidate');
-		header('Pragma: no-cache');
-	endif;
+/**
+ *	Downloads a file with X-Sendfile
+ *	@param string $file Full path name + file name + extension
+ *	@param string $localname File name + extension
+ */
+function _download(string $file,string $localname) {
+	header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+	header(sprintf('Content-Disposition: attachment; filename="%s"',$localname));
+	header(sprintf('Content-Type: application/octet-stream; name="%s"',$localname));
+	header(sprintf('X-Sendfile: %s',$file));
+	exit;
 }
-
-function _download($file,$localname) {
-	// $file = full path name, file name and extension
-	// $localname = file name and extension
-	// determine availability of x-sendfile
-	$use_x_sendfile = false;
-	if(function_exists('apache_get_modules') && in_array('mod_xsendfile',apache_get_modules())):
-		$hdr_x_sendfile= sprintf('X-Sendfile: %s',$file);
-		$use_x_sendfile = true;
-	elseif(preg_match('/nginx\1(\.[0-9]+)+/',$_SERVER['SERVER_SOFTWARE'])):
-		$hdr_x_sendfile= sprintf('X-Accel-Redirect: %s',$file);
-		$use_x_sendfile = true;
-	elseif(preg_match('/lighttpd\/1(\.[0-9]+)+/',$_SERVER['SERVER_SOFTWARE'])):
-		$hdr_x_sendfile = sprintf('X-Sendfile: %s',$file);
-		$use_x_sendfile = true;
-	endif;
-	if($use_x_sendfile): // use x-sendfile
-		$hdr_content_disposition = sprintf('Content-Disposition: attachment; filename="%s"',$localname);
-		$hdr_content_type = sprintf('Content-Type: application/octet-stream; name="%s"',$localname);
-		header($hdr_content_disposition);
-		header($hdr_content_type);
-		header($hdr_x_sendfile);
-		exit;
-	else: // use php
-		_download_header($localname,@filesize($file));
-		@readfile($file);
-		exit;
-	endif;
-}
+/**
+ *	_is_download_allowed
+ *	@param string $dir
+ *	@param array $items
+ *	@return boolean
+ */
 function _is_download_allowed($dir,$items) {
 	foreach($items as $file):
 		if(!permissions_grant($dir,$file,'read')):
@@ -130,4 +111,3 @@ function _is_download_allowed($dir,$items) {
 	endforeach;
 	return true;
 }
-?>
