f22f2165aa8774eaa6d7e608ecd7eab41598bfae
Michael Schneider <ms49434@users.noreply.github.com>
2020-01-02 18:15:13 +0000

    2020
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@7140 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/quixplorer/_lang/ko_mimes.php
+++ www/quixplorer/_lang/ko_mimes.php
@@ -2,8 +2,8 @@
 /*
 	ko_mimes.php
 
-	Part of XigmaNAS (https://www.xigmanas.com).
-	Copyright (c) 2018-2019 XigmaNAS <info@xigmanas.com>.
+	Part of XigmaNAS® (https://www.xigmanas.com).
+	Copyright © 2018-2020 XigmaNAS® <info@xigmanas.com>.
 	All rights reserved.
 
 	Portions of Quixplorer (http://quixplorer.sourceforge.net).
@@ -32,7 +32,7 @@
 
 	The views and conclusions contained in the software and documentation are those
 	of the authors and should not be interpreted as representing official policies
-	of XigmaNAS, either expressed or implied.
+	of XigmaNAS®, either expressed or implied.
 */
 // Korean Mimes Module
 
