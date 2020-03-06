251887af3570434c1b4f0450c2ef09db280e9416
Michael Schneider <ms49434@users.noreply.github.com>
2020-03-06 22:02:22 +0000

    fix invalid index
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@7297 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/services_mariadb.php
+++ www/services_mariadb.php
@@ -41,8 +41,8 @@ use services\mariadb\shared_toolbox;
 //	init indicators
 $input_errors = [];
 //	preset $savemsg when a reboot is pending
-if(file_exists($d_sysrebootreqd_path)):
-	$savemsg = get_std_save_message(0);
+if(\file_exists($d_sysrebootreqd_path)):
+	$savemsg = \get_std_save_message(0);
 endif;
 //	init properties, sphere and rmo
 $cop = toolbox::init_properties();
@@ -54,15 +54,15 @@ $a_referer = [
 	$cop->get_auxparam(),
 	$cop->get_phrasecookieauth()
 ];
-$pending_changes = updatenotify_exists($sphere->get_notifier());
+$pending_changes = \updatenotify_exists($sphere->get_notifier());
 list($page_method,$page_action,$page_mode) = $rmo->validate();
 switch($page_method):
 	case 'SESSION':
 		switch($page_action):
 			case $sphere->get_script()->get_basename():
-				$retval = filter_var($_SESSION[$sphere->get_script()->get_basename()],FILTER_VALIDATE_INT,['options' => ['default' => 0]]);
+				$retval = \filter_var($_SESSION[$sphere->get_script()->get_basename()],FILTER_VALIDATE_INT,['options' => ['default' => 0]]);
 				unset($_SESSION['submit'],$_SESSION[$sphere->get_script()->get_basename()]);
-				$savemsg = get_std_save_message($retval);
+				$savemsg = \get_std_save_message($retval);
 				if($retval !== 0):
 					$page_action = 'edit';
 					$page_mode = PAGE_MODE_EDIT;
@@ -77,27 +77,28 @@ switch($page_method):
 		switch($page_action):
 			case 'apply':
 				$retval = 0;
-				$retval |= updatenotify_process($sphere->get_notifier(),$sphere->get_notifier_processor());
-				config_lock();
-				$retval |= rc_exec_service('userdb');
-				$retval |= rc_update_service('mysqldb');
-				config_unlock();
+				$retval |= \updatenotify_process($sphere->get_notifier(),$sphere->get_notifier_processor());
+				\config_lock();
+				$retval |= \rc_exec_service('userdb');
+				$retval |= \rc_update_service('mysqldb');
+				\config_unlock();
 				$_SESSION['submit'] = $sphere->get_script()->get_basename();
 				$_SESSION[$sphere->get_script()->get_basename()] = $retval;
-				header($sphere->get_script()->get_location());
+				\header($sphere->get_script()->get_location());
 				exit;
 				break;
 /*
 			case 'reload':
 				$retval = 0;
 				$name = $cop->get_enable()->get_name();
+				$sphere->grid[$name] ??= false;
 				if($sphere->grid[$name] && !$pending_changes):
-					config_lock();
-					$retval |= rc_update_service('mysqldb',true);
-					config_unlock();
+					\config_lock();
+					$retval |= \rc_update_service('mysqldb',true);
+					\config_unlock();
 					$_SESSION['submit'] = $sphere->get_script()->get_basename();
 					$_SESSION[$sphere->get_script()->get_basename()] = $retval;
-					header($sphere->get_script()->get_location());
+					\header($sphere->get_script()->get_location());
 				else:
 					$page_action = 'view';
 					$page_mode = PAGE_MODE_VIEW;
@@ -108,13 +109,14 @@ switch($page_method):
 			case 'restart':
 				$retval = 0;
 				$name = $cop->get_enable()->get_name();
+				$sphere->grid[$name] ??= false;
 				if($sphere->grid[$name] && !$pending_changes):
-					config_lock();
-					$retval |= rc_update_service('mysqldb');
-					config_unlock();
+					\config_lock();
+					$retval |= \rc_update_service('mysqldb');
+					\config_unlock();
 					$_SESSION['submit'] = $sphere->get_script()->get_basename();
 					$_SESSION[$sphere->get_script()->get_basename()] = $retval;
-					header($sphere->get_script()->get_location());
+					\header($sphere->get_script()->get_location());
 					exit;
 				else:
 					$page_action = 'view';
@@ -124,15 +126,16 @@ switch($page_method):
 			case 'disable':
 				$retval = 0;
 				$name = $cop->get_enable()->get_name();
+				$sphere->grid[$name] ??= false;
 				if($sphere->grid[$name]):
 					$sphere->grid[$name] = false;
-					write_config();
-					config_lock();
-					$retval |= rc_update_service('mysqldb');
-					config_unlock();
+					\write_config();
+					\config_lock();
+					$retval |= \rc_update_service('mysqldb');
+					\config_unlock();
 					$_SESSION['submit'] = $sphere->get_script()->get_basename();
 					$_SESSION[$sphere->get_script()->get_basename()] = $retval;
-					header($sphere->get_script()->get_location());
+					\header($sphere->get_script()->get_location());
 					exit;
 				else:
 					$page_action = 'view';
@@ -141,18 +144,19 @@ switch($page_method):
 			case 'enable':
 				$retval = 0;
 				$name = $cop->get_enable()->get_name();
+				$sphere->grid[$name] ??= false;
 				if($sphere->grid[$name] || $pending_changes):
 					$page_action = 'view';
 					$page_mode = PAGE_MODE_VIEW;
 				else:
 					$sphere->grid[$name] = true;
-					write_config();
-					config_lock();
-					$retval |= rc_update_service('mysqldb');
-					config_unlock();
+					\write_config();
+					\config_lock();
+					$retval |= \rc_update_service('mysqldb');
+					\config_unlock();
 					$_SESSION['submit'] = $sphere->get_script()->get_basename();
 					$_SESSION[$sphere->get_script()->get_basename()] = $retval;
-					header($sphere->get_script()->get_location());
+					\header($sphere->get_script()->get_location());
 					exit;
 				endif;
 				break;
@@ -168,16 +172,14 @@ switch($page_action):
 			$name = $referer->get_name();
 			switch($name):
 				case 'auxparam':
-					if(array_key_exists($name,$source)):
-						if(is_array($source[$name])):
-							$source[$name] = implode(PHP_EOL,$source[$name]);
-						endif;
+					if(\array_key_exists($name,$source) && \is_array($source[$name])):
+						$source[$name] = \implode("\n",$source[$name]);
 					endif;
 					break;
 			endswitch;
 			$sphere->row[$name] = $referer->validate_array_element($source);
-			if(is_null($sphere->row[$name])):
-				if(array_key_exists($name,$source) && is_scalar($source[$name])):
+			if(\is_null($sphere->row[$name])):
+				if(\array_key_exists($name,$source) && \is_scalar($source[$name])):
 					$sphere->row[$name] = $source[$name];
 				else:
 					$sphere->row[$name] = $referer->get_defaultvalue();
@@ -190,9 +192,9 @@ switch($page_action):
 		foreach($a_referer as $referer):
 			$name = $referer->get_name();
 			$sphere->row[$name] = $referer->validate_input();
-			if(is_null($sphere->row[$name])):
+			if(\is_null($sphere->row[$name])):
 				$input_errors[] = $referer->get_message_error();
-				if(array_key_exists($name,$source) && is_scalar($source[$name])):
+				if(\array_key_exists($name,$source) && \is_scalar($source[$name])):
 					$sphere->row[$name] = $source[$name];
 				else:
 					$sphere->row[$name] = $referer->get_defaultvalue();
@@ -205,8 +207,8 @@ switch($page_action):
 				switch($name):
 					case 'auxparam':
 						$auxparam_grid = [];
-						foreach(explode(PHP_EOL,$sphere->row[$name]) as $auxparam_row):
-							$auxparam_grid[] = trim($auxparam_row,"\t\n\r");
+						foreach(\explode("\n",$sphere->row[$name]) as $auxparam_row):
+							$auxparam_grid[] = \trim($auxparam_row,"\t\n\r");
 						endforeach;
 						$sphere->row[$name] = $auxparam_grid;
 						break;
@@ -214,25 +216,25 @@ switch($page_action):
 				$sphere->grid[$name] = $sphere->row[$name];
 			endforeach;
 			$usermysqlhomedir = $sphere->row[$cop->get_homedir()->get_name()] ?? '';
-			if(1 === preg_match('/\S/',$usermysqlhomedir) && file_exists($usermysqlhomedir)):
+			if((\preg_match('/\S/',$usermysqlhomedir) === 1) && \file_exists($usermysqlhomedir)):
 			else:
 				$usermysqlhomedir = '/nonexistent';
 			endif;
 			// update user mysql home dir
 			$extraoptions_changed = false;
-			$users = &array_make_branch($config,'system','usermanagement','user');
-			$index = array_search_ex('mysql',$users,'name');
+			$users = &\array_make_branch($config,'system','usermanagement','user');
+			$index = \array_search_ex('mysql',$users,'name');
 			if($index !== false):
-				$extraoptions_new = sprintf('-c "MySQL user" -d "%s" -s /usr/sbin/nologin',$usermysqlhomedir);
+				$extraoptions_new = \sprintf('-c "MySQL user" -d "%s" -s /usr/sbin/nologin',$usermysqlhomedir);
 				$extraoptions_current = $users[$index]['extraoptions'] ?? '';
 				$extraoptions_changed = $extraoptions_current !== $extraoptions_new;
 				if($extraoptions_changed):
 					$users[$index]['extraoptions'] = $extraoptions_new;
 				endif;
 			endif;
-			write_config();
-			updatenotify_set($sphere->get_notifier(),UPDATENOTIFY_MODE_MODIFIED,'SERVICE',$sphere->get_notifier_processor());
-			header($sphere->get_script()->get_location());
+			\write_config();
+			\updatenotify_set($sphere->get_notifier(),UPDATENOTIFY_MODE_MODIFIED,'SERVICE',$sphere->get_notifier_processor());
+			\header($sphere->get_script()->get_location());
 			exit;
 		else:
 			$page_mode = PAGE_MODE_EDIT;
@@ -240,14 +242,14 @@ switch($page_action):
 		break;
 endswitch;
 //	determine final page mode and calculate readonly flag
-list($page_mode,$is_readonly) = calc_skipviewmode($page_mode);
+list($page_mode,$is_readonly) = \calc_skipviewmode($page_mode);
 $is_enabled = $sphere->row[$cop->get_enable()->get_name()];
-$is_running = (0 === rc_is_service_running('mysqldb'));
-$is_running_message = $is_running ? gettext('Yes') : gettext('No');
-$input_errors_found = count($input_errors) > 0;
+$is_running = (0 === \rc_is_service_running('mysqldb'));
+$is_running_message = $is_running ? \gettext('Yes') : \gettext('No');
+$input_errors_found = \count($input_errors) > 0;
 //	create document
-$pgtitle = [gettext('Services'),gettext('MariaDB'),gettext('Settings')];
-$document = new_page($pgtitle,$sphere->get_script()->get_scriptname());
+$pgtitle = [\gettext('Services'),\gettext('MariaDB'),\gettext('Settings')];
+$document = \new_page($pgtitle,$sphere->get_script()->get_scriptname());
 //	add tab navigation
 shared_toolbox::add_tabnav($document);
 //	get areas
@@ -264,7 +266,7 @@ if($pending_changes && !$input_errors_found):
 	$content->ins_config_has_changed_box();
 endif;
 //	add content
-$n_auxparam_rows = min(64,max(5,1 + substr_count($sphere->row[$cop->get_auxparam()->get_name()],PHP_EOL)));
+$n_auxparam_rows = \min(64,\max(5,1 + \substr_count($sphere->row[$cop->get_auxparam()->get_name()],PHP_EOL)));
 $tds1 = $content->add_table_data_settings();
 $tds1->ins_colgroup_data_settings();
 $thead1 = $tds1->addTHEAD();
@@ -272,23 +274,23 @@ $tbody1 = $tds1->addTBODY();
 $tfoot1 = $tds1->addTFOOT()->c2_separator();
 switch($page_mode):
 	case PAGE_MODE_VIEW:
-		$thead1->c2_titleline(gettext('MariaDB'));
+		$thead1->c2_titleline(\gettext('MariaDB'));
 		break;
 	case PAGE_MODE_EDIT:
-		$thead1->c2_titleline_with_checkbox($cop->get_enable(),$sphere,false,$is_readonly,gettext('MariaDB'));
+		$thead1->c2_titleline_with_checkbox($cop->get_enable(),$sphere,false,$is_readonly,\gettext('MariaDB'));
 		break;
 endswitch;
 $tbody1->
-	c2_textinfo('running',gettext('Service Active'),$is_running_message)->
+	c2_textinfo('running',\gettext('Service Active'),$is_running_message)->
 	c2_filechooser($cop->get_homedir(),$sphere->row[$cop->get_homedir()->get_name()],true,$is_readonly)->
 	c2_textarea($cop->get_auxparam(),$sphere,false,$is_readonly,60,$n_auxparam_rows);
 $tds2 = $content->add_table_data_settings();
 $tds2->ins_colgroup_data_settings();
-$thead2 = $tds2->addTHEAD()->c2_titleline(sprintf('%s (%s)',gettext('Administrative WebGUI'),gettext('phpMyAdmin')));
+$thead2 = $tds2->addTHEAD()->c2_titleline(\sprintf('%s (%s)',\gettext('Administrative WebGUI'),\gettext('phpMyAdmin')));
 $tbody2 = $tds2->addTBODY();
 $tbody2->c2_input_password($cop->get_phrasecookieauth(),$sphere,false,$is_readonly);
 if($is_running):
-	$tbody2->c2_textinfo('url1',gettext('URL'),sprintf('<a href="%1$s" id="a_url1" target="_blank">%1$s</a>',htmlspecialchars('/phpMyAdmin/index.php')));
+	$tbody2->c2_textinfo('url1',\gettext('URL'),\sprintf('<a href="%1$s" id="a_url1" target="_blank">%1$s</a>',\htmlspecialchars('/phpMyAdmin/index.php')));
 endif;
 //	add buttons
 $buttons = $document->add_area_buttons();
