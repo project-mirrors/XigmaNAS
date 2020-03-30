a7d69c4ca48101f731403c4d5b5ec4b422cdac63
Michael Schneider <ms49434@users.noreply.github.com>
2020-03-30 21:50:47 +0000

    code rework
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@7445 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/quixplorer/qx.php
+++ www/quixplorer/qx.php
@@ -35,71 +35,60 @@
 	of the authors and should not be interpreted as representing official policies
 	of XigmaNAS®, either expressed or implied.
 */
-function qx_language()
-{
-    global $language;
-    print $language;
-}
+function qx_language() {
+	global $language;
 
-function qx_title()
-{
-    global $site_name;
-    print $site_name;
+	print $language;
 }
+function qx_title() {
+	global $site_name;
 
-function qx_img($image, $msg)
-{
+	print $site_name;
+}
+function qx_img($image,$msg) {
     ?><img class="button" src="_img/$image" alt="$msg" title="$msg" /><?php
 }
-
-function qx_user() { echo qx_user_s(); }
-
-function qx_user_s()
-{
-    //FIXME return real user
-    $user = $_SESSION["s_user"];
-    return (isset($user) ? $user : "anonymous");
+function qx_user() {
+	echo qx_user_s();
 }
-
+function qx_user_s() {
+//	FIXME return real user
+	return $_SESSION['uname'] ?? 'anonymous';
+}
 // @returns the relative path $rel to the current directory displayed.
-function qx_directory($rel = NULL)
-{
-    global $dir;
-    return $dir . "/" . $rel;
+function qx_directory($rel = null) {
+	global $dir;
+
+	return $dir . '/' . $rel;
 }
+function qx_grant($link) {
+	global $dir;
 
-function qx_grant($link)
-{
-    global $dir;
-
-    switch ($link)
-    {
-        case "javascript:Move();": return permissions_grant($dir, NULL, "change");
-        case "javascript:Copy();": return permissions_grant_all($dir, NULL, array("create", "read"));
-        case "javascript:Delete();": return permissions_grant($dir, NULL, "delete");
-        case "javascript:Archive();": return true;
-        case "javascript:location.reload();": return true;
-    }
-
-    if (preg_match("/\?action=upload/", $link)) return permissions_grant($dir, NULL, "create") && get_cfg_var("file_uploads");
-    if (preg_match("/\?action=list/", $link)) return true;
-
-    return false;
+	switch($link):
+		case 'javascript:Move();': return permissions_grant($dir,null,'change');
+		case 'javascript:Copy();': return permissions_grant_all($dir,null,['create','read']);
+		case 'javascript:Delete();': return permissions_grant($dir,null,'delete');
+		case 'javascript:Archive();': return true;
+		case 'javascript:location.reload();': return true;
+	endswitch;
+	if(preg_match('/\?action=upload/', $link)):
+		return permissions_grant($dir,null,'create') && get_cfg_var('file_uploads');
+	endif;
+	if(preg_match('/\?action=list/',$link)):
+		return true;
+	endif;
+	return false;
 }
-
-function qx_page($pagename)
-{
+function qx_page($pagename) {
     $pagefile = qx_var_template_dir() . "/$pagename.php";
-    if (!file_exists($pagefile))
-        show_error(qx_msg_s("error.qxmissingpage"), $pagefile);
-    require_once qx_var_template_dir() . "/header.php";
+    if(!file_exists($pagefile)):
+		show_error(qx_msg_s('error.qxmissingpage'),$pagefile);
+	endif;
+    require_once qx_var_template_dir() . '/header.php';
     require_once "$pagefile";
-    require_once qx_var_template_dir() . "/footer.php";
+    require_once qx_var_template_dir() . '/footer.php';
     exit;
 }
-
-function qx_request($var, $default)
-{
-  return isset($_REQUEST[$var]) ? $_REQUEST[$var] : $default;
+function qx_request($var,$default) {
+  return $_REQUEST[$var] ?? $default;
 }
-?>
