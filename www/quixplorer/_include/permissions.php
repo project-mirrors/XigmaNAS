6806983d3e450540faf304d8b3ec496fe85a9871
Michael Schneider <ms49434@users.noreply.github.com>
2020-04-01 21:57:45 +0000

    remove obsolete code
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/11.3.0.4@7464 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/quixplorer/_include/permissions.php
+++ www/quixplorer/_include/permissions.php
@@ -34,7 +34,7 @@
 	of the authors and should not be interpreted as representing official policies
 	of XigmaNAS®, either expressed or implied.
 */
-require_once("./_include/session.php");
+require_once('./_include/session.php');
 
 /**
 	This functions creates the list of assignments of
@@ -43,18 +43,16 @@ require_once("./_include/session.php");
 */
 function permissions_get ()
 {
-	static $perms = array (
-			"read"		=> 0x0001,
-			"create"	=> 0x0002,
-			"change"	=> 0x0004,
-			"delete"	=> 0x0008,
-			"password"	=> 0x0040,
-			"admin"		=> 0x8000,	// admin
-			);
+	static $perms = [
+		'read'		=> 0x0001,
+		'create'	=> 0x0002,
+		'change'	=> 0x0004,
+		'delete'	=> 0x0008,
+		'password'	=> 0x0040,
+		'admin'		=> 0x8000
+	];
 	return $perms;
 }
-
-
 /**
   The permission engine.
 
@@ -63,7 +61,7 @@ function permissions_get ()
 
   @param $dir	Directory in which  the action should happen. If this parameter is
   		NULL the engine checks the global permissions of the user.
-  		
+
   @param $file	File on which the action should happen, if this parameter is NULL
   		the permission engine checks the user permissions on the directory.
 
@@ -71,7 +69,7 @@ function permissions_get ()
   		One ore more action of the action set (see permissions_get) which sould
   		be exectuted.
 		More actions are seperated by a &.
-		
+
 		Example:
 
 		"read&write&password" grants only if user has all three permissions
@@ -85,74 +83,25 @@ function permissions_get ()
 		be chaned if the permission engine will support this features in
 		the future.
   */
-function permissions_grant ($dir, $file, $action)
-{
-	// determine if a user has logged in
-	$user = session_get("s_user");
-
-	// if no user is logged in, use the global permissions
-	if (!isset($user))
-		return permissions_global($dir, $file, $action);
-
-	// check if the user currently logged in has the given rights
-	return permissions_grant_user($user, $dir, $file, $action);
-}
-
-/**
-  This function return the global permission settings.
-
-  The global permission settings forbid any access as long
-  as the require login setting is set to true.
-
-  Otherwise, the global permission settings allow that function
-  defined in the configuration variable 'global_permissions'
-  in conf.php
-  */
-function permissions_global ($dir, $file, $action)
-{
-	// check if login is required
-	if ($GLOBALS['require_login'] == true)
-		return false;
-
-	// if no login is required, get the global permissions
-	$permissions = $GLOBALS['global_permissions'];
-
-	// if the global permissions are undefined, nothing
-	// is allowed
-	if (! isset($permissions))
-		return false;
-
+function permissions_grant_user($user,$dir,$file,$action) {
+	$permissions = user_get_permissions($user);
+//	determine the permission definitions
 	$permdefs = permissions_get();
-
-	// check if this action is allowed by the global permissions
-	return $permissions & $permdefs[$action];
+//	check if the action is allowed
+	return ($permdefs[$action] & $permissions) !== 0;
 }
-
-function permissions_grant_all ($dir, $file, $actions)
-{
-	foreach ($actions as $action)
-	{
-		if (!permissions_grant($dir, $file, $action))
+function permissions_grant($dir,$file,$action) {
+	$user = Session::getUserName();
+	if($user === false):
+		return false;
+	endif;
+	return permissions_grant_user($user,$dir,$file,$action);
+}
+function permissions_grant_all($dir,$file,$actions) {
+	foreach($actions as $action):
+		if(!permissions_grant($dir,$file,$action)):
 			return false;
-	}
-
+		endif;
+	endforeach;
 	return true;
 }
-
-function permissions_grant_user ($user, $dir, $file, $action)
-{
-	// determine the user permissions of the given user
-	$permissions = user_get_permissions($user);
-
-	// determine the permission definitions
-	$permdefs = permissions_get();
-
-	// the user with the name "admin" always has admin rights
-	if ($action == "admin" && $user == "admin")
-		return true;
-
-	// check if the action is allowed
-	return ($permdefs[$action] & $permissions) != 0;
-}
-
-?>
