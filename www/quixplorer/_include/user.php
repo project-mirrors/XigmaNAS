180dfc83f91b1be7ec8a78e0ca47daaf357bffbc
Michael Schneider <ms49434@users.noreply.github.com>
2020-04-01 22:34:37 +0000

    remove obsolete code
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/11.3.0.4@7475 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/quixplorer/_include/user.php
+++ www/quixplorer/_include/user.php
@@ -47,46 +47,14 @@ function _idx($what) {
  *	Loads the user database for authenticating the users
  *	@param string $file The name of the file containing the user database. Default is ./_config/.htusers.php
 */
-function user_load($file = NULL) {
+function user_load($file = null) {
 	if(!isset($file)):
 		$file = './_config/.htusers.php';
 	endif;
 	if(!is_readable($file)):
-		show_error("user database $file does not exist or is not readable.<p>See the installation manual for details");
+		show_error(gtext('User database does not exist or is not readable.'));
 	endif;
 	require $file;
-}
-/**
- *	Write user configuration to .htusers.php
- *	@return boolean Return true if successful, otherwise false
- */
-function _saveUsers() {
-	$cnt = count($GLOBALS['users']);
-	if($cnt > 1):
-		sort($GLOBALS['users']);
-	endif;
-//	prepare file
-	$content = ['<?php' . PHP_EOL];
-	$content[] = '//	created by saveusers' . PHP_EOL;
-	$content[] = '$GLOBALS["users"] = [];' . PHP_EOL;
-	for($i = 0;$i < $cnt;++$i):
-		$content[] = sprintf('$GLOBALS["%s"][] = [\'%s\',\'%s\',\'%s\',\'%s\',%u,\'%s\',%u,%u];' . PHP_EOL,
-			'users',
-			$GLOBALS['users'][$i][0],
-			$GLOBALS['users'][$i][1],
-			$GLOBALS['users'][$i][2],
-			$GLOBALS['users'][$i][3],
-			$GLOBALS['users'][$i][4],
-			$GLOBALS['users'][$i][5],
-			$GLOBALS['users'][$i][6],
-			$GLOBALS['users'][$i][7]
-		);
-	endfor;
-//	write to file
-	if(false === file_put_contents('./_config/.htusers.php',$content)):
-		return false;
-	endif;
-	return true;
 }
 /**
  *	Returns the index of the user in the user configuration
@@ -115,33 +83,24 @@ function user_get_index($user) {
 /**
  *	Try to find the user with the username $user and the password $pass
  *	in the user table.
- *	If you provide NULL as password, no password and user active check
+ *	If you provide null as password, no password and user active check
  *	is done. otherwise, this function returns the user, if $pass matches
  *	the user password and the user is active.
- *	If the user is inactive or the password mismatches, NULL is returned.
+ *	If the user is inactive or the password mismatches, null is returned.
  *	@param string $user
  *	@param string $pass
  *	@return array
  */
-function user_find($user,$pass = NULL) {
+function user_find(string $user) {
+//	find user
 	$idx = user_get_index($user);
 	if($idx < 0):
-		return;
-	endif;
-//	if no password check should be done, return the user
-	if(!isset($pass)):
-		return $GLOBALS['users'][$idx];
-	endif;
-//	check if the password matches
-	$userpw = $GLOBALS['users'][$idx][_idx('password')];
-	if(!password_verify($pass,$userpw)):
 		return;
 	endif;
 //	check if the user is active
 	if(!$GLOBALS['users'][$idx][_idx('useractive')]):
 		return;
 	endif;
-//	return the user if all checks are passed
 	return $GLOBALS['users'][$idx];
 }
 /**
@@ -157,17 +116,14 @@ function user_find($user,$pass = NULL) {
  *	@param string $pass Password of the user to authenticate
  *	@return boolean
  */
-function user_activate($user,$pass) {
-//	try to find and authenticate the user.
-	$data = user_find($user,$pass);
-//	if the user could not be authenticated, return false.
+function user_activate(string $user) {
+//	try to find the user.
+	$data = user_find($user);
+//	if the user cannot be found, return false.
 	if(!isset($data)):
 		return false;
 	endif;
 //	store the user data in the globals variable
-	$_SESSION['s_user']	= $data[0];
-//	$_SESSION['s_pass']	= $data[1];
-	$_SESSION['s_pass']	= base64_encode($pass);
 	$GLOBALS['home_dir']	= $data[2];
 	$GLOBALS['home_url']	= $data[3];
 	$GLOBALS['show_hidden']	= $data[4];
@@ -176,59 +132,16 @@ function user_activate($user,$pass) {
 	return true;
 }
 /**
- *	Updates the user data for the given user.
- *	@param string $user
- *	@param array $new_data
- *	@return type
- */
-function user_update($user,$new_data) {
-	$idx = user_get_index($user);
-	if($idx < 0):
-		return;
-	endif;
-	$data = $new_data;
-	$GLOBALS['users'][$idx] = $new_data;
-	return _saveUsers();
-}
-/**
- *	Adds a new user to the user database.
- *	@param array $data
- *	@return boolean
- */
-function user_add($data) {
-	if(user_find($data[0],NULL)):
-		return false;
-	endif;
-	$GLOBALS['users'][] = $data;
-	return _saveUsers();
-}
-/**
- *	Removes the user with the given user name from the user database.
- *	@param string $user
- *	@return boolean
- */
-function user_remove($user) {
-//	copy valid users
-	$cnt = count($GLOBALS['users']);
-	for($i = 0;$i < $cnt;++$i):
-		if($GLOBALS['users'][$i][0] != $user):
-			$save_users[] = $GLOBALS['users'][$i];
-		endif;
-	endfor;
-	$GLOBALS['users'] = $save_users;
-	return _saveUsers();
-}
-/**
  *	This function returns the permission values of the user with the given user name.
  *	if the user is not found in the user database, this function returns
- *	NULL, otherwise, it returns the permissions of the user.
+ *	null, otherwise, it returns the permissions of the user.
  *	@param string $username
  *	@return int
  */
 function user_get_permissions($username) {
 //	try to find the user in the user database
-	$data = user_find($username,NULL);
-//	return NULL if the user does not exist
+	$data = user_find($username,null);
+//	return null if the user does not exist
 	if(!isset($data)):
 		return;
 	endif;
