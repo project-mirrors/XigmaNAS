6d182210595996879b57e27699feb75ec68b772e
Michael Zoon <zoon01@users.noreply.github.com>
2020-06-30 21:15:35 +0000

    update kernel patches
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/11.4.0.4@7613 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/kernel-patches/swap_pager/files/patch-sys__vm__swap_pager.c
+++ build/kernel-patches/swap_pager/files/patch-sys__vm__swap_pager.c
@@ -1,11 +1,11 @@
---- sys/vm/swap_pager.c.orig	2019-09-23 17:46:32.489287000 +0200
-+++ sys/vm/swap_pager.c	2019-09-23 18:22:22.000000000 +0200
+--- /usr/src/sys/vm/swap_pager.c.orig	2020-06-30 21:25:26.465587000 +0200
++++ /usr/src/sys/vm/swap_pager.c	2020-06-30 23:11:43.000000000 +0200
 @@ -860,7 +860,7 @@
  	VM_OBJECT_WLOCK(object);
  	while (size) {
  		if (n == 0) {
 -			n = BLIST_MAX_ALLOC;
-+			n = min(BLIST_MAX_ALLOC, size); /* Happy with small size */
++			n = min(BLIST_MAX_ALLOC, size); /* Happy with small size for XigmaNAS*/
  			while ((blk = swp_pager_getswapspace(n)) == SWAPBLK_NONE) {
  				n >>= 1;
  				if (n == 0) {
