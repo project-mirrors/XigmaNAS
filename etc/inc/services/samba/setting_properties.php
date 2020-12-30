adf2f1e253c59efdf138b761eb9988a79658e41f
Michael Schneider <ms49434@users.noreply.github.com>
2020-12-30 17:12:26 +0000

    change description
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@7895 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/services/samba/setting_properties.php
+++ etc/inc/services/samba/setting_properties.php
@@ -64,11 +64,20 @@ final class setting_properties extends grid_properties {
 		$options = [
 			'' => gettext('Default'),
 			'SMB3' => gettext('SMB3'),
-			'SMB3_11' => gettext('SMB3_11 (Windows 10)'),
+			'SMB3_11' => gettext('SMB3 (Windows 10)'),
+//			'SMB3_10' => gettext('SMB3 (Early technical preview of Windows 10'),
 			'SMB3_02' => gettext('SMB3 (Windows 8.1)'),
 			'SMB3_00' => gettext('SMB3 (Windows 8)'),
 			'SMB2' => gettext('SMB2'),
+//			'SMB2_24' => gettext('SMB2 (Windows 8 beta SMB2 version)'),
+//			'SMB2_22' => gettext('SMB2 (Early Windows 8 SMB2 version)'),
+//			'SMB2_10' => gettext('SMB2 (Windows 7 SMB2 version)'),
+//			'SMB2_02' => gettext('SMB2 (The earliest SMB2 version)'),
 			'NT1' => gettext('NT1 (CIFS)')
+//			'LANMAN2' => gettext('LANMAN2 (Updates to Lanman1 protocol)'),
+//			'LANMAN1' => gettext('LANMAN1 (First modern version of the protocol. Long filename support)'),
+//			'COREPLUS' => gettext('COREPLUS (Slight improvements on CORE for efficiency)'),
+//			'CORE' => gettext('CORE (Earliest version. No concept of user names)')
 		];
 		return $options;
 	}
