6dd2c3dc33e6bd5994ad1ccd3a926554ee6595ad
Michael Schneider <ms49434@users.noreply.github.com>
2021-03-10 18:23:01 +0000

    backport
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@8132 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/services_sshd.php
+++ www/services_sshd.php
@@ -31,6 +31,7 @@
 	of the authors and should not be interpreted as representing official policies
 	of XigmaNAS®, either expressed or implied.
 */
+
 require_once 'auth.inc';
 require_once 'guiconfig.inc';
 require_once 'autoload.php';
@@ -41,8 +42,8 @@ use services\sshd\shared_toolbox;
 //	init indicators
 $input_errors = [];
 //	preset $savemsg when a reboot is pending
-if(\file_exists($d_sysrebootreqd_path)):
-	$savemsg = \get_std_save_message(0);
+if(file_exists($d_sysrebootreqd_path)):
+	$savemsg = get_std_save_message(0);
 endif;
 //	init properties, sphere and rmo
 $cop = toolbox::init_properties();
@@ -63,15 +64,15 @@ $a_referer = [
 	$cop->get_loglevel(),
 	$cop->get_auxparam()
 ];
-$pending_changes = \updatenotify_exists($sphere->get_notifier());
+$pending_changes = updatenotify_exists($sphere->get_notifier());
 list($page_method,$page_action,$page_mode) = $rmo->validate();
 switch($page_method):
 	case 'SESSION':
 		switch($page_action):
 			case $sphere->get_script()->get_basename():
-				$retval = \filter_var($_SESSION[$sphere->get_script()->get_basename()],FILTER_VALIDATE_INT,['options' => ['default' => 0]]);
+				$retval = filter_var($_SESSION[$sphere->get_script()->get_basename()],FILTER_VALIDATE_INT,['options' => ['default' => 0]]);
 				unset($_SESSION['submit'],$_SESSION[$sphere->get_script()->get_basename()]);
-				$savemsg = \get_std_save_message($retval);
+				$savemsg = get_std_save_message($retval);
 				if($retval !== 0):
 					$page_action = 'edit';
 					$page_mode = PAGE_MODE_EDIT;
@@ -86,14 +87,14 @@ switch($page_method):
 		switch($page_action):
 			case 'apply':
 				$retval = 0;
-				$retval |= \updatenotify_process($sphere->get_notifier(),$sphere->get_notifier_processor());
-				\config_lock();
-				$retval |= \rc_update_service('sshd');
-				$retval |= \rc_update_service('mdnsresponder');
-				\config_unlock();
+				$retval |= updatenotify_process($sphere->get_notifier(),$sphere->get_notifier_processor());
+				config_lock();
+				$retval |= rc_update_service('sshd');
+				$retval |= rc_update_service('mdnsresponder');
+				config_unlock();
 				$_SESSION['submit'] = $sphere->get_script()->get_basename();
 				$_SESSION[$sphere->get_script()->get_basename()] = $retval;
-				\header($sphere->get_script()->get_location());
+				header($sphere->get_script()->get_location());
 				exit;
 				break;
 /*
@@ -102,12 +103,12 @@ switch($page_method):
 				$name = $cop->get_enable()->get_name();
 				$sphere->grid[$name] ??= false;
 				if($sphere->grid[$name] && !$pending_changes):
-					\config_lock();
-					$retval |= \rc_update_service('sshd',true);
-					\config_unlock();
+					config_lock();
+					$retval |= rc_update_service('sshd',true);
+					config_unlock();
 					$_SESSION['submit'] = $sphere->get_script()->get_basename();
 					$_SESSION[$sphere->get_script()->get_basename()] = $retval;
-					\header($sphere->get_script()->get_location());
+					header($sphere->get_script()->get_location());
 				else:
 					$page_action = 'view';
 					$page_mode = PAGE_MODE_VIEW;
@@ -120,13 +121,13 @@ switch($page_method):
 				$name = $cop->get_enable()->get_name();
 				$sphere->grid[$name] ??= false;
 				if($sphere->grid[$name] && !$pending_changes):
-					\config_lock();
-					$retval |= \rc_update_service('sshd');
-					$retval |= \rc_update_service('mdnsresponder');
-					\config_unlock();
+					config_lock();
+					$retval |= rc_update_service('sshd');
+					$retval |= rc_update_service('mdnsresponder');
+					config_unlock();
 					$_SESSION['submit'] = $sphere->get_script()->get_basename();
 					$_SESSION[$sphere->get_script()->get_basename()] = $retval;
-					\header($sphere->get_script()->get_location());
+					header($sphere->get_script()->get_location());
 					exit;
 				else:
 					$page_action = 'view';
@@ -139,19 +140,20 @@ switch($page_method):
 				$sphere->grid[$name] ??= false;
 				if($sphere->grid[$name]):
 					$sphere->grid[$name] = false;
-					\write_config();
-					\config_lock();
-					$retval |= \rc_update_service('sshd');
-					$retval |= \rc_update_service('mdnsresponder');
-					\config_unlock();
+					write_config();
+					config_lock();
+					$retval |= rc_update_service('sshd');
+					$retval |= rc_update_service('mdnsresponder');
+					config_unlock();
 					$_SESSION['submit'] = $sphere->get_script()->get_basename();
 					$_SESSION[$sphere->get_script()->get_basename()] = $retval;
-					\header($sphere->get_script()->get_location());
+					header($sphere->get_script()->get_location());
 					exit;
 				else:
 					$page_action = 'view';
 					$page_mode = PAGE_MODE_VIEW;
 				endif;
+				break;
 			case 'enable':
 				$retval = 0;
 				$name = $cop->get_enable()->get_name();
@@ -161,14 +163,14 @@ switch($page_method):
 					$page_mode = PAGE_MODE_VIEW;
 				else:
 					$sphere->grid[$name] = true;
-					\write_config();
-					\config_lock();
-					$retval |= \rc_update_service('sshd');
-					$retval |= \rc_update_service('mdnsresponder');
-					\config_unlock();
+					write_config();
+					config_lock();
+					$retval |= rc_update_service('sshd');
+					$retval |= rc_update_service('mdnsresponder');
+					config_unlock();
 					$_SESSION['submit'] = $sphere->get_script()->get_basename();
 					$_SESSION[$sphere->get_script()->get_basename()] = $retval;
-					\header($sphere->get_script()->get_location());
+					header($sphere->get_script()->get_location());
 					exit;
 				endif;
 				break;
@@ -184,13 +186,13 @@ switch($page_action):
 			$name = $referer->get_name();
 			switch($name):
 				case $cop->get_auxparam()->get_name():
-					if(\array_key_exists($name,$source) && is_array($source[$name])):
-						$source[$name] = \implode("\n",$source[$name]);
+					if(array_key_exists($name,$source) && is_array($source[$name])):
+						$source[$name] = implode("\n",$source[$name]);
 					endif;
 					break;
 				case $cop->get_rawprivatekey()->get_name():
 //					decode value from privatekey
-					$rawprivatekey = \base64_decode($source[$cop->get_privatekey()->get_name()] ?? '');
+					$rawprivatekey = base64_decode($source[$cop->get_privatekey()->get_name()] ?? '');
 					if(false !== $rawprivatekey):
 						$source[$name] = $rawprivatekey;
 					else:
@@ -199,8 +201,8 @@ switch($page_action):
 					break;
 			endswitch;
 			$sphere->row[$name] = $referer->validate_array_element($source);
-			if(\is_null($sphere->row[$name])):
-				if(\array_key_exists($name,$source) && \is_scalar($source[$name])):
+			if(is_null($sphere->row[$name])):
+				if(array_key_exists($name,$source) && is_scalar($source[$name])):
 					$sphere->row[$name] = $source[$name];
 				else:
 					$sphere->row[$name] = $referer->get_defaultvalue();
@@ -213,9 +215,9 @@ switch($page_action):
 		foreach($a_referer as $referer):
 			$name = $referer->get_name();
 			$sphere->row[$name] = $referer->validate_input();
-			if(\is_null($sphere->row[$name])):
+			if(is_null($sphere->row[$name])):
 				$input_errors[] = $referer->get_message_error();
-				if(\array_key_exists($name,$source) && \is_scalar($source[$name])):
+				if(array_key_exists($name,$source) && is_scalar($source[$name])):
 					$sphere->row[$name] = $source[$name];
 				else:
 					$sphere->row[$name] = $referer->get_defaultvalue();
@@ -229,12 +231,12 @@ switch($page_action):
 					case $cop->get_auxparam()->get_name():
 						$auxparam_grid = [];
 						foreach(explode("\n",$sphere->row[$name]) as $auxparam_row):
-							$auxparam_grid[] = \trim($auxparam_row,"\t\n\r");
+							$auxparam_grid[] = trim($auxparam_row,"\t\n\r");
 						endforeach;
 						$sphere->row[$name] = $auxparam_grid;
 						break;
 					case $cop->get_rawprivatekey()->get_name():
-						$privatekey = \base64_encode($sphere->row[$name]);
+						$privatekey = base64_encode($sphere->row[$name]);
 //						switch to privatekey field
 						$name = $cop->get_privatekey()->get_name();
 						$sphere->row[$name] = $privatekey;
@@ -242,9 +244,9 @@ switch($page_action):
 				endswitch;
 				$sphere->grid[$name] = $sphere->row[$name];
 			endforeach;
-			\write_config();
-			\updatenotify_set($sphere->get_notifier(),UPDATENOTIFY_MODE_MODIFIED,'SERVICE',$sphere->get_notifier_processor());
-			\header($sphere->get_script()->get_location());
+			write_config();
+			updatenotify_set($sphere->get_notifier(),UPDATENOTIFY_MODE_MODIFIED,'SERVICE',$sphere->get_notifier_processor());
+			header($sphere->get_script()->get_location());
 			exit;
 		else:
 			$page_mode = PAGE_MODE_EDIT;
@@ -252,15 +254,15 @@ switch($page_action):
 		break;
 endswitch;
 //	determine final page mode and calculate readonly flag
-list($page_mode,$is_readonly) = \calc_skipviewmode($page_mode);
+list($page_mode,$is_readonly) = calc_skipviewmode($page_mode);
 $is_enabled = $sphere->row[$cop->get_enable()->get_name()];
-$is_running = (0 === \rc_is_service_running('sshd'));
-$is_running_message = $is_running ? \gettext('Yes') : \gettext('No');
-$input_errors_found = \count($input_errors) > 0;
-$pgtitle = [\gettext('Services'),\gettext('SSH')];
-$n_auxparam_rows = \min(64,\max(5,1 + \substr_count($sphere->row[$cop->get_auxparam()->get_name()],PHP_EOL)));
-$n_rawprivatekey_rows = \min(64,\max(5,1 + \substr_count($sphere->row[$cop->get_rawprivatekey()->get_name()],PHP_EOL)));
-$document = \new_page($pgtitle,$sphere->get_script()->get_scriptname());
+$is_running = (rc_is_service_running('sshd') === 0);
+$is_running_message = $is_running ? gettext('Yes') : gettext('No');
+$input_errors_found = count($input_errors) > 0;
+$pgtitle = [gettext('Services'),gettext('SSH')];
+$n_auxparam_rows = min(64,max(5,1 + substr_count($sphere->row[$cop->get_auxparam()->get_name()],PHP_EOL)));
+$n_rawprivatekey_rows = min(64,max(5,1 + substr_count($sphere->row[$cop->get_rawprivatekey()->get_name()],PHP_EOL)));
+$document = new_page($pgtitle,$sphere->get_script()->get_scriptname());
 //	add tab navigation
 shared_toolbox::add_tabnav($document);
 //	get areas
@@ -280,7 +282,7 @@ endif;
 $tds = $content->add_table_data_settings();
 $tds->ins_colgroup_data_settings();
 $thead = $tds->addTHEAD();
-$title = \gettext('SSH');
+$title = gettext('SSH');
 switch($page_mode):
 	case PAGE_MODE_VIEW:
 		$thead->c2_titleline($title);
@@ -291,7 +293,7 @@ switch($page_mode):
 endswitch;
 $tbody = $tds->addTBODY();
 $tbody->
-	c2_textinfo('running',\gettext('Service Active'),$is_running_message)->
+	c2_textinfo('running',gettext('Service Active'),$is_running_message)->
 	c2_input_text($cop->get_port(),$sphere,false,$is_readonly)->
 	c2_checkbox($cop->get_allowpa(),$sphere,false,$is_readonly)->
 	c2_checkbox($cop->get_allowcra(),$sphere,false,$is_readonly)->
