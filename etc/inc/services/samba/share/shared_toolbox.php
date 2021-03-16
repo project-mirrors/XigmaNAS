d0f39d868f842ba152b74ab174badeaf6e724505
Michael Schneider <ms49434@users.noreply.github.com>
2021-03-16 18:10:18 +0000

    update gui code
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@8170 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/services/samba/share/shared_toolbox.php
+++ etc/inc/services/samba/share/shared_toolbox.php
@@ -34,6 +34,7 @@
 
 namespace services\samba\share;
 
+use DOMDocument;
 use common\arr;
 use common\sphere as mys;
 
@@ -93,10 +94,10 @@ final class shared_toolbox {
 	}
 /**
  *	Add the tab navigation menu of this sphere
- *	@param \co_DOMDocument $document
+ *	@param DOMDocument $document
  *	@return int
  */
-	public static function add_tabnav(\co_DOMDocument $document) {
+	public static function add_tabnav(DOMDocument $document) {
 		$retval = 0;
 		$document->
 			add_area_tabnav()->
