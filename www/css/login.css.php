d61a7b245ffbbe85dcba45288009b9aa73c81ed1
Michael Schneider <ms49434@users.noreply.github.com>
2021-03-17 16:42:26 +0000

    cleanup css code
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@8205 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/css/login.css.php
+++ www/css/login.css.php
@@ -31,57 +31,10 @@
 	of the authors and should not be interpreted as representing official policies
 	of XigmaNAS®, either expressed or implied.
 */
+
+require_once 'autoload.php';
 require_once 'config.inc';
 
-header('Content-type: text/css');
-$config_subkey = 'cssloginfile';
-$filemode_subkey = 'cssloginfilemode';
-$css_default_filename = sprintf('%1$s/css/%2$s',$g['www_path'],'login.css');
-$webgui_settings = &array_make_branch($config,'system','webgui');
-$css_filemode = filter_var($webgui_settings[$filemode_subkey],FILTER_VALIDATE_REGEXP,['flags' => FILTER_REQUIRE_SCALAR,'options' => ['default' => '','regexp' => '/^(|append|replace)$/']]);
-$css_custom_filename = filter_var($webgui_settings[$config_subkey],FILTER_VALIDATE_REGEXP,['flags' => FILTER_REQUIRE_SCALAR,'options' => ['default' => NULL,'regexp' => '/\S/']]);
-switch($css_filemode):
-	default:
-		if(file_exists($css_default_filename)):
-			$css_content = file_get_contents($css_default_filename);
-			if(false !== $css_content):
-				echo $css_content;
-			endif;
-		endif;
-		break;
-	case 'append':
-		if(file_exists($css_default_filename)):
-			$css_content = file_get_contents($css_default_filename);
-			if(false !== $css_content):
-				echo $css_content;
-			endif;
-		endif;
-//		append customizations
-		if(isset($css_custom_filename) && file_exists($css_custom_filename)):
-			$css_content = file_get_contents($css_custom_filename);
-			if(false !== $css_content):
-				echo "/*\n\tCustomizations\n*/\n",$css_content;
-			endif;
-		endif;
-		break;
-	case 'replace':
-		$css_content = null;
-		if(isset($css_custom_filename) && file_exists($css_custom_filename)):
-			$css_content = file_get_contents($css_custom_filename);
-			if(false === $css_content):
-				$css_content = NULL;
-			else:
-				echo "/*\n\tCustom CSS\n*/\n",$css_content;
-			endif;
-		endif;
-//		fallback
-		if(is_null($css_content)):
-			if(file_exists($css_default_filename)):
-				$css_content = file_get_contents($css_default_filename);
-				if(false !== $css_content):
-					echo $css_content;
-				endif;
-			endif;
-		endif;
-		break;
-endswitch;
+use gui\css;
+
+css::render('cssloginfile','cssloginfilemode','login.css');
