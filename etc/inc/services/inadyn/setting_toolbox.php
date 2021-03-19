752124b85f00c58d9228bea364daf98e9bc5a502
Michael Schneider <ms49434@users.noreply.github.com>
2021-03-19 02:17:47 +0000

    add reload and restart options
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/11.4.0.4@8212 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/services/inadyn/setting_toolbox.php
+++ etc/inc/services/inadyn/setting_toolbox.php
@@ -70,7 +70,7 @@ final class setting_toolbox {
 			add('POST','apply',PAGE_MODE_VIEW)->
 			add('POST','edit',PAGE_MODE_EDIT)->
 			add('POST','reload',PAGE_MODE_VIEW)->
-//			add('POST','restart',PAGE_MODE_VIEW)->
+			add('POST','restart',PAGE_MODE_VIEW)->
 			add('POST','save',PAGE_MODE_POST)->
 			add('POST','view',PAGE_MODE_VIEW)->
 			add('SESSION',$sphere->get_script()->get_basename(),PAGE_MODE_VIEW);
