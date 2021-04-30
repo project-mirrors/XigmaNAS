292d4717b214ef60d36e9470fc55db55a0e6e0b3
Michael Schneider <ms49434@users.noreply.github.com>
2021-04-30 17:05:53 +0000

    add scheduler for scrub
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/11.4.0.4@8387 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

---
+++ etc/inc/disks/zfs/scheduler/pool/scrub/grid_toolbox.php
@@ -0,0 +1,291 @@
+<?php
+/*
+	grid_toolbox.php
+
+	Part of XigmaNAS® (https://www.xigmanas.com).
+	Copyright © 2018-2021 XigmaNAS® <info@xigmanas.com>.
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice, this
+	   list of conditions and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the above copyright notice,
+	   this list of conditions and the following disclaimer in the documentation
+	   and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+	The views and conclusions contained in the software and documentation are those
+	of the authors and should not be interpreted as representing official policies
+	of XigmaNAS®, either expressed or implied.
+*/
+
+namespace disks\zfs\scheduler\pool\scrub;
+
+use common\properties as myp,
+	common\rmo as myr,
+	common\sphere as mys;
+use const FILTER_VALIDATE_INT,
+	UPDATENOTIFY_MODE_DIRTY,
+	UPDATENOTIFY_MODE_DIRTY_CONFIG;
+use function count,
+	file_exists,
+	filter_var,
+	get_std_save_message,
+	gettext,
+	header,
+	is_bool,
+	new_page,
+	updatenotify_cbm_delete,
+	updatenotify_cbm_disable,
+	updatenotify_cbm_enable,
+	updatenotify_cbm_toggle,
+	updatenotify_exists,
+	updatenotify_get_mode,
+	updatenotify_process,
+	write_config;
+
+/**
+ *	Wrapper class for autoloading functions
+ */
+final class grid_toolbox {
+/**
+ *	Create the sphere object
+ *	@return \common\sphere\grid
+ */
+	public static function init_sphere() {
+		$sphere = new mys\grid();
+		shared_toolbox::init_sphere($sphere);
+		$sphere->
+			set_script('disks_zfs_scheduler_pool_scrub')->
+			set_modify('disks_zfs_scheduler_pool_scrub_edit')->
+			setmsg_sym_add(gettext('Add Schedule'))->
+			setmsg_sym_mod(gettext('Edit Schedule'))->
+			setmsg_sym_del(gettext('Schedule is marked for deletion'))->
+			setmsg_sym_loc(gettext('Schedule is locked'))->
+			setmsg_sym_unl(gettext('Schedule is unlocked'))->
+			setmsg_cbm_delete(gettext('Delete Selected Schedules'))->
+			setmsg_cbm_delete_confirm(gettext('Do you want to delete selected schedules?'))->
+			setmsg_cbm_disable(gettext('Disable Selected Schedules'))->
+			setmsg_cbm_disable_confirm(gettext('Do you want to disable selected schedules?'))->
+			setmsg_cbm_enable(gettext('Enable Selected Schedules'))->
+			setmsg_cbm_enable_confirm(gettext('Do you want to enable selected schedules?'))->
+			setmsg_cbm_toggle(gettext('Toggle Selected Schedules'))->
+			setmsg_cbm_toggle_confirm(gettext('Do you want to toggle selected schedules?'));
+		return $sphere;
+	}
+/**
+ *	Create the request method object
+ *	@param grid_properties $cop
+ *	@param mys\grid $sphere
+ *	@return \common\rmo\rmo The request method object
+ */
+	public static function init_rmo(grid_properties $cop,mys\grid $sphere) {
+		$rmo = myr\rmo_grid_templates::rmo_base($cop,$sphere);
+		return $rmo;
+	}
+/**
+ *	Create the property object
+ *	@return grid_properties
+ */
+	public static function init_properties() {
+		$cop = new grid_properties();
+		return $cop;
+	}
+/**
+ *	Render the page
+ *	@global array $input_errors
+ *	@global string $errormsg
+ *	@global string $savemsg
+ *	@param grid_properties $cop
+ *	@param \common\sphere\grid $sphere
+ */
+	public static function render(grid_properties $cop,mys\grid $sphere) {
+		global $input_errors;
+		global $errormsg;
+		global $savemsg;
+
+		$pgtitle = [gettext('Disks'),gettext('ZFS'),gettext('Scheduler'),gettext('Pool'),gettext('Scrub')];
+		$record_exists = count($sphere->grid) > 0;
+		$morethanonerecord = count($sphere->grid) > 1;
+		$a_col_width = ['5%','15%','15%','45%','10%','10%'];
+		$n_col_width = count($a_col_width);
+		if($morethanonerecord):
+			$document = new_page($pgtitle,$sphere->get_script()->get_scriptname(),'tablesort');
+		else:
+			$document = new_page($pgtitle,$sphere->get_script()->get_scriptname());
+		endif;
+//		get areas
+		$body = $document->getElementById('main');
+		$pagecontent = $document->getElementById('pagecontent');
+//		add tab navigation
+		shared_toolbox::add_tabnav($document);
+//		create data area
+		$content = $pagecontent->add_area_data();
+//		display information, warnings and errors
+		$content->
+			ins_input_errors($input_errors)->
+			ins_info_box($savemsg)->
+			ins_error_box($errormsg);
+		if(updatenotify_exists($sphere->get_notifier())):
+			$content->ins_config_has_changed_box();
+		endif;
+//		add content
+		$table = $content->add_table_data_selection();
+		$table->ins_colgroup_with_styles('width',$a_col_width);
+		$thead = $table->addTHEAD();
+		$tbody = $table->addTBODY();
+		$tfoot = $table->addTFOOT();
+		$thead->ins_titleline(gettext('Overview'),$n_col_width);
+		$tr = $thead->addTR();
+		if($morethanonerecord):
+			$tr->
+				push()->
+				addTHwC('lhelc sorter-false parser-false')->
+					ins_cbm_checkbox_toggle($sphere)->
+				pop()->
+				insTHwC('lhell',$cop->get_poolname()->get_title())->
+				insTHwC('lhell',$cop->get_preset()->get_title())->
+				insTHwC('lhell',$cop->get_description()->get_title())->
+				insTHwC('lhelc sorter-image',gettext('Status'))->
+				insTHwC('lhebl sorter-false parser-false',$cop->get_toolbox()->get_title());
+		else:
+			$tr->
+				insTHwC('lhelc')->
+				insTHwC('lhell',$cop->get_poolname()->get_title())->
+				insTHwC('lhell',$cop->get_preset()->get_title())->
+				insTHwC('lhell',$cop->get_description()->get_title())->
+				insTHwC('lhelc',gettext('Status'))->
+				insTHwC('lhebl',$cop->get_toolbox()->get_title());
+		endif;
+		if($record_exists):
+			$preset_name = $cop->get_preset()->get_name();
+			$preset_options = $cop->get_preset()->get_options();
+			foreach($sphere->grid as $sphere->row_id => $sphere->row):
+				$notificationmode = updatenotify_get_mode($sphere->get_notifier(),$sphere->get_row_identifier_value());
+				$is_notdirty = (UPDATENOTIFY_MODE_DIRTY != $notificationmode) && (UPDATENOTIFY_MODE_DIRTY_CONFIG != $notificationmode);
+				$is_enabled = $sphere->is_enadis_enabled() ? (is_bool($test = $sphere->row[$cop->get_enable()->get_name()] ?? false) ? $test : true) : true;
+				$is_notprotected = $sphere->is_lock_enabled() ? !(is_bool($test = $sphere->row[$cop->get_protected()->get_name()] ?? false) ? $test : true) : true;
+				$dc = $is_enabled ? '' : 'd';
+				if(array_key_exists($sphere->row[$preset_name],$preset_options)):
+					$preset_value = $preset_options[$sphere->row[$preset_name]];
+				else:
+					$preset_value = gettext('Invalid');
+				endif;
+				$tbody->
+					addTR()->
+						push()->
+						addTDwC('lcelc' . $dc)->
+							ins_cbm_checkbox($sphere,!($is_notdirty && $is_notprotected))->
+						pop()->
+						insTDwC('lcell' . $dc,$sphere->row[$cop->get_poolname()->get_name()] ?? '')->
+						insTDwC('lcell' . $dc,$preset_value)->
+						insTDwC('lcell' . $dc,$sphere->row[$cop->get_description()->get_name()] ?? '')->
+						ins_enadis_icon($is_enabled)->
+						add_toolbox_area()->
+							ins_toolbox($sphere,$is_notprotected,$is_notdirty)->
+							ins_maintainbox($sphere,false)->
+							ins_informbox($sphere,false);
+			endforeach;
+		else:
+			$tfoot->ins_no_records_found($n_col_width);
+		endif;
+		$tfoot->ins_record_add($sphere,$n_col_width);
+		$document->
+			add_area_buttons()->
+				ins_cbm_button_enadis($sphere)->
+				ins_cbm_button_delete($sphere);
+//		additional javascript code
+		$body->ins_javascript($sphere->get_js());
+		$body->add_js_on_load($sphere->get_js_on_load());
+		$body->add_js_document_ready($sphere->get_js_document_ready());
+		$document->render();
+	}
+/**
+ *	process request method
+ *	@global string $d_sysrebootreqd_path
+ *	@global array $input_errors
+ *	@global string $errormsg
+ *	@global string $savemsg
+ *	@param \common\properties\container $cop
+ *	@param \common\sphere\root $sphere
+ *	@param \common\rmo\rmo $rmo
+ */
+	final public static function looper(myp\container $cop,mys\root $sphere,myr\rmo $rmo) {
+		global $d_sysrebootreqd_path;
+		global $input_errors;
+		global $errormsg;
+		global $savemsg;
+
+//		preset $savemsg in case a reboot is pending
+		if(file_exists($d_sysrebootreqd_path)):
+			$savemsg = get_std_save_message(0);
+		endif;
+		[$page_method,$page_action,$page_mode] = $rmo->validate();
+		switch($page_method):
+			case 'SESSION':
+				switch($page_action):
+					case $sphere->get_script()->get_basename():
+//						catch error code
+						$retval = filter_var($_SESSION[$sphere->get_script()->get_basename()],FILTER_VALIDATE_INT,['options' => ['default' => 0]]);
+						unset($_SESSION['submit'],$_SESSION[$sphere->get_script()->get_basename()]);
+						$savemsg = get_std_save_message($retval);
+						break;
+				endswitch;
+				break;
+			case 'POST':
+				switch($page_action):
+					case 'apply':
+						$retval = 0;
+						$retval |= updatenotify_process($sphere->get_notifier(),$sphere->get_notifier_processor());
+						config_lock();
+						$retval |= rc_update_service('cron');
+						config_unlock();
+						$_SESSION['submit'] = $sphere->get_script()->get_basename();
+						$_SESSION[$sphere->get_script()->get_basename()] = $retval;
+						header($sphere->get_script()->get_location());
+						exit;
+						break;
+					case $sphere->get_cbm_button_val_delete():
+						updatenotify_cbm_delete($sphere,$cop);
+						header($sphere->get_script()->get_location());
+						exit;
+						break;
+					case $sphere->get_cbm_button_val_toggle():
+						if(updatenotify_cbm_toggle($sphere,$cop)):
+							write_config();
+						endif;
+						header($sphere->get_script()->get_location());
+						exit;
+						break;
+					case $sphere->get_cbm_button_val_enable():
+						if(updatenotify_cbm_enable($sphere,$cop)):
+							write_config();
+						endif;
+						header($sphere->get_script()->get_location());
+						exit;
+						break;
+					case $sphere->get_cbm_button_val_disable():
+						if(updatenotify_cbm_disable($sphere,$cop)):
+							write_config();
+						endif;
+						header($sphere->get_script()->get_location());
+						exit;
+						break;
+				endswitch;
+				break;
+		endswitch;
+	}
+}
