2b632764c77928111a9165de5377b3a2a8d0a55e
Michael Schneider <ms49434@users.noreply.github.com>
2021-09-09 15:58:53 +0000

    move page title
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@8473 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/system_backup_settings.php
+++ www/system_backup_settings.php
@@ -31,9 +31,10 @@
 	of the authors and should not be interpreted as representing official policies
 	of XigmaNAS®, either expressed or implied.
 */
+
+require_once 'autoload.php';
 require_once 'auth.inc';
 require_once 'guiconfig.inc';
-require_once 'autoload.php';
 
 use system\backup\setting_toolbox as toolbox;
 use system\backup\shared_toolbox;
@@ -41,25 +42,25 @@ use system\backup\shared_toolbox;
 //	init indicators
 $input_errors = [];
 //	preset $savemsg when a reboot is pending
-if(\file_exists($d_sysrebootreqd_path)):
-	$savemsg = \get_std_save_message(0);
+if(file_exists($d_sysrebootreqd_path)):
+	$savemsg = get_std_save_message(0);
 endif;
 //	init properties, sphere and rmo
 $cop = toolbox::init_properties();
 $sphere = toolbox::init_sphere();
 $rmo = toolbox::init_rmo($cop,$sphere);
-$a_referer = [
+$cops = [
 	$cop->get_reminderintervalshow()
 ];
-$pending_changes = \updatenotify_exists($sphere->get_notifier());
+$pending_changes = updatenotify_exists($sphere->get_notifier());
 list($page_method,$page_action,$page_mode) = $rmo->validate();
 switch($page_method):
 	case 'SESSION':
 		switch($page_action):
 			case $sphere->get_script()->get_basename():
-				$retval = \filter_var($_SESSION[$sphere->get_script()->get_basename()],FILTER_VALIDATE_INT,['options' => ['default' => 0]]);
+				$retval = filter_var($_SESSION[$sphere->get_script()->get_basename()],FILTER_VALIDATE_INT,['options' => ['default' => 0]]);
 				unset($_SESSION['submit'],$_SESSION[$sphere->get_script()->get_basename()]);
-				$savemsg = \get_std_save_message($retval);
+				$savemsg = get_std_save_message($retval);
 				if($retval !== 0):
 					$page_action = 'edit';
 					$page_mode = PAGE_MODE_EDIT;
@@ -74,10 +75,10 @@ switch($page_method):
 		switch($page_action):
 			case 'apply':
 				$retval = 0;
-				$retval |= \updatenotify_process($sphere->get_notifier(),$sphere->get_notifier_processor());
+				$retval |= updatenotify_process($sphere->get_notifier(),$sphere->get_notifier_processor());
 				$_SESSION['submit'] = $sphere->get_script()->get_basename();
 				$_SESSION[$sphere->get_script()->get_basename()] = $retval;
-				\header($sphere->get_script()->get_location());
+				header($sphere->get_script()->get_location());
 				exit;
 				break;
 		endswitch;
@@ -88,54 +89,54 @@ switch($page_action):
 	case 'edit':
 	case 'view':
 		$source = $sphere->grid;
-		foreach($a_referer as $referer):
-			$name = $referer->get_name();
-			switch($name):
-				case 'auxparam':
-					if(\array_key_exists($name,$source)):
-						if(\is_array($source[$name])):
-							$source[$name] = \implode("\n",$source[$name]);
+		foreach($cops as $cops_element):
+			$name = $cops_element->get_name();
+			switch($cops_element->get_input_type()):
+				case 'textarea':
+					if(array_key_exists($name,$source)):
+						if(is_array($source[$name])):
+							$source[$name] = implode("\n",$source[$name]);
 						endif;
 					endif;
 					break;
 			endswitch;
-			$sphere->row[$name] = $referer->validate_array_element($source);
-			if(\is_null($sphere->row[$name])):
-				if(\array_key_exists($name,$source) && \is_scalar($source[$name])):
+			$sphere->row[$name] = $cops_element->validate_array_element($source);
+			if(is_null($sphere->row[$name])):
+				if(array_key_exists($name,$source) && is_scalar($source[$name])):
 					$sphere->row[$name] = $source[$name];
 				else:
-					$sphere->row[$name] = $referer->get_defaultvalue();
+					$sphere->row[$name] = $cops_element->get_defaultvalue();
 				endif;
 			endif;
 		endforeach;
 		break;
 	case 'save':
 		$source = $_POST;
-		foreach($a_referer as $referer):
-			$name = $referer->get_name();
-			$sphere->row[$name] = $referer->validate_input();
-			if(\is_null($sphere->row[$name])):
-				$input_errors[] = $referer->get_message_error();
-				if(\array_key_exists($name,$source) && \is_scalar($source[$name])):
+		foreach($cops as $cops_element):
+			$name = $cops_element->get_name();
+			$sphere->row[$name] = $cops_element->validate_input();
+			if(is_null($sphere->row[$name])):
+				$input_errors[] = $cops_element->get_message_error();
+				if(array_key_exists($name,$source) && is_scalar($source[$name])):
 					$sphere->row[$name] = $source[$name];
 				else:
-					$sphere->row[$name] = $referer->get_defaultvalue();
+					$sphere->row[$name] = $cops_element->get_defaultvalue();
 				endif;
 			endif;
 		endforeach;
 		if(empty($input_errors)):
-			foreach($a_referer as $referer):
-				$name = $referer->get_name();
-				switch($name):
-					case 'auxparam':
-						$sphere->row[$name] = \array_map(function($element) { return \trim($element,"\n\r\t"); },\explode("\n",$sphere->row[$name]));
+			foreach($cops as $cops_element):
+				$name = $cops_element->get_name();
+				switch($cops_element->get_input_type()):
+					case 'textarea':
+						$sphere->row[$name] = array_map(fn($element) => trim($element,"\n\r\t"),explode("\n",$sphere->row[$name]));
 						break;
 				endswitch;
 				$sphere->grid[$name] = $sphere->row[$name];
 			endforeach;
-			\write_config();
-			\updatenotify_set($sphere->get_notifier(),UPDATENOTIFY_MODE_MODIFIED,'SERVICE',$sphere->get_notifier_processor());
-			\header($sphere->get_script()->get_location());
+			write_config();
+			updatenotify_set($sphere->get_notifier(),UPDATENOTIFY_MODE_MODIFIED,'SERVICE',$sphere->get_notifier_processor());
+			header($sphere->get_script()->get_location());
 			exit;
 		else:
 			$page_mode = PAGE_MODE_EDIT;
@@ -143,11 +144,10 @@ switch($page_action):
 		break;
 endswitch;
 //	determine final page mode and calculate readonly flag
-list($page_mode,$is_readonly) = \calc_skipviewmode($page_mode);
-$input_errors_found = \count($input_errors) > 0;
+list($page_mode,$is_readonly) = calc_skipviewmode($page_mode);
+$input_errors_found = count($input_errors) > 0;
 //	create document
-$pgtitle = [\gettext('System'),\gettext('Backup Configuration'),\gettext('Settings')];
-$document = \new_page($pgtitle,$sphere->get_script()->get_scriptname());
+$document = new_page($sphere->get_page_title(),$sphere->get_script()->get_scriptname());
 //	add tab navigation
 shared_toolbox::add_tabnav($document);
 //	get areas
@@ -169,7 +169,7 @@ $content->
 		push()->
 		ins_colgroup_data_settings()->
 		addTHEAD()->
-			c2_titleline(\gettext('Reminder Settings'))->
+			c2_titleline(gettext('Reminder Settings'))->
 		pop()->
 		addTBODY()->
 			c2_input_text($cop->get_reminderintervalshow(),$sphere,false,$is_readonly);
