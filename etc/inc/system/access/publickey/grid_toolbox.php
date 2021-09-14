ad89ce40e95983e0b18644fa6afa3dd1f796bd12
Michael Schneider <ms49434@users.noreply.github.com>
2021-09-14 16:16:19 +0000

    flush repository, sync codebase
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/12.2.0.4@8539 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/system/access/publickey/grid_toolbox.php
+++ etc/inc/system/access/publickey/grid_toolbox.php
@@ -37,11 +37,31 @@ namespace system\access\publickey;
 use common\properties as myp,
 	common\rmo as myr,
 	common\sphere as mys;
-use function count,file_exists,filter_var,gettext,header,is_bool,config_lock,
-		config_unlock,get_std_save_message,new_page,rc_exec_service,
-		updatenotify_cbm_delete,updatenotify_cbm_disable,
-		updatenotify_cbm_enable,updatenotify_cbm_toggle,updatenotify_exists,
-		updatenotify_get_mode,updatenotify_process,write_config;
+
+use const FILTER_VALIDATE_INT,
+	PAGE_MODE_POST,
+	UPDATENOTIFY_MODE_DIRTY,
+	UPDATENOTIFY_MODE_DIRTY_CONFIG;
+
+use function config_lock,
+	config_unlock,
+	count,
+	file_exists,
+	filter_var,
+	get_std_save_message,
+	gettext,
+	header,
+	is_bool,
+	new_page,
+	rc_exec_service,
+	updatenotify_cbm_delete,
+	updatenotify_cbm_disable,
+	updatenotify_cbm_enable,
+	updatenotify_cbm_toggle,
+	updatenotify_exists,
+	updatenotify_get_mode,
+	updatenotify_process,
+	write_config;
 
 /**
  *	Wrapper class for autoloading functions
@@ -104,15 +124,14 @@ final class grid_toolbox {
 		global $errormsg;
 		global $savemsg;
 
-		$pgtitle = [gettext('Access'),gettext('Public Keys')];
 		$record_exists = count($sphere->grid) > 0;
 		$use_tablesort = count($sphere->grid) > 1;
 		$a_col_width = ['5%','20%','10%','55%','10%'];
 		$n_col_width = count($a_col_width);
 		if($use_tablesort):
-			$document = new_page($pgtitle,$sphere->get_script()->get_scriptname(),'tablesort');
+			$document = new_page($sphere->get_page_title(),$sphere->get_script()->get_scriptname(),'tablesort');
 		else:
-			$document = new_page($pgtitle,$sphere->get_script()->get_scriptname());
+			$document = new_page($sphere->get_page_title(),$sphere->get_script()->get_scriptname());
 		endif;
 //		get areas
 		$body = $document->getElementById('main');
