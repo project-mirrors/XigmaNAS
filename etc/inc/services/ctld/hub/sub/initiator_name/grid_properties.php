6503548354ace7add53cb6142f0974c50af58f7e
Michael Schneider <ms49434@users.noreply.github.com>
2021-09-19 16:49:19 +0000

    move pgtitle, flush local repository
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/11.4.0.4@8579 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/services/ctld/hub/sub/initiator_name/grid_properties.php
+++ etc/inc/services/ctld/hub/sub/initiator_name/grid_properties.php
@@ -31,7 +31,9 @@
 	of the authors and should not be interpreted as representing official policies
 	of XigmaNAS®, either expressed or implied.
 */
+
 namespace services\ctld\hub\sub\initiator_name;
+
 use common\properties as myp;
 
 class grid_properties extends myp\container_row {
