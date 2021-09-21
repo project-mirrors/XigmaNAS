89fb17f416b409fecec78db67d76c8a58853b5c0
Michael Schneider <ms49434@users.noreply.github.com>
2021-09-21 16:00:42 +0000

    use c2 hub, move pgtitle
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@8606 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/system/sysctl/grid_toolbox.php
+++ etc/inc/system/sysctl/grid_toolbox.php
@@ -36,13 +36,24 @@ namespace system\sysctl;
 
 use common\properties as myp;
 use common\rmo as myr;
-
 use common\sphere as mys;
-use function count,file_exists,filter_var,gettext,header,is_bool,config_lock,
-		config_unlock,get_std_save_message,new_page,rc_update_service,
-		updatenotify_cbm_delete,updatenotify_cbm_disable,
-		updatenotify_cbm_enable,updatenotify_cbm_toggle,updatenotify_exists,
-		updatenotify_get_mode,updatenotify_process,write_config;
+
+use const UPDATENOTIFY_MODE_DIRTY;
+use const UPDATENOTIFY_MODE_DIRTY_CONFIG;
+
+use function config_lock;
+use function config_unlock;
+use function get_std_save_message;
+use function new_page;
+use function rc_update_service;
+use function updatenotify_cbm_delete;
+use function updatenotify_cbm_disable;
+use function updatenotify_cbm_enable;
+use function updatenotify_cbm_toggle;
+use function updatenotify_exists;
+use function updatenotify_get_mode;
+use function updatenotify_process;
+use function write_config;
 
 /**
  *	Wrapper class for autoloading functions
@@ -104,15 +115,14 @@ final class grid_toolbox {
 		global $errormsg;
 		global $savemsg;
 
-		$pgtitle = [gettext('System'),gettext('Advanced'),gettext('sysctl.conf')];
 		$record_exists = count($sphere->grid) > 0;
 		$use_tablesort = count($sphere->grid) > 1;
 		$a_col_width = ['5%','30%','20%','10%','25%','10%'];
 		$n_col_width = count($a_col_width);
 		if($use_tablesort):
-			$document = new_page($pgtitle,$sphere->get_script()->get_scriptname(),'tablesort');
+			$document = new_page($sphere->get_page_title(),$sphere->get_script()->get_scriptname(),'tablesort');
 		else:
-			$document = new_page($pgtitle,$sphere->get_script()->get_scriptname());
+			$document = new_page($sphere->get_page_title(),$sphere->get_script()->get_scriptname());
 		endif;
 //		get areas
 		$body = $document->getElementById('main');
