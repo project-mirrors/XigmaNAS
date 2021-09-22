19da4df676ec9663f827ad35eb269f4eee828b42
Michael Schneider <ms49434@users.noreply.github.com>
2021-09-22 15:42:57 +0000

    use c2 hub, move pgtitle
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/12.2.0.4@8619 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/services/minidlnad/media/grid_toolbox.php
+++ etc/inc/services/minidlnad/media/grid_toolbox.php
@@ -38,11 +38,22 @@ use common\properties as myp;
 use common\rmo as myr;
 use common\sphere as mys;
 
-use function count,file_exists,filter_var,gettext,header,is_bool,
-		config_lock,config_unlock,get_std_save_message,new_page,
-		rc_update_service,updatenotify_cbm_delete,updatenotify_cbm_disable,
-		updatenotify_cbm_enable,updatenotify_cbm_toggle,updatenotify_exists,
-		updatenotify_get_mode,updatenotify_process,write_config;
+use const UPDATENOTIFY_MODE_DIRTY;
+use const UPDATENOTIFY_MODE_DIRTY_CONFIG;
+
+use function config_lock;
+use function config_unlock;
+use function get_std_save_message;
+use function new_page;
+use function rc_update_service;
+use function updatenotify_cbm_delete;
+use function updatenotify_cbm_disable;
+use function updatenotify_cbm_enable;
+use function updatenotify_cbm_toggle;
+use function updatenotify_exists;
+use function updatenotify_get_mode;
+use function updatenotify_process;
+use function write_config;
 
 /**
  *	Wrapper class for autoloading functions
@@ -50,7 +61,7 @@ use function count,file_exists,filter_var,gettext,header,is_bool,
 final class grid_toolbox {
 /**
  *	Create the sphere object
- *	@return \common\sphere\grid
+ *	@return mys\grid
  */
 	public static function init_sphere() {
 		$sphere = new mys\grid();
@@ -77,7 +88,7 @@ final class grid_toolbox {
  *	Create the request method object
  *	@param grid_properties $cop
  *	@param mys\grid $sphere
- *	@return \common\rmo\rmo The request method object
+ *	@return myr\rmo The request method object
  */
 	public static function init_rmo(grid_properties $cop,mys\grid $sphere) {
 		$rmo = myr\rmo_grid_templates::rmo_base($cop,$sphere);
@@ -97,22 +108,21 @@ final class grid_toolbox {
  *	@global string $errormsg
  *	@global string $savemsg
  *	@param grid_properties $cop
- *	@param \common\sphere\grid $sphere
+ *	@param mys\grid $sphere
  */
 	public static function render(grid_properties $cop,mys\grid $sphere) {
 		global $input_errors;
 		global $errormsg;
 		global $savemsg;
 
-		$pgtitle = [gettext('Services'),gettext('MiniDLNA'),gettext('Media Folder')];
 		$record_exists = count($sphere->grid) > 0;
 		$use_tablesort = count($sphere->grid) > 1;
 		$a_col_width = ['5%','25%','25%','10%','25%','10%'];
 		$n_col_width = count($a_col_width);
 		if($use_tablesort):
-			$document = new_page($pgtitle,$sphere->get_script()->get_scriptname(),'tablesort');
+			$document = new_page($sphere->get_page_title(),$sphere->get_script()->get_scriptname(),'tablesort');
 		else:
-			$document = new_page($pgtitle,$sphere->get_script()->get_scriptname());
+			$document = new_page($sphere->get_page_title(),$sphere->get_script()->get_scriptname());
 		endif;
 		//	add tab navigation
 		shared_toolbox::add_tabnav($document);
@@ -199,9 +209,9 @@ final class grid_toolbox {
  *	@global array $input_errors
  *	@global string $errormsg
  *	@global string $savemsg
- *	@param \common\properties\container $cop
- *	@param \common\sphere\root $sphere
- *	@param \common\rmo\rmo $rmo
+ *	@param myp\container $cop
+ *	@param mys\root $sphere
+ *	@param myr\rmo $rmo
  */
 	final public static function looper(myp\container $cop,mys\root $sphere,myr\rmo $rmo) {
 		global $d_sysrebootreqd_path;
