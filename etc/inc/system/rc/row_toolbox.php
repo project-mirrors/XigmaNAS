7b9a86c5ef64c37a9aeca7cab5ceaace65355e19
Michael Schneider <ms49434@users.noreply.github.com>
2021-09-22 17:41:22 +0000

    use c2 hub, move pgtitle
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/11.4.0.4@8621 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/system/rc/row_toolbox.php
+++ etc/inc/system/rc/row_toolbox.php
@@ -31,10 +31,12 @@
 	of the authors and should not be interpreted as representing official policies
 	of XigmaNAS®, either expressed or implied.
 */
+
 namespace system\rc;
 
 use common\rmo as myr;
 use common\sphere as mys;
+
 /**
  *	Wrapper class for autoloading functions
  */
@@ -42,7 +44,7 @@ final class row_toolbox {
 /**
  *	Create the sphere object
  *	@global array $config
- *	@return \common\sphere\row The sphere object
+ *	@return mys\row The sphere object
  */
 	public static function init_sphere() {
 		global $config;
@@ -56,7 +58,7 @@ final class row_toolbox {
 	}
 /**
  *	Create the request method object
- *	@return \common\rmo\rmo The request method object
+ *	@return myr\rmo The request method object
  */
 	public static function init_rmo() {
 		$rmo = myr\rmo_row_templates::rmo_with_clone();
@@ -64,7 +66,7 @@ final class row_toolbox {
 	}
 /**
  *	Create the properties object
- *	@return \system\rc\row_properties The properties object
+ *	@return row_properties The properties object
  */
 	public static function init_properties() {
 		$cop = new row_properties();
