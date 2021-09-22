5b34f9b3303774fa77122e52558eefc1aa691ed2
Michael Schneider <ms49434@users.noreply.github.com>
2021-09-22 19:49:00 +0000

    use c2 hub, move pgtitle
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/12.2.0.4@8627 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/services/ctld/hub/sub/lun/grid_properties.php
+++ etc/inc/services/ctld/hub/sub/lun/grid_properties.php
@@ -36,8 +36,6 @@ namespace services\ctld\hub\sub\lun;
 
 use common\properties as myp;
 
-use function gettext;
-
 class grid_properties extends myp\container_row {
 	protected $x_number;
 	public function init_number(): myp\property_int {
