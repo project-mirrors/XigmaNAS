89203085407d80fe3303ac4a34ea57a0561707d1
Michael Schneider <ms49434@users.noreply.github.com>
2021-09-23 00:35:23 +0000

    use c2 hub, move pgtitle
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/11.4.0.4@8630 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/services/ctld/hub/sub/port/row_properties.php
+++ etc/inc/services/ctld/hub/sub/port/row_properties.php
@@ -31,7 +31,9 @@
 	of the authors and should not be interpreted as representing official policies
 	of XigmaNAS®, either expressed or implied.
 */
+
 namespace services\ctld\hub\sub\port;
+
 use common\properties as myp;
 
 final class row_properties extends grid_properties {
@@ -47,7 +49,7 @@ final class row_properties extends grid_properties {
 			set_maxlength(223)->
 			set_filter(FILTER_VALIDATE_REGEXP)->
 			set_filter_flags(FILTER_REQUIRE_SCALAR)->
-			set_filter_options(['default' => NULL,'regexp' => $regexp]);
+			set_filter_options(['default' => null,'regexp' => $regexp]);
 		return $property;
 	}
 	public function init_group(): myp\property_list_multi {
