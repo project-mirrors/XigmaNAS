3bb62974c9b8c89a10b2dbb82cc805a42fac6344
Michael Schneider <ms49434@users.noreply.github.com>
2021-09-29 04:28:41 +0000

    class changes
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/11.4.0.4@8673 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/services/websrv/webdav/row_toolbox.php
+++ etc/inc/services/websrv/webdav/row_toolbox.php
@@ -41,7 +41,7 @@ use common\toolbox as myt;
 /**
  *	Wrapper class for autoloading functions
  */
-final class row_toolbox extends myt\row_toolbox {
+class row_toolbox extends myt\row_toolbox {
 /**
  *	Create the sphere object
  *	@return mys\row
