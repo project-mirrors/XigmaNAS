558de0c969a1426bf6196913f4e2707bca616b1f
Michael Schneider <ms49434@users.noreply.github.com>
2021-10-12 16:18:05 +0000

    grant full access to users with admin rights.
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@8704 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/fm_user.php
+++ etc/inc/fm_user.php
@@ -1,6 +1,6 @@
 <?php
 /*
-	user.php
+	fm_user.php
 
 	Part of XigmaNAS® (https://www.xigmanas.com).
 	Copyright © 2018-2021 XigmaNAS® <info@xigmanas.com>.
@@ -34,6 +34,11 @@
 	of the authors and should not be interpreted as representing official policies
 	of XigmaNAS®, either expressed or implied.
 */
+
+require_once 'autoload.php';
+
+use common\arr;
+
 /**
  *	Activate the user with the given user name.
  *	this function tries to find the user with the given user name
@@ -49,35 +54,34 @@
 function user_activate(string $user) {
 	global $config;
 
-	$sphere = array_make_branch($config,'access','user');
-//	lookup user
-	$sphere_rowid = array_search_ex($user,$sphere,'login');
-	if($sphere_rowid !== false):
-		$sphere_row = $sphere[$sphere_rowid];
-		if(is_bool($test = $sphere_row['fm_enable'] ?? false) ? $test : true):
-			if( is_string($sphere_row['homedir'])
-				&&
-				(preg_match('/\S/',$sphere_row['homedir']) === 1)
-			):
-				$GLOBALS['home_dir'] = $sphere_row['homedir'];
-			else:
-				$GLOBALS['home_dir'] = '/mnt';
-			endif;
-			$GLOBALS['home_url'] = sprintf('%s://%s/%s',$config['system']['webgui']['protocol'] ?? 'http',$_SERVER['HTTP_HOST'] ?? 'localhost',$user);
-			if(is_bool($test = $sphere_row['fmp_show_hidden_items'] ?? false) ? $test : true):
-				$GLOBALS['show_hidden']	= 1;
-			else:
-				$GLOBALS['show_hidden']	= 0;
-			endif;
-			$GLOBALS['no_access'] = '^\.ht';
-			return true;
-		endif;
-	elseif(Session::isAdmin()):
+	if(Session::isAdmin()):
 		$GLOBALS['home_dir'] = '/';
 		$GLOBALS['home_url'] = sprintf('%s://%s/%s',$config['system']['webgui']['protocol'] ?? 'http',$_SERVER['HTTP_HOST'] ?? 'localhost',$user);
 		$GLOBALS['show_hidden']	= 1;
 		$GLOBALS['no_access'] = '';
 		return true;
+	else:
+		$sphere = arr::make_branch($config,'access','user');
+//		lookup user
+		$sphere_rowid = arr::search_ex($user,$sphere,'login');
+		if($sphere_rowid !== false):
+			$sphere_row = $sphere[$sphere_rowid];
+			$test = $sphere_row['fm_enable'] ?? false;
+			$is_enabled = is_bool($test) ? $test : true;
+			if($is_enabled):
+				if(is_string($sphere_row['homedir']) && (preg_match('/\S/',$sphere_row['homedir']) === 1)):
+					$GLOBALS['home_dir'] = $sphere_row['homedir'];
+				else:
+					$GLOBALS['home_dir'] = '/mnt';
+				endif;
+				$GLOBALS['home_url'] = sprintf('%s://%s/%s',$config['system']['webgui']['protocol'] ?? 'http',$_SERVER['HTTP_HOST'] ?? 'localhost',$user);
+				$test = $sphere_row['fmp_show_hidden_items'] ?? false;
+				$show_hidden = is_bool($test) ? $test : true;
+				$GLOBALS['show_hidden']	= $show_hidden ? 1 : 0;
+				$GLOBALS['no_access'] = '^\.ht';
+				return true;
+			endif;
+		endif;
 	endif;
 	return false;
 }
