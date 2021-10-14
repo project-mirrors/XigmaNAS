d62d8f023d85f8f76fc23ef5971bca5e7a1f779b
Michael Schneider <ms49434@users.noreply.github.com>
2021-10-14 18:56:08 +0000

    use session class
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@8709 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/common/session.php
+++ etc/inc/common/session.php
@@ -37,73 +37,103 @@ namespace common;
 /*
 	$_SESSION
 
-	access.hidesystemgroups -> boolean Hide/show system groups in Access > Groups
-	access.hidesystemusers --> boolean Hide/Show system users in Access > Users
+	@start
+
 	admin -------------------> boolean True when logged in user has admin rights, false otherwise
 	at ----------------------> int     Timestamp
 	authtoken ---------------> string  The authentication token
+	login -------------------> boolean True when user logged in successfully
+	ts ----------------------> int     Timestamp
+	tz ----------------------> string  Timezone
+	uid ---------------------> string  User ID
+	uname -------------------> string  Login name of the user
+
+	@runtime
+
+	access.hidesystemgroups -> boolean Hide/show system groups in Access > Groups
+	access.hidesystemusers --> boolean Hide/Show system users in Access > Users
 	cpu ---------------------> array
 	dev ---------------------> array
 	filter_time_id ----------> string  Filter for snapshot overview in disks_zfs_snapshot.php
 	g -----------------------> array   Global array containing session related information
 		headermenu ----------> array   Cache for header menu
 		shrinkpageheader ----> boolean Set this to true to suppress the header
-	login -------------------> boolean True when user logged in successfully
+	kern.cp_times.{id} ------> system_get_cpu_usage
+	kern.cp_times.{id} ------> system_get_smp_cpu_usage
 	{scriptname} ------------> int     Return value of 'Apply' action
 	submit ------------------> string  Name of the script submitting the form
-	ts ----------------------> int     Timestamp
-	tz ----------------------> string  Timezone
-	uid ---------------------> string  User ID
-	uname -------------------> string  Login name of the user
 	userlang ----------------> string  Language
 */
 
 class session {
 /**
- *	Initialize default session variables.
+ *	Start session.
  */
-	private static function _init() {
+	public static function start() {
+		switch(session_status()):
+//			case PHP_SESSION_DISABLED:
+//				break;
+			case PHP_SESSION_NONE:
+				session_start();
+				$_SESSION['at'] = time();
+				break;
+//			case PHP_SESSION_ACTIVE:
+//				break;
+		endswitch;
+	}
+	public static function commit() {
+		switch(session_status()):
+//			case PHP_SESSION_DISABLED:
+//				break;
+//			case PHP_SESSION_NONE:
+//				break;
+			case PHP_SESSION_ACTIVE:
+				session_write_close();
+				break;
+		endswitch;
+	}
+/**
+ *	Destroy session.
+ */
+	public static function destroy() {
+		switch(session_status()):
+//			case PHP_SESSION_DISABLED:
+//				break;
+			case PHP_SESSION_NONE:
+				session_start();
+				session_destroy();
+				break;
+			case PHP_SESSION_ACTIVE:
+				session_destroy();
+				break;
+		endswitch;
+	}
+/**
+ *	Initialize user.
+ *	@param string $uid The user ID
+ *	@param string $uname The user name
+ *	@param bool $admin
+ *	@param string $timezone
+ */
+	public static function init(string $uid,string $uname,bool $admin = false,string $timezone = null) {
 		session_regenerate_id(true);
-		$_SESSION['login'] = true;
 		$_SESSION['authtoken'] = bin2hex(random_bytes(32));
+		$_SESSION['admin'] = $admin;
+		$_SESSION['login'] = true;
 		$_SESSION['ts'] = time();
+		$_SESSION['uid'] = $uid;
+		$_SESSION['uname'] = $uname;
+		$_SESSION['tz'] = $timezone;
 		$_SESSION['g'] = [
 			'shrinkpageheader' => false,
 			'headermenu' => []
 		];
 	}
 /**
- *	Start session.
- *	@return bool
- */
-	public static function start() {
-		session_start();
-		$_SESSION['at'] = time();
-	}
-/**
- *	Destroy session.
- */
-	public static function destroy() {
-		session_start();
-		session_destroy();
-	}
-/**
- *	Initialize user.
- *	@param uid The user ID.
- *	@param uname The user name.
- */
-	public static function initUser($uid,$uname,bool $admin = false,$timezone = null) {
-		static::_init();
-		$_SESSION['uid'] = $uid;
-		$_SESSION['uname'] = $uname;
-		$_SESSION['admin'] = $admin;
-		$_SESSION['tz'] = $timezone;
-	}
-/**
  *	Has the current user administration permissions?
  *	@return true if the current user has administration permissions, otherwise false.
  */
-	public static function isAdmin() {
+	public static function is_admin() {
 		if(!isset($_SESSION['admin']) || !$_SESSION['admin']):
 			return false;
 		endif;
@@ -112,7 +142,7 @@ class session {
 /**
  *	Is the login flag set?
  */
-	public static function isLogin() {
+	public static function is_login() {
 		if(!isset($_SESSION['login']) || !$_SESSION['login']):
 			return false;
 		endif;
@@ -123,7 +153,7 @@ class session {
  *	@param authtoken The token to be validated.
  *	@return true if the token is valid, otherwise false.
  */
-	public static function isValidAuthToken($authtoken) {
+	public static function is_valid_authtoken($authtoken) {
 		if(!isset($_SESSION['authtoken']) || !$_SESSION['authtoken']):
 			return false;
 		endif;
@@ -133,14 +163,14 @@ class session {
  *	Get the current authentication token.
  *	@return The current authentication token.
  */
-	public static function getAuthToken() {
+	public static function get_authtoken() {
 		return $_SESSION['authtoken'];
 	}
 /**
  *	Get the current user name.
  *	@return Returns the current user name, otherwise false.
  */
-	public static function getUserName() {
+	public static function get_user_name() {
 		if(!isset($_SESSION['uname']) || !$_SESSION['uname']):
 			return false;
 		endif;
@@ -150,7 +180,7 @@ class session {
  *	Get the current user ID.
  *	@return Returns the current user ID, otherwise false.
  */
-	public static function getUserId() {
+	public static function get_user_id() {
 		if(!isset($_SESSION['uid']) || !$_SESSION['uid']):
 			return false;
 		endif;
