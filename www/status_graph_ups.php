90f7c5d7950c50a837a6f51db8a476f4e5004201
Michael Schneider <ms49434@users.noreply.github.com>
2021-10-25 22:42:52 +0000

    use new tabnav, add sticky header
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@8734 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/status_graph_ups.php
+++ www/status_graph_ups.php
@@ -32,20 +32,16 @@
 	of XigmaNAS®, either expressed or implied.
 */
 
+require_once 'autoload.php';
 require_once 'auth.inc';
 require_once 'guiconfig.inc';
-require_once 'autoload.php';
 
 use common\arr;
+use status\monitor\shared_toolbox as myst;
 
-arr::make_branch($config,'rrdgraphs');
-$rrd_ups = true;
-$refresh = 300;
-if(isset($config['rrdgraphs']['refresh_time'])):
-	if(!empty($config['rrdgraphs']['refresh_time'])):
-		$refresh = $config['rrdgraphs']['refresh_time'];
-	endif;
-endif;
+$grid = arr::make_branch($config,'rrdgraphs');
+$refresh = empty($grid['refresh_time']) ? 300 : $grid['refresh_time'];
+$now = time();
 mwexec('/usr/local/share/rrdgraphs/rrd-graph.sh ups',true);
 $document = new_page([gettext('Status'),gettext('Monitoring'),gettext('UPS')]);
 //	get areas
@@ -53,22 +49,28 @@ $head = $document->getElementById('head');
 $pagecontent = $document->getElementById('pagecontent');
 $head->insElement('meta',['http-equiv' => 'refresh','content' => $refresh]);
 //	add tab navigation
-include 'status_graph_tabs.inc';
+myst::add_tabnav($document,myst::RRD_UPS);
 //	create data area
 $content = $pagecontent->add_area_data();
 //	display information, warnings and errors
 if(file_exists($d_sysrebootreqd_path)):
 	$content->ins_info_box(get_std_save_message(0));
 endif;
-$table = $content->add_table_data_settings();
-$table->addTHEAD()->ins_titleline(gettext('UPS'));
-$now = time();
 $content->
-	ins_remark('remark','',sprintf(gettext('Graph updates every %d seconds.'),$refresh));
-$content->
-	addDIV(['class' => 'rrdgraphs'])->
-		insIMG(['class' => 'rrdgraphs','src' => sprintf('/images/rrd/rrd-ups_daily.png?rand=%s',$now),'alt' => gettext('RRDGraphs Daily UPS Graph')])->
-		insIMG(['class' => 'rrdgraphs','src' => sprintf('/images/rrd/rrd-ups_weekly.png?rand=%s',$now),'alt' => gettext('RRDGraphs Weekly UPS Graph')])->
-		insIMG(['class' => 'rrdgraphs','src' => sprintf('/images/rrd/rrd-ups_monthly.png?rand=%s',$now),'alt' => gettext('RRDGraphs Monthly UPS Graph')])->
-		insIMG(['class' => 'rrdgraphs','src' => sprintf('/images/rrd/rrd-ups_yearly.png?rand=%s',$now),'alt' => gettext('RRDGraphs Yearly UPS Graph')]);
+	add_table_data_settings()->
+		push()->
+		addTHEAD()->
+			ins_titleline(gettext('UPS'))->
+		pop()->
+		addTBODY(['class' => 'donothighlight'])->
+			addTR()->
+				addTD()->
+					addDIV(['class' => 'rrdgraphs'])->
+						insIMG(['class' => 'rrdgraphs','src' => sprintf('/images/rrd/rrd-ups_daily.png?rand=%s',$now),'alt' => gettext('RRDGraphs Daily UPS Graph')])->
+						insIMG(['class' => 'rrdgraphs','src' => sprintf('/images/rrd/rrd-ups_weekly.png?rand=%s',$now),'alt' => gettext('RRDGraphs Weekly UPS Graph')])->
+						insIMG(['class' => 'rrdgraphs','src' => sprintf('/images/rrd/rrd-ups_monthly.png?rand=%s',$now),'alt' => gettext('RRDGraphs Monthly UPS Graph')])->
+						insIMG(['class' => 'rrdgraphs','src' => sprintf('/images/rrd/rrd-ups_yearly.png?rand=%s',$now),'alt' => gettext('RRDGraphs Yearly UPS Graph')]);
+$document->
+	add_area_buttons()->
+		ins_remark('remark','',sprintf(gettext('Graph updates every %d seconds.'),$refresh));
 $document->render();
