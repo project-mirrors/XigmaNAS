8f05b48a9204b002d9f2e8fdea2f43b599cec5b5
Michael Zoon <zoon01@users.noreply.github.com>
2021-11-23 03:33:57 +0000

    update php80 port files
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@8868 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/ports/php80/files/patch-ext_mysqli_php__mysqli__structs.h
+++ build/ports/php80/files/patch-ext_mysqli_php__mysqli__structs.h
@@ -1,6 +1,6 @@
---- ext/mysqli/php_mysqli_structs.h.orig	2021-01-05 14:54:54.000000000 +0100
-+++ ext/mysqli/php_mysqli_structs.h	2021-01-10 21:39:54.000000000 +0100
-@@ -34,7 +34,7 @@
+--- ext/mysqli/php_mysqli_structs.h.orig	2019-12-17 10:29:23 UTC
++++ ext/mysqli/php_mysqli_structs.h
+@@ -36,7 +36,7 @@
  #define FALSE 0
  #endif
  
