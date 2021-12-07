aba17b24e5ff66eaf04f87bd5d55f2b8a2074042
Michael Zoon <zoon01@users.noreply.github.com>
2021-12-07 00:33:21 +0000

    patch refresh
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/branches/12.3.0.4@8914 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/kernel-patches/swap_pager/files/patch-sys__vm__swap_pager.c
+++ build/kernel-patches/swap_pager/files/patch-sys__vm__swap_pager.c
@@ -1,5 +1,5 @@
---- sys/vm/swap_pager.c.orig	2020-12-14 01:04:08.424197000 +0100
-+++ sys/vm/swap_pager.c	2020-12-14 01:10:18.000000000 +0100
+--- sys/vm/swap_pager.c.orig	2021-12-07 01:01:45.874553000 +0100
++++ sys/vm/swap_pager.c	2021-12-07 01:31:09.000000000 +0100
 @@ -904,7 +904,7 @@
  	VM_OBJECT_WLOCK(object);
  	while (size) {
