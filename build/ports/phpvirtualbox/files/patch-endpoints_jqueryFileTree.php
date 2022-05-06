cfc6972e71757fa403a825e65fcd176bf2a31cb0
Michael Zoon <zoon01@users.noreply.github.com>
2022-05-06 12:33:12 +0000

    proper fix fileTree listing and extension filtering #299
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@9047 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/ports/phpvirtualbox/files/patch-endpoints_jqueryFileTree.php
+++ build/ports/phpvirtualbox/files/patch-endpoints_jqueryFileTree.php
@@ -1,51 +1,23 @@
---- endpoints/jqueryFileTree.php.orig	2021-11-17 08:10:37.421036000 +0100
-+++ endpoints/jqueryFileTree.php	2021-11-19 14:21:30.000000000 +0100
-@@ -227,33 +227,33 @@
+--- endpoints/jqueryFileTree.php.orig	2022-05-06 14:23:41.328996000 +0200
++++ endpoints/jqueryFileTree.php	2022-05-06 14:29:59.000000000 +0200
+@@ -223,6 +223,8 @@
+  */
+ function getdir($dir, $dirsOnly=false, $recurse=array()) {
+ 
++	global $allowed_exts;
++
+ 	if(!$dir) $dir = DSEP;
  
  	$entries = getDirEntries($dir, $dirsOnly);
- 
--    if(!count($entries))
-+    	if(!is_countable($entries))
-     	return array();
- 
--    $dirents = array();
--    foreach($entries as $path => $type) {
-+    	$dirents = array();
-+    	foreach($entries as $path => $type) {
- 
--        if($type == 'folder' && count($recurse) && (strcasecmp($recurse[0],vbox_basename($path)) == 0)) {
-+	if($type == 'folder' && is_countable($recurse) && (strcasecmp($recurse[0],vbox_basename($path)) == 0)) {
- 
--        	$entry = folder_entry($path, false, true);
-+        $entry = folder_entry($path, false, true);
- 
--            $entry['children'] = getdir($dir.DSEP.array_shift($recurse), $dirsOnly, $recurse);
-+        $entry['children'] = getdir($dir.DSEP.array_shift($recurse), $dirsOnly, $recurse);
- 
--            array_push($dirents, $entry);
-+	array_push($dirents, $entry);
- 
--        } else {
-+    } else {
- 
--        	// Push folder on to stack
--        	if($type == 'folder') {
-+	// Push folder on to stack
-+	if($type == 'folder') {
- 
--        	   array_push($dirents, folder_entry($path));
-+        array_push($dirents, folder_entry($path));
- 
--        	// Push file on to stack
--        	} else {
-+        // Push file on to stack
-+        } else {
+@@ -251,9 +253,9 @@
+         	// Push file on to stack
+         	} else {
  
 -        		$ext = strtolower(preg_replace('/^.*\./', '', $file));
-+           $ext = strtolower(preg_replace('/^.*./', '', $file));
++        		$ext = strtolower(preg_replace('/^.*\./', '', $path));
  
 -                if(count($allowed) && !$allowed['.'.$ext]) continue;
-+              if(is_countable($allowed) && !$allowed['.'.$ext]) continue;
++                if(count($allowed_exts) && !$allowed_exts['.'.$ext]) continue;
  
                  array_push($dirents, file_entry($path));
          	}
