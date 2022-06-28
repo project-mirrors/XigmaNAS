99118ccadd56d4160101a40d1f775be411bca26c
Michael Schneider <ms49434@users.noreply.github.com>
2022-06-28 19:25:24 +0000

    rewrite to DOM
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@9182 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/status_interfaces.php
+++ www/status_interfaces.php
@@ -41,115 +41,88 @@ $ifdescrs = ['lan' => 'LAN'];
 for($j = 1;isset($config['interfaces']['opt' . $j]);$j++):
 	$ifdescrs['opt' . $j] = $config['interfaces']['opt' . $j]['descr'];
 endfor;
-$pgtitle = [gtext('Status'),gtext('Interfaces')];
-include 'fbegin.inc';
-?>
-<table id="area_data"><tbody><tr><td id="area_data_frame"><form action="<?=$sphere_scriptname;?>" method="post" name="iform" id="iform">
-<?php
-	foreach($ifdescrs as $ifdescr => $ifname):
-		$sphere_record = get_interface_info_ex($ifdescr);
-?>
-		<table class="area_data_settings">
-			<colgroup>
-				<col class="area_data_settings_col_tag">
-				<col class="area_data_settings_col_data">
-			</colgroup>
-			<thead>
-<?php
-				if($show_separator):
-					html_separator2();
-				else:
-					$show_separator = true;
-				endif;
-				html_titleline2(sprintf(gettext('%s Interface'),$ifname));
-?>
-			</thead>
-			<tbody>
-<?php
-				$show_ip_details = true;
-				html_text2('interface',gettext('Interface'),$sphere_record['hwif']);
-				if(isset($sphere_record['dhcplink']) && $sphere_record['dhcplink']):
-					html_text2('dhcplink',gettext('DHCP'),$sphere_record['dhcplink']);
-					if('down' == $sphere_record['dhcplink']):
-						$show_ip_details = false;
-					endif;
-				endif;
-				if(isset($sphere_record['pppoelink']) && $sphere_record['pppoelink']):
-					html_text2('pppoelink',gettext('PPPoE'),$sphere_record['pppoelink']);
-					if('down' == $sphere_record['pppoelink']):
-						$show_ip_details = false;
-					endif;
-				endif;
-				if(isset($sphere_record['pptplink']) && $sphere_record['pptplink']):
-					html_text2('pptplink',gettext('PPTP'),$sphere_record['pptplink']);
-					if('down' == $sphere_record['pptplink']):
-						$show_ip_details = false;
-					endif;
-				endif;
-				if(isset($sphere_record['macaddr']) && $sphere_record['macaddr']):
-					html_text2('macaddr',gettext('MAC Address'),$sphere_record['macaddr']);
-				endif;
-				if('down' != $sphere_record['status']):
-					if($show_ip_details):
-						if(isset($sphere_record['ipaddr']) && $sphere_record['ipaddr']):
-							html_text2('ipaddr',gettext('IP Address'),$sphere_record['ipaddr']);
-						endif;
-						if(isset($sphere_record['subnet']) && $sphere_record['subnet']):
-							html_text2('subnet',gettext('Subnet Mask'),$sphere_record['subnet']);
-						endif;
-						if(isset($sphere_record['gateway']) && $sphere_record['gateway']):
-							html_text2('gateway',gettext('Gateway'),$sphere_record['gateway']);
-						endif;
-						if(isset($sphere_record['ipv6addr']) && $sphere_record['ipv6addr']):
-							html_text2('ipv6addr',gettext('IPv6 Address'),$sphere_record['ipv6addr']);
-						endif;
-						if(isset($sphere_record['ipv6subnet']) && $sphere_record['ipv6subnet']):
-							html_text2('ipv6subnet',gettext('IPv6 Prefix'),$sphere_record['ipv6subnet']);
-						endif;
-						if(isset($sphere_record['ipv6gateway']) && $sphere_record['ipv6gateway']):
-							html_text2('ipv6gateway',gettext('IPv6 Gateway'),$sphere_record['ipv6gateway']);
-						endif;
-						if('wan' == $ifdescr && file_exists("{$g['varetc_path']}/nameservers.conf")):
-							$filename = sprintf('%s/nameservers.conf',$g['varetc_path']);
-							$helpinghand = '<pre>' . htmlspecialchars(file_get_contents($filename)) .'</pre>';
-							html_text2('ispdnsservers',gettext('ISP DNS Servers').$helpinghand);
-						endif;
-					endif;
-					if(isset($sphere_record['media']) && $sphere_record['media']):
-						html_text2('media',gettext('Media'),$sphere_record['media']);
-					endif;
-					if(isset($sphere_record['channel']) && $sphere_record['channel']):
-						html_text2('channel',gettext('Channel'),$sphere_record['channel']);
-					endif;
-					if(isset($sphere_record['ssid']) && $sphere_record['ssid']):
-						html_text2('ssid',gettext('SSID'),$sphere_record['ssid']);
-					endif;
-					html_text2('mtu',gettext('MTU'),$sphere_record['mtu']);
-					$helpinghand = $sphere_record['inpkts'] .
-							'/' .
-							$sphere_record['outpkts'] .
-							' (' .
-							format_bytes($sphere_record['inbytes']) .
-							'/' .
-							format_bytes($sphere_record['outbytes']) .
-							')';
-					html_text2('inpkts',gettext('In/Out Packets'),$helpinghand);
-					if(isset($sphere_record['inerrs'])):
-						$helpinghand = $sphere_record['inerrs'] . '/' . $sphere_record['outerrs'];
-						html_text2('inerrs',gettext('In/Out Errors'),$helpinghand);
-					endif;
-					if(isset($sphere_record['collisions'])):
-						html_text2('collisions',gettext('Collisions'),$sphere_record['collisions']);
-					endif;
-				endif;
-				html_text2('status',gettext('Status'),$sphere_record['status']);
-?>
-			</tbody>
-		</table>
-<?php
-	endforeach;
-	include 'formend.inc';
-?>
-</form></td></tr></tbody></table>
-<?php
-include 'fend.inc';
+$pgtitle = [gettext('Status'),gettext('Interfaces')];
+$document = new_page(page_title: $pgtitle,options: 'notabnav');
+//	get areas
+$body = $document->getElementById('main');
+$pagecontent = $document->getElementById('pagecontent');
+$content = $pagecontent->add_area_data();
+foreach($ifdescrs as $ifdescr => $ifname):
+	$sphere_record = get_interface_info_ex($ifdescr);
+	$table = $content->add_table_data_settings();
+	$table->ins_colgroup_data_settings();
+	$thead = $table->addTHEAD();
+	$tbody = $table->addTBODY();
+	$thead->c2_titleline(sprintf('%s: %s',gettext('Interface'),$ifname));
+	$show_ip_details = true;
+	$tbody->c2_textinfo('interface',gettext('Interface'),$sphere_record['hwif']);
+	if(isset($sphere_record['dhcplink']) && $sphere_record['dhcplink']):
+		$tbody->c2_textinfo('dhcplink',gettext('DHCP'),$sphere_record['dhcplink']);
+		if($sphere_record['dhcplink'] === 'down'):
+			$show_ip_details = false;
+		endif;
+	endif;
+	if(isset($sphere_record['pppoelink']) && $sphere_record['pppoelink']):
+		$tbody->c2_textinfo('pppoelink',gettext('PPPoE'),$sphere_record['pppoelink']);
+		if($sphere_record['pppoelink'] === 'down'):
+			$show_ip_details = false;
+		endif;
+	endif;
+	if(isset($sphere_record['pptplink']) && $sphere_record['pptplink']):
+		$tbody->c2_textinfo('pptplink',gettext('PPTP'),$sphere_record['pptplink']);
+		if($sphere_record['pptplink'] === 'down'):
+			$show_ip_details = false;
+		endif;
+	endif;
+	if(isset($sphere_record['macaddr']) && $sphere_record['macaddr']):
+		$tbody->c2_textinfo('macaddr',gettext('MAC Address'),$sphere_record['macaddr']);
+	endif;
+	if($sphere_record['status'] !== 'down'):
+		if($show_ip_details):
+			if(isset($sphere_record['ipaddr']) && $sphere_record['ipaddr']):
+				$tbody->c2_textinfo('ipaddr',gettext('IP Address'),$sphere_record['ipaddr']);
+			endif;
+			if(isset($sphere_record['subnet']) && $sphere_record['subnet']):
+				$tbody->c2_textinfo('subnet',gettext('Subnet Mask'),$sphere_record['subnet']);
+			endif;
+			if(isset($sphere_record['gateway']) && $sphere_record['gateway']):
+				$tbody->c2_textinfo('gateway',gettext('Gateway'),$sphere_record['gateway']);
+			endif;
+			if(isset($sphere_record['ipv6addr']) && $sphere_record['ipv6addr']):
+				$tbody->c2_textinfo('ipv6addr',gettext('IPv6 Address'),$sphere_record['ipv6addr']);
+			endif;
+			if(isset($sphere_record['ipv6subnet']) && $sphere_record['ipv6subnet']):
+				$tbody->c2_textinfo('ipv6subnet',gettext('IPv6 Prefix'),$sphere_record['ipv6subnet']);
+			endif;
+			if(isset($sphere_record['ipv6gateway']) && $sphere_record['ipv6gateway']):
+				$tbody->c2_textinfo('ipv6gateway',gettext('IPv6 Gateway'),$sphere_record['ipv6gateway']);
+			endif;
+			if(($ifdescr === 'wan') && file_exists("{$g['varetc_path']}/nameservers.conf")):
+				$filename = sprintf('%s/nameservers.conf',$g['varetc_path']);
+				$helpinghand = sprintf('<pre>%s</pre>',file_get_contents($filename));
+				$tbody->c2_textinfo('ispdnsservers',gettext('ISP DNS Servers') . $helpinghand);
+			endif;
+		endif;
+		if(isset($sphere_record['media']) && $sphere_record['media']):
+			$tbody->c2_textinfo('media',gettext('Media'),$sphere_record['media']);
+		endif;
+		if(isset($sphere_record['channel']) && $sphere_record['channel']):
+			$tbody->c2_textinfo('channel',gettext('Channel'),$sphere_record['channel']);
+		endif;
+		if(isset($sphere_record['ssid']) && $sphere_record['ssid']):
+			$tbody->c2_textinfo('ssid',gettext('SSID'),$sphere_record['ssid']);
+		endif;
+		$tbody->c2_textinfo('mtu',gettext('MTU'),$sphere_record['mtu']);
+		$helpinghand = sprintf('%s/%s (%s/%s)',$sphere_record['inpkts'],$sphere_record['outpkts'],format_bytes($sphere_record['inbytes']),format_bytes($sphere_record['outbytes']));
+		$tbody->c2_textinfo('inpkts',gettext('In/Out Packets'),$helpinghand);
+		if(isset($sphere_record['inerrs'])):
+			$helpinghand = sprintf('%s/%s',$sphere_record['inerrs'],$sphere_record['outerrs']);
+			$tbody->c2_textinfo('inerrs',gettext('In/Out Errors'),$helpinghand);
+		endif;
+		if(isset($sphere_record['collisions'])):
+			$tbody->c2_textinfo('collisions',gettext('Collisions'),$sphere_record['collisions']);
+		endif;
+	endif;
+	$tbody->c2_textinfo('status',gettext('Status'),$sphere_record['status']);
+endforeach;
+$document->render();
