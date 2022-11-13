5ff69bd8985a5e850eeb68b1ce82cc2b611ee6bf
Michael Zoon <zoon01@users.noreply.github.com>
2022-11-13 18:27:54 +0000

    add icon to phpvirtualbox
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@9416 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

---
+++ build/ports/phpvirtualbox/files/patch-js_utils.js
@@ -0,0 +1,14 @@
+--- js/utils.js.orig	2022-11-12 17:35:13.407624000 +0100
++++ js/utils.js	2022-11-13 06:32:01.000000000 +0100
+@@ -249,8 +249,9 @@
+ 		case "Windows8_64":     strIcon = "os_win8_64.png"; break;
+ 		case "Windows81":       strIcon = "os_win81.png"; break;
+ 		case "Windows81_64":    strIcon = "os_win81_64.png"; break;
+-		case "Windows10":       strIcon = "os_win10.png"; break
+-		case "Windows10_64":    strIcon = "os_win10.png"; break
++		case "Windows10":       strIcon = "os_win10.png"; break;
++		case "Windows10_64":    strIcon = "os_win10_64.png"; break;
++		case "Windows11_64":    strIcon = "os_win11_64.png"; break;
+ 		case "WindowsNT":       strIcon = "os_win_other.png"; break;
+ 		case "WindowsNT_64":    strIcon = "os_win_other_64.png"; break; 
+ 		case "Windows2012_64":	strIcon = "os_win2k12_64.png"; break;
