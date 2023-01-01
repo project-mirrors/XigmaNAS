a83a8e5dda0d37a3c837bd97df268dd33b0786d6
Michael Schneider <ms49434@users.noreply.github.com>
2023-01-01 19:34:55 +0000

    Happy New Year!
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@9484 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/services_mariadb.php
+++ www/services_mariadb.php
@@ -3,7 +3,7 @@
 	services_mariadb.php
 
 	Part of XigmaNAS® (https://www.xigmanas.com).
-	Copyright © 2018-2022 XigmaNAS® <info@xigmanas.com>.
+	Copyright © 2018-2023 XigmaNAS® <info@xigmanas.com>.
 	All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without
--- www/status_services.php
+++ www/status_services.php
@@ -3,7 +3,7 @@
 	services_status.php
 
 	Part of XigmaNAS® (https://www.xigmanas.com).
-	Copyright © 2018-2022 XigmaNAS® <info@xigmanas.com>.
+	Copyright © 2018-2023 XigmaNAS® <info@xigmanas.com>.
 	All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without
