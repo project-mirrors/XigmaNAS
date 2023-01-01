24d91c56a601a43e81f2dde77f85fadd4a55979d
Michael Schneider <ms49434@users.noreply.github.com>
2023-01-01 19:55:56 +0000

    Happy New Year!
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@9490 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- etc/inc/filemanager/fm_down.php
+++ etc/inc/filemanager/fm_down.php
@@ -3,7 +3,7 @@
 	fm_down.php
 
 	Part of XigmaNAS® (https://www.xigmanas.com).
-	Copyright © 2028-2022 XigmaNAS® <info@xigmanas.com>.
+	Copyright © 2018-2023 XigmaNAS® <info@xigmanas.com>.
 	All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without
