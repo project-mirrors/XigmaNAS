8ed2978bbc10524d8d6eaa346b9456f1af3c1d6a
Michael Schneider <ms49434@users.noreply.github.com>
2023-02-26 22:00:57 +0000

    rewrite phase 1
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@9727 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/services_rsyncd_module_edit.php
+++ www/services_rsyncd_module_edit.php
@@ -31,20 +31,21 @@
 	of the authors and should not be interpreted as representing official policies
 	of XigmaNAS®, either expressed or implied.
 */
+
+require_once 'autoload.php';
 require_once 'auth.inc';
 require_once 'guiconfig.inc';
 
-if (isset($_GET['uuid']))
-	$uuid = $_GET['uuid'];
-if (isset($_POST['uuid']))
-	$uuid = $_POST['uuid'];
+use common\arr;
+use	common\uuid;
+use gui\document;
 
-$a_mount = &array_make_branch($config,'mounts','mount');
-array_sort_key($a_mount,'devicespecialfile');
-$a_module = &array_make_branch($config,'rsyncd','module');
-array_sort_key($a_module,'name');
-
-if (isset($uuid) && (FALSE !== ($cnid = array_search_ex($uuid, $a_module, "uuid")))) {
+$uuid = $_POST['uuid'] ?? $_GET['uuid'] ?? null;
+$a_mount = &arr::make_branch($config,'mounts','mount');
+arr::sort_key($a_mount,'devicespecialfile');
+$a_module = &arr::make_branch($config,'rsyncd','module');
+arr::sort_key($a_module,'name');
+if(isset($uuid) && (false !== ($cnid = arr::search_ex($uuid,$a_module,'uuid')))):
 	$pconfig['uuid'] = $a_module[$cnid]['uuid'];
 	$pconfig['name'] = $a_module[$cnid]['name'];
 	$pconfig['path'] = $a_module[$cnid]['path'];
@@ -56,192 +57,137 @@ if (isset($uuid) && (FALSE !== ($cnid = array_search_ex($uuid, $a_module, "uuid"
 	$pconfig['hostsdeny'] = $a_module[$cnid]['hostsdeny'];
 	$pconfig['uid'] = $a_module[$cnid]['uid'];
 	$pconfig['gid'] = $a_module[$cnid]['gid'];
-	if (isset($a_module[$cnid]['auxparam']) && is_array($a_module[$cnid]['auxparam']))
-		$pconfig['auxparam'] = implode("\n", $a_module[$cnid]['auxparam']);
-} else {
-	$pconfig['uuid'] = uuid();
-	$pconfig['name'] = "";
-	$pconfig['path'] = "";
-	$pconfig['comment'] = "";
+	if(isset($a_module[$cnid]['auxparam']) && is_array($a_module[$cnid]['auxparam'])):
+		$pconfig['auxparam'] = implode("\n",$a_module[$cnid]['auxparam']);
+	endif;
+else:
+	$pconfig['uuid'] = uuid::create_v4();
+	$pconfig['name'] = '';
+	$pconfig['path'] = '';
+	$pconfig['comment'] = '';
 	$pconfig['list'] = true;
-	$pconfig['rwmode'] = "rw";
-	$pconfig['maxconnections'] = "0";
-	$pconfig['hostsallow'] = "";
-	$pconfig['hostsdeny'] = "";
-	$pconfig['uid'] = "";
-	$pconfig['gid'] = "";
-	$pconfig['auxparam'] = "";
-}
-
-if ($_POST) {
+	$pconfig['rwmode'] = 'rw';
+	$pconfig['maxconnections'] = '0';
+	$pconfig['hostsallow'] = '';
+	$pconfig['hostsdeny'] = '';
+	$pconfig['uid'] = '';
+	$pconfig['gid'] = '';
+	$pconfig['auxparam'] = '';
+endif;
+if($_POST):
 	unset($input_errors);
 	$pconfig = $_POST;
-
-	if (isset($_POST['Cancel']) && $_POST['Cancel']) {
+	if(isset($_POST['Cancel']) && $_POST['Cancel']):
 		header("Location: services_rsyncd_module.php");
 		exit;
-	}
-
-	// Input validation.
+	endif;
+//	Input validation.
 	$reqdfields = ['name','comment'];
 	$reqdfieldsn = [gtext('Name'),gtext('Comment')];
-	do_input_validation($_POST, $reqdfields, $reqdfieldsn, $input_errors);
+	do_input_validation($_POST,$reqdfields,$reqdfieldsn,$input_errors);
 	$reqdfieldst = ['string','string'];
-	do_input_validation_type($_POST, $reqdfields, $reqdfieldsn, $reqdfieldst, $input_errors);
-
-	if(empty($input_errors)) {
+	do_input_validation_type($_POST,$reqdfields,$reqdfieldsn,$reqdfieldst,$input_errors);
+	if(empty($input_errors)):
 		$module = [];
 		$module['uuid'] = $_POST['uuid'];
 		$module['name'] = $_POST['name'];
 		$module['path'] = $_POST['path'];
 		$module['comment'] = $_POST['comment'];
-		$module['list'] = isset($_POST['list']) ? true : false;
+		$module['list'] = isset($_POST['list']);
 		$module['rwmode'] = $_POST['rwmode'];
 		$module['maxconnections'] = $_POST['maxconnections'];
 		$module['hostsallow'] = $_POST['hostsallow'];
 		$module['hostsdeny'] = $_POST['hostsdeny'];
 		$module['uid'] = $_POST['uid'];
 		$module['gid'] = $_POST['gid'];
-
-		# Write additional parameters.
+//		Write additional parameters.
 		unset($module['auxparam']);
-		$a_auxparam = explode("\n", $_POST['auxparam']);
-		foreach($a_auxparam as $auxparam) {
-			$auxparam = trim($auxparam, "\t\n\r");
-			if (!empty($auxparam))
+		$a_auxparam = explode("\n",$_POST['auxparam']);
+		foreach($a_auxparam as $auxparam):
+			$auxparam = trim($auxparam,"\t\n\r");
+			if(!empty($auxparam)):
 				$module['auxparam'][] = $auxparam;
-	}
-		if (isset($uuid) && (FALSE !== $cnid)) {
+			endif;
+		endforeach;
+		if(isset($uuid) && ($cnid !== false)):
 			$a_module[$cnid] = $module;
 			$mode = UPDATENOTIFY_MODE_MODIFIED;
-		} else {
+		else:
 			$a_module[] = $module;
 			$mode = UPDATENOTIFY_MODE_NEW;
-		}
-		updatenotify_set("rsyncd", $mode, $module['uuid']);
+		endif;
+		updatenotify_set('rsyncd',$mode,$module['uuid']);
 		write_config();
-		header("Location: services_rsyncd_module.php");
+		header('Location: services_rsyncd_module.php');
 		exit;
-	}
-}
+	endif;
+endif;
 $pgtitle = [gtext('Services'),gtext('Rsync'),gtext('Server'),gtext('Module'),isset($uuid) ? gtext('Edit') : gtext('Add')];
+include 'fbegin.inc';
+$document = new document();
+$document->
+	add_area_tabnav()->
+		push()->
+		add_tabnav_upper()->
+			ins_tabnav_record(href: 'services_rsyncd.php',value: gettext('Server'),title: gettext('Reload page'),active: true)->
+			ins_tabnav_record(href: 'services_rsyncd_client.php',value: gettext('Client'))->
+			ins_tabnav_record(href: 'services_rsyncd_local.php',value: gettext('Local'))->
+		pop()->
+		add_tabnav_lower()->
+			ins_tabnav_record(href: 'services_rsyncd.php',value: gettext('Settings'))->
+			ins_tabnav_record(href: 'services_rsyncd_module.php',value: gettext('Modules'),title: gettext('Reload page'),active: true);
+$document->render();
 ?>
-<?php include 'fbegin.inc';?>
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
-	<tr>
-		<td class="tabnavtbl">
-			<ul id="tabnav">
-				<li class="tabact"><a href="services_rsyncd.php" title="<?=gtext('Reload page');?>"><span><?=gtext("Server");?></span></a></li>
-				<li class="tabinact"><a href="services_rsyncd_client.php"><span><?=gtext("Client");?></span></a></li>
-				<li class="tabinact"><a href="services_rsyncd_local.php"><span><?=gtext("Local");?></span></a></li>
-			</ul>
-		</td>
-	</tr>
-	<tr>
-		<td class="tabnavtbl">
-			<ul id="tabnav2">
-				<li class="tabinact"><a href="services_rsyncd.php"><span><?=gtext("Settings");?></span></a></li>
-				<li class="tabact"><a href="services_rsyncd_module.php" title="<?=gtext('Reload page');?>"><span><?=gtext("Modules");?></span></a></li>
-			</ul>
-		</td>
-	</tr>
-	<tr>
-		<td class="tabcont">
-			<form action="services_rsyncd_module_edit.php" method="post" name="iform" id="iform" onsubmit="spinner()">
-			<?php if (!empty($input_errors)) print_input_errors($input_errors); ?>
-			<table width="100%" border="0" cellpadding="6" cellspacing="0">
-			<?php html_titleline(gtext("Module Settings"));?>
-				<tr>
-				<td width="22%" valign="top" class="vncellreq"><?=gtext("Name");?></td>
-				<td width="78%" class="vtable">
-				<input name="name" type="text" class="formfld" id="name" size="30" value="<?=htmlspecialchars($pconfig['name']);?>" />
-				</td>
-				</tr>
-				<tr>
-					<td width="22%" valign="top" class="vncellreq"><?=gtext("Comment");?></td>
-					<td width="78%" class="vtable">
-						<input name="comment" type="text" class="formfld" id="comment" size="30" value="<?=htmlspecialchars($pconfig['comment']);?>" />
-					</td>
-				</tr>
-				<tr>
-					<td width="22%" valign="top" class="vncellreq"><?=gtext("Path");?></td>
-					<td width="78%" class="vtable">
-						<input name="path" type="text" class="formfld" id="path" size="60" value="<?=htmlspecialchars($pconfig['path']);?>" />
-						<input name="browse" type="button" class="formbtn" id="Browse" onclick='ifield = form.path; filechooser = window.open("filechooser.php?p="+encodeURIComponent(ifield.value)+"&amp;sd=<?=$g['media_path'];?>", "filechooser", "scrollbars=yes,toolbar=no,menubar=no,statusbar=no,width=550,height=300"); filechooser.ifield = ifield; window.ifield = ifield; window.slash_path = 1;' value="..." /><br />
-						<span class="vexpl"><?=gtext("Path to be shared.");?></span>
-					</td>
-				</tr>
-				<tr>
-					<td width="22%" valign="top" class="vncell"><?=gtext("List");?></td>
-					<td width="78%" class="vtable">
-						<input name="list" type="checkbox" id="list" value="yes" <?php if (!empty($pconfig['list'])) echo "checked=\"checked\""; ?> />
-						<?=gtext("Enable module listing.");?><br />
-						<span class="vexpl"><?=gtext("This option determines if this module should be listed when the client asks for a listing of available modules. By setting this to false you can create hidden modules.");?></span>
-					</td>
-				</tr>
-				<tr>
-					<td width="22%" valign="top" class="vncell"><?=gtext("Access Mode");?></td>
-					<td width="78%" class="vtable">
-						<select name="rwmode" size="1" id="rwmode">
-						<option value="ro" <?php if ("ro" === $pconfig['rwmode']) echo "selected=\"selected\"";?>><?=gtext("Read only");?></option>
-						<option value="rw" <?php if ("rw" === $pconfig['rwmode']) echo "selected=\"selected\"";?>><?=gtext("Read/Write");?></option>
-						<option value="wo" <?php if ("wo" === $pconfig['rwmode']) echo "selected=\"selected\"";?>><?=gtext("Write only");?></option>
-						</select><br />
-						<span class="vexpl"><?=gtext("This controls the access a remote host has to this module.");?></span>
-					</td>
-				</tr>
-				<tr>
-					<td width="22%" valign="top" class="vncell"><?=gtext("Max. Connections");?></td>
-					<td width="78%" class="vtable">
-					<input name="maxconnections" type="text" id="maxconnections" size="5" value="<?=htmlspecialchars($pconfig['maxconnections']);?>" /><br />
-					<span class="vexpl"><?=gtext("Maximum number of simultaneous connections. Default is 0 (unlimited).");?></span>
-					</td>
-				</tr>
-				<tr>
-					<td width="22%" valign="top" class="vncell"><?=gtext("User ID");?></td>
-					<td width="78%" class="vtable">
-					<input name="uid" type="text" class="formfld" id="uid" size="60" value="<?=htmlspecialchars($pconfig['uid']);?>" /><br />
-					<span class="vexpl"><?=sprintf(gtext("This option specifies the user name or user ID that file transfers to and from that module should take place. In combination with the '%s' option this determines what file permissions are available. Leave this field empty to use default settings."), gtext("Group ID"));?></span>
-					</td>
-				</tr>
-				<tr>
-					<td width="22%" valign="top" class="vncell"><?=gtext("Group ID");?></td>
-					<td width="78%" class="vtable">
-					<input name="gid" type="text" class="formfld" id="gid" size="60" value="<?=htmlspecialchars($pconfig['gid']);?>" /><br />
-					<span class="vexpl"><?=gtext("This option specifies the group name or group ID that file transfers to and from that module should take place. Leave this field empty to use default settings.");?></span>
-					</td>
-				</tr>
-				<tr>
-					<td width="22%" valign="top" class="vncell"><?=gtext("Hosts Allow");?></td>
-					<td width="78%" class="vtable">
-					<input name="hostsallow" type="text" class="formfld" id="hostsallow" size="60" value="<?=htmlspecialchars($pconfig['hostsallow']);?>" /><br />
-					<span class="vexpl"><?=gtext("This option is a comma, space, or tab delimited set of hosts which are permitted to access this module. You can specify the hosts by name or IP number. Leave this field empty to use default settings.");?></span>
-					</td>
-				</tr>
-				<tr>
-					<td width="22%" valign="top" class="vncell"><?=gtext("Hosts Deny");?></td>
-					<td width="78%" class="vtable">
-					<input name="hostsdeny" type="text" class="formfld" id="hostsdeny" size="60" value="<?=htmlspecialchars($pconfig['hostsdeny']);?>" /><br />
-					<span class="vexpl"><?=gtext("This option is a comma, space, or tab delimited set of host which are NOT permitted to access this module. Where the lists conflict, the allow list takes precedence. In the event that it is necessary to deny all by default, use the keyword ALL (or the netmask 0.0.0.0/0) and then explicitly specify to the hosts allow parameter those hosts that should be permitted access. Leave this field empty to use default settings.");?></span>
-					</td>
-				</tr>
-				<?php
-					$helpinghand = '<a href="'
-					. 'http://rsync.samba.org/ftp/rsync/rsync.html'
-					. '" target="_blank">'
-					. gtext('Please check the documentation')
-					. '</a>.';
-					html_textarea("auxparam", gtext("Additional Parameters"), !empty($pconfig['auxparam']) ? $pconfig['auxparam'] : "", gtext("These parameters will be added to the module configuration in rsyncd.conf.") . " " . $helpinghand, false, 65, 5, false, false);
-					?>
-				</table>
-		<div id="submit">
-			<input name="Submit" type="submit" class="formbtn" value="<?=(isset($uuid) && (FALSE !== $cnid)) ? gtext("Save") : gtext("Add")?>" />
-			<input name="Cancel" type="submit" class="formbtn" value="<?=gtext("Cancel");?>" />
-			<input name="uuid" type="hidden" value="<?=$pconfig['uuid'];?>" />
-			</div>
-	<?php include 'formend.inc';?>
-</form>
-</td>
-</tr>
-</table>
-<?php include 'fend.inc';?>
+<form action="services_rsyncd_module_edit.php" method="post" name="iform" id="iform" class="pagecontent"><div class="area_data_top"></div><div id="area_data_frame">
+<?php
+	if(!empty($input_errors)):
+		print_input_errors($input_errors);
+	endif;
+?>
+	<table class="area_data_settings">
+		<colgroup>
+			<col class="area_data_settings_col_tag">
+			<col class="area_data_settings_col_data">
+		</colgroup>
+		<thead>
+<?php
+			html_titleline2(gettext('Module Settings'));
+?>
+		</thead>
+		<tbody>
+<?php
+			html_inputbox2(ctrlname: 'name',title: gettext('Name'),value: $pconfig['name'] ?? '',desc: '',required: true,size: 30,readonly: false,altpadding: false,maxlength: 0,placeholder: '');
+			html_inputbox2(ctrlname: 'comment',title: gettext('Comment'),value: $pconfig['comment'] ?? '',desc: '',required: true,size: 30,readonly: false,altpadding: false,maxlength: 0,placeholder: '');
+			html_filechooser2(ctrlname: 'path',title: gettext('Path'),value: $pconfig['path'] ?? '',desc: gettext('Path to be shared.'),path: $g['media_path'],required: true,size: 60,readonly: false);
+			html_checkbox2(ctrlname: 'list',title: gettext('List'),checked: !empty($pconfig['list']),caption: gettext('Enable module listing.'),desc: gettext('This option determines if this module should be listed when the client asks for a listing of available modules. By setting this to false you can create hidden modules.'),required: false,readonly: false,onclick: '',altpadding: false);
+			$options = [
+				"ro" => gettext('Read only'),
+				"rw" => gettext('Read/Write'),
+				"wo" => gettext('Write only')
+			];
+			html_combobox2(ctrlname: 'rwmode',title: gettext('Access Mode'),value: $pconfig['rwmode'] ?? '',options: $options,desc: gettext('This controls the access a remote host has to this module.'),required: false,readonly: false,onclick: '');
+			html_inputbox2(ctrlname: 'maxconnections',title: gettext('Max. Connections'),value: $pconfig['maxconnections'] ?? '',desc: gettext('Maximum number of simultaneous connections. Default is 0 (unlimited).'),required: false,size: 5,readonly: false,altpadding: false,maxlength: 0,placeholder: '');
+			html_inputbox2(ctrlname: 'uid',title: gettext('User ID'),value: $pconfig['uid'] ?? '',desc: sprintf(gettext("This option specifies the user name or user ID that file transfers to and from that module should take place. In combination with the '%s' option this determines what file permissions are available. Leave this field empty to use default settings."),gettext('Group ID')),required: false,size: 60,readonly: false,altpadding: false,maxlength: 0,placeholder: '');
+			html_inputbox2(ctrlname: 'gid',title: gettext('Group ID'),value: $pconfig['gid'] ?? '',desc: gettext('This option specifies the group name or group ID that file transfers to and from that module should take place. Leave this field empty to use default settings.'),required: false,size: 60,readonly: false,altpadding: false,maxlength: 0,placeholder: '');
+			html_inputbox2(ctrlname: 'hostsallow',title: gettext('Hosts Allow'),value: $pconfig['hostsallow'] ?? '',desc: gettext('This option is a comma, space, or tab delimited set of hosts which are permitted to access this module. You can specify the hosts by name or IP number. Leave this field empty to use default settings.'),required: false,size: 60,readonly: false,altpadding: false,maxlength: 0,placeholder: '');
+			html_inputbox2(ctrlname: 'hostsdeny',title: gettext('Hosts Deny'),value: $pconfig['hostsdeny'] ?? '',desc: gettext('This option is a comma, space, or tab delimited set of hosts which are NOT permitted to access this module. Where the lists conflict, the allow list takes precedence. In the event that it is necessary to deny all by default, use the keyword ALL (or the netmask 0.0.0.0/0) and then explicitly specify to the hosts allow parameter those hosts that should be permitted access. Leave this field empty to use default settings.'),required: false,size: 60,readonly: false,altpadding: false,maxlength: 0,placeholder: '');
+			$helpinghand = '<a href="'
+			. 'http://rsync.samba.org/ftp/rsync/rsync.html'
+			. '" target="_blank">'
+			. gettext('Please check the documentation')
+			. '</a>.';
+			html_textarea2(ctrlname: 'auxparam',title: gettext('Additional Parameters'),value: $pconfig['auxparam'] ?? '',desc: gettext('These parameters will be added to the module configuration in rsyncd.conf.') . ' ' . $helpinghand,required: false,columns: 65,rows: 5,readonly: false,wrap: false);
+?>
+		</tbody>
+	</table>
+	<div id="submit">
+		<input name="Submit" type="submit" class="formbtn" value="<?=(isset($uuid) && ($cnid !== false)) ? gtext('Save') : gtext('Add')?>" />
+		<input name="Cancel" type="submit" class="formbtn" value="<?=gtext('Cancel');?>" />
+		<input name="uuid" type="hidden" value="<?=$pconfig['uuid'];?>" />
+	</div>
+<?php
+	include 'formend.inc';
+?>
+</div><div class="area_data_pot"></div></form>
+<?php
+include 'fend.inc';
