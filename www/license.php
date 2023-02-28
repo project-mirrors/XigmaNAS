6ac0f19f10b50fe2f41d8e2cb699c8a604c6e603
Michael Schneider <ms49434@users.noreply.github.com>
2023-02-28 16:57:43 +0000

    em -> rem
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@9744 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- www/license.php
+++ www/license.php
@@ -299,7 +299,7 @@ $td->addP([],'<strong>XigmaNAS is a registered trademark of Michael Zoon (<a hre
 $td->addP([],'The compilation of software, code and documentation known as XigmaNAS® is distributed under the following terms:');
 $td->addP([],'Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:');
 $td->
-	addElement('ol',['start' => '1','style' => 'padding-left: 2em;'])->
+	addElement('ol',['start' => '1','style' => 'padding-left: 2rem;'])->
 		insElement('li',[],'Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.')->
 		insElement('li',[],'Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.');
 $td->addP([],
