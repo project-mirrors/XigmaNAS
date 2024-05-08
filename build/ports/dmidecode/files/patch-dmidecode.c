4eea036d8fa6b93ef4d64fd81fb8efdd8fa491f8
Michael Zoon <zoon01@users.noreply.github.com>
2024-05-08 16:09:51 +0000

    update patch file
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@9961 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/ports/dmidecode/files/patch-dmidecode.c
+++ build/ports/dmidecode/files/patch-dmidecode.c
@@ -1,5 +1,14 @@
 --- dmidecode.c.orig	2023-03-14 17:32:17.000000000 +0100
-+++ dmidecode.c	2024-03-29 21:22:19.000000000 +0100
++++ dmidecode.c	2024-05-08 18:06:37.000000000 +0200
+@@ -2225,7 +2225,7 @@
+ 
+ 	if (code1 & (1 << 0))
+ 		pr_attr(attr, "Unknown");
+-	else if ((code1 & 0xFE) == 0 && (code2 & 0x07) == 0)
++	else if ((code1 & 0xFE) == 0 && code2 == 0)
+ 		pr_attr(attr, "None");
+ 	else
+ 	{
 @@ -6025,17 +6025,25 @@
  		pr_comment("dmidecode %s", VERSION);
  
