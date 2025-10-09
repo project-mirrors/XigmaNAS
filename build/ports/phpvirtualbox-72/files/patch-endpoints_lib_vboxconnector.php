c74adbd84c627607dafafd8ea1feb570fd165cd0
Michael Zoon <zoon01@users.noreply.github.com>
2025-10-09 03:10:55 +0000

    update patch
    git-svn-id: https://svn.code.sf.net/p/xigmanas/code/trunk@10503 beaaabd7-781f-4c1e-bc4d-5cfe25df7511

--- build/ports/phpvirtualbox-72/files/patch-endpoints_lib_vboxconnector.php
+++ build/ports/phpvirtualbox-72/files/patch-endpoints_lib_vboxconnector.php
@@ -1,6 +1,6 @@
---- endpoints/lib/vboxconnector.php.orig	2025-03-09 01:02:03 UTC
-+++ endpoints/lib/vboxconnector.php
-@@ -113,6 +113,8 @@ class vboxconnector {
+--- endpoints/lib/vboxconnector.php.orig	2025-10-08 03:41:46.325272000 +0200
++++ endpoints/lib/vboxconnector.php	2025-10-09 04:59:00.000000000 +0200
+@@ -113,6 +113,8 @@
  	 */
  	var $dsep = null;
  
@@ -9,7 +9,7 @@
  	/**
  	 * Obtain configuration settings and set object vars
  	 * @param boolean $useAuthMaster use the authentication master obtained from configuration class
-@@ -388,7 +390,8 @@ class vboxconnector {
+@@ -388,7 +390,8 @@
  
  			// The amount of time we will wait for events is determined by
  			// the amount of listeners - at least half a second
@@ -19,3 +19,13 @@
  		}
  
  		// Get events from each configured event listener
+@@ -3859,6 +3862,9 @@
+ 
+ 		// Save and register
+ 		$m->saveSettings();
++		if((string)$m->FirmwareSettings->firmwareType == 'EFI') {
++			$m->nonVolatileStore->initUefiVariableStore(0);
++		}
+ 		$this->vbox->registerMachine($m->handle);
+ 		$vm = $m->id;
+ 		$m->releaseRemote();
